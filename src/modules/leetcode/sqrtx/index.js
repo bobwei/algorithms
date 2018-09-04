2cc9af241075689021b3618d3d7d6b57b21020b0
Bob Wei <bob.wei@linecorp.com>
2018-09-05 02:32:24 +0800

    Refactor sqrtx

--- src/modules/leetcode/sqrtx/index.js
+++ src/modules/leetcode/sqrtx/index.js
@@ -2,24 +2,22 @@
  * @param {number} x
  * @return {number}
  */
-const fn = x => {
-  if (!x) {
-    return 0;
-  }
-  let low = 1;
-  let high = x;
-  while (low <= high) {
-    const mid = Math.floor((low + high) / 2);
-    const result = mid * mid;
+const mySqrt = function(x) {
+  let i = 1;
+  let j = x;
+  while (i <= j) {
+    const m = Math.floor((i + j) / 2);
+    const result = m * m;
     if (result === x) {
-      return mid;
-    } else if (result > x) {
-      high = mid - 1;
+      return m;
+    }
+    if (result > x) {
+      j = m - 1;
     } else if (result < x) {
-      low = mid + 1;
+      i = m + 1;
     }
   }
-  return high;
+  return j;
 };
 
-export default fn;
+export default mySqrt;
