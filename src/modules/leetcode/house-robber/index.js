a42bad279869c5729d8ac3cc20f9a5809972a0fd
Bob Wei <bob.wei@linecorp.com>
2018-09-06 00:23:20 +0800

    Refactor house-robber

--- src/modules/leetcode/house-robber/index.js
+++ src/modules/leetcode/house-robber/index.js
@@ -8,22 +8,22 @@ f(0) = arr[0]
 f(1) = max(arr[0], arr[1])
 f(2) = max(arr[2] + f(0), f(1))
 */
-const rob = nums => {
-  if (nums.length <= 0) {
+const rob = (arr) => {
+  if (arr.length <= 0) {
     return 0;
   }
-  if (nums.length <= 1) {
-    return nums[0];
+  if (arr.length <= 1) {
+    return arr[0];
   }
-  let nMinus2 = nums[0];
-  let nMinus1 = Math.max(nums[0], nums[1]);
-  let n = Math.max(nMinus1, nMinus2);
-  for (let i = 2; i < nums.length; i++) {
-    n = Math.max(nums[i] + nMinus2, nMinus1);
-    nMinus2 = nMinus1;
-    nMinus1 = n;
+  let x = arr[0];
+  let y = Math.max(arr[0], arr[1]);
+  let output = y;
+  for (let i = 2; i < arr.length; i++) {
+    output = Math.max(arr[i] + x, y);
+    x = y;
+    y = output;
   }
-  return n;
+  return output;
 };
 
 export default rob;
