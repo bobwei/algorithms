587c6e48d7485604fbeb72aba3cfa35c04c7ef31
Bob Wei <bob.wei@linecorp.com>
2018-09-11 00:58:58 +0800

    Add two-sum

---
+++ src/modules/leetcode/two-sum/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+export default twoSum;
