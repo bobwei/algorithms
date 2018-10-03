c14841dbf2da0edb895c649c0a2f9986797490c2
Bob Wei <bob.wei@linecorp.com>
2018-10-04 01:08:40 +0800

    Update threeSum

--- src/leetcode/3sum/index.js
+++ src/leetcode/3sum/index.js
@@ -2,35 +2,36 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
 /*
-  [-4, -4, -1, -1, -1, -1, 0, 0, 0, 1, 2, 2, 2]
+  [-4, -1, -1, -1, -1, 0, 0, 0, 1, 1]
 */
 
 const threeSum = function(nums) {
   nums.sort((a, b) => a - b);
   const output = [];
-  for (let i = 0; i < nums.length - 2; i++) {
+  for (let i = 0; i < nums.length; i++) {
     if (nums[i] === nums[i - 1]) {
       continue;
     }
-    let j = i + 1;
-    let k = nums.length - 1;
-    while (j < k) {
-      const sum = nums[i] + nums[j] + nums[k];
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
       if (sum === 0) {
-        output.push([nums[i], nums[j], nums[k]]);
-        j += 1;
-        k -= 1;
-        while (nums[k] === nums[k + 1]) {
-          k -= 1;
+        output.push([nums[i], nums[left], nums[right]]);
+        left += 1;
+        while (nums[left] === nums[left - 1]) {
+          left += 1;
         }
-        while (nums[j] === nums[j - 1]) {
-          j += 1;
+        right -= 1;
+        while (nums[right] === nums[right + 1]) {
+          right -= 1;
         }
-      } else if (sum > 0) {
-        k -= 1;
       } else if (sum < 0) {
-        j += 1;
+        left += 1;
+      } else if (sum > 0) {
+        right -= 1;
       }
     }
   }
