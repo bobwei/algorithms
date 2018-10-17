1b9221de93be0b4e7702771ccac32ee66dcba82c
Bob Wei <bob.wei@linecorp.com>
2018-10-18 01:11:23 +0800

    Update maximum-size-subarray-sum-equals-k

--- src/leetcode/maximum-size-subarray-sum-equals-k/index.js
+++ src/leetcode/maximum-size-subarray-sum-equals-k/index.js
@@ -2,33 +2,19 @@
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
-/*
-  [-2, -1, 2, 1]
-  1
-  [1,0,-1]
-  -1
-  sum i to j = sum[j] - sum[i - 1]
-  let's call sum[i - 1] "sum previous"
-  we are looking for sum previous such that sum[j] - sum previous = k
-  sum previous = sum[j] - k
-*/
-
-const maxSubArrayLen = function(nums, k) {
-  let max = 0;
-  const sums = new Map();
+ */
+var maxSubArrayLen = function(nums, k) {
+  const cache = new Map([[0, -1]]);
   let sum = 0;
-  for (let j = 0; j < nums.length; j++) {
-    sum += nums[j];
-    if (sum === k) {
-      max = Math.max(max, j + 1);
-    } else if (sums.has(sum - k)) {
-      const i = sums.get(sum - k) + 1;
-      const length = j - i + 1;
-      max = Math.max(max, length);
+  let max = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (cache.has(sum - k)) {
+      max = Math.max(max, i - cache.get(sum - k));
     }
-    if (!sums.has(sum)) {
-      sums.set(sum, j);
+    if (!cache.has(sum)) {
+      cache.set(sum, i);
     }
   }
-  return max;
+  return max > -Infinity ? max : 0;
 };
