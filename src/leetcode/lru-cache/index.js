67e5c80b13d666a0c339ed49ad10157ab76c3590
Bob Wei <bob.wei@linecorp.com>
2018-10-31 00:48:58 +0800

    Update lru-cache

--- src/leetcode/lru-cache/index.js
+++ src/leetcode/lru-cache/index.js
@@ -1,65 +1,72 @@
 const Node = function(key, value) {
   this.key = key;
   this.value = value;
-  this.next = null;
   this.prev = null;
+  this.next = null;
 };
 
-const LRUCache = function(capacity) {
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
   this.capacity = capacity;
-  this.data = {
-    hash: {},
-    size: 0,
-  };
+  this.data = {};
+  this.size = 0;
   this.head = new Node();
   this.tail = new Node();
   this.head.next = this.tail;
   this.tail.prev = this.head;
 };
 
+LRUCache.prototype.moveToHead = function(node) {
+  node.next = this.head.next;
+  this.head.next.prev = node;
+  this.head.next = node;
+  node.prev = this.head;
+};
+
+LRUCache.prototype.disconnect = function(node) {
+  node.prev.next = node.next;
+  node.next.prev = node.prev;
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
 LRUCache.prototype.get = function(key) {
-  if (this.data.hash[key]) {
-    this.disconnect(key);
-    this.moveHead(key);
-    return this.data.hash[key].value;
+  if (!this.data[key]) {
+    return -1;
   }
-  return -1;
+  this.disconnect(this.data[key]);
+  this.moveToHead(this.data[key]);
+  return this.data[key].value;
 };
 
-LRUCache.prototype.disconnect = function(key) {
-  this.data.hash[key].next.prev = this.data.hash[key].prev;
-  this.data.hash[key].prev.next = this.data.hash[key].next;
-};
-
-LRUCache.prototype.moveHead = function(key) {
-  if (this.head.next === this.data.hash[key]) {
-    return;
-  }
-  this.head.next.prev = this.data.hash[key];
-  this.data.hash[key].next = this.head.next;
-  this.data.hash[key].prev = this.head;
-  this.head.next = this.data.hash[key];
-};
-
-LRUCache.prototype.moveTail = function() {
-  delete this.data.hash[this.tail.prev.key];
-  this.data.size -= 1;
-  this.tail.prev.prev.next = this.tail;
-  this.tail.prev = this.tail.prev.prev;
-};
-
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
 LRUCache.prototype.put = function(key, value) {
-  if (!this.data.hash[key]) {
-    this.data.hash[key] = new Node(key, value);
-    this.data.size += 1;
+  if (!this.data[key]) {
+    this.data[key] = new Node(key, value);
+    this.size += 1;
   } else {
-    this.data.hash[key].value = value;
-    this.disconnect(key);
+    this.data[key].value = value;
+    this.disconnect(this.data[key]);
   }
-  this.moveHead(key);
-  if (this.data.size > this.capacity) {
-    this.moveTail();
+  this.moveToHead(this.data[key]);
+  if (this.size > this.capacity) {
+    delete this.data[this.tail.prev.key];
+    this.size -= 1;
+    this.disconnect(this.tail.prev);
   }
 };
 
-export default LRUCache;
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = Object.create(LRUCache).createNew(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
