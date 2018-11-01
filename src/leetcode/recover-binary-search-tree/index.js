9ae62591d8550e534db2a53d23d1f055a3337fcb
Bob Wei <bob.wei@linecorp.com>
2018-11-01 23:53:43 +0800

    Update recover-binary-search-tree

--- src/leetcode/recover-binary-search-tree/index.js
+++ src/leetcode/recover-binary-search-tree/index.js
@@ -10,21 +10,6 @@
  * @return {void} Do not return anything, modify root in-place instead.
  */
 
-const inOrder = (root, result) => {
-  if (!root) {
-    return;
-  }
-  inOrder(root.left, result);
-  if (result.pre && root.val < result.pre.val) {
-    if (!result.first) {
-      result.first = result.pre;
-    }
-    result.second = root;
-  }
-  result.pre = root;
-  inOrder(root.right, result);
-};
-
 const swap = (r1, r2) => {
   const tmp = r1.val;
   r1.val = r2.val;
@@ -32,7 +17,26 @@ const swap = (r1, r2) => {
 };
 
 var recoverTree = function(root) {
-  const result = {};
-  inOrder(root, result);
-  swap(result.first, result.second);
+  const stack = [];
+  let ptr = root;
+  let pre;
+  let first;
+  let second;
+  while (ptr || stack.length) {
+    if (ptr) {
+      stack.push(ptr);
+      ptr = ptr.left;
+    } else {
+      const node = stack.pop();
+      if (pre && pre.val > node.val) {
+        if (!first) {
+          first = pre;
+        }
+        second = node;
+      }
+      pre = node;
+      ptr = node.right;
+    }
+  }
+  swap(first, second);
 };
