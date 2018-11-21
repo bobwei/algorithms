efc11ece82558bcaa66c2d536f65588d4b6ad5e3
Bob Wei <bob.wei@linecorp.com>
2018-11-22 00:22:43 +0800

    Update merge-k-sorted-lists

--- src/leetcode/merge-k-sorted-lists/index.js
+++ src/leetcode/merge-k-sorted-lists/index.js
@@ -10,37 +10,38 @@
  * @return {ListNode}
  */
 
-const merge = (r1, r2) => {
-  if (!r1 || !r2) {
-    return r1 || r2;
-  }
-  let root;
-  if (r1.val <= r2.val) {
-    root = r1;
-    r1 = r1.next;
-  } else {
-    root = r2;
-    r2 = r2.next;
-  }
-  let ptr = root;
-  while (r1 && r2) {
-    if (r1.val <= r2.val) {
-      ptr.next = r1;
-      r1 = r1.next;
-    } else {
-      ptr.next = r2;
-      r2 = r2.next;
+const merge = (h1, h2) => {
+  const dummy = new ListNode();
+  let ptr = dummy;
+  let p1 = h1;
+  let p2 = h2;
+  while (p1 || p2) {
+    if (!p1 || !p2) {
+      ptr.next = p1 || p2;
+      return dummy.next;
+    } else if (p1.val <= p2.val) {
+      ptr.next = p1;
+      ptr = ptr.next;
+      p1 = p1.next;
+    } else if (p2.val <= p1.val) {
+      ptr.next = p2;
+      ptr = ptr.next;
+      p2 = p2.next;
     }
-    ptr = ptr.next;
   }
-  ptr.next = r1 || r2;
-  return root;
+  return dummy.next;
 };
 
-var mergeKLists = function(lists) {
-  let ptr = null;
-  for (let i = 0; i < lists.length; i++) {
-    ptr = merge(ptr, lists[i]);
+var mergeKLists = function(lists, start = 0, end = lists.length - 1) {
+  if (!lists.length) {
+    return null;
   }
-  return ptr;
+  if (end - start + 1 <= 1) {
+    return lists[start];
+  }
+  if (end - start + 1 <= 2) {
+    return merge(lists[start], lists[end]);
+  }
+  const mid = Math.floor((start + end) / 2);
+  return merge(mergeKLists(lists, start, mid), mergeKLists(lists, mid + 1, end));
 };
