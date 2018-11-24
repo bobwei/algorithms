68036af4710c95b4522fb02a9a8f889fafa59e70
Bob Wei <bob.wei@linecorp.com>
2018-11-24 18:39:09 +0800

    Update number-of-islands

--- src/leetcode/number-of-islands/index.js
+++ src/leetcode/number-of-islands/index.js
@@ -2,46 +2,36 @@
  * @param {character[][]} grid
  * @return {number}
  */
-/*
-  11000
-  11000
-  00100
-  00011
-*/
+const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
-const visit = (grid, isVisited, i, j) => {
-  if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
+const dfs = (grid, visited, m, n, i, j) => {
+  if (i < 0 || i >= m || j < 0 || j >= n) {
     return;
   }
-  if (grid[i][j] === '1' && !isVisited[i][j]) {
-    isVisited[i][j] = true;
-    visit(grid, isVisited, i - 1, j);
-    visit(grid, isVisited, i, j + 1);
-    visit(grid, isVisited, i + 1, j);
-    visit(grid, isVisited, i, j - 1);
+  if (grid[i][j] === '0' || visited[i][j]) {
+    return;
+  }
+  visited[i][j] = true;
+  for (let k = 0; k < directions.length; k++) {
+    dfs(grid, visited, m, n, i + directions[k][0], j + directions[k][1]);
   }
 };
 
-const numIslands = function(grid) {
-  if (!grid.length) {
+var numIslands = function(grid) {
+  if (!grid.length || !grid[0].length) {
     return 0;
   }
-  if (!grid[0].length) {
-    return 0;
-  }
-  const isVisited = [...new Array(grid.length)].map(() =>
-    new Array(grid[0].length).fill(false),
-  );
-  let nIslands = 0;
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      if (!isVisited[i][j] && grid[i][j] === '1') {
-        nIslands += 1;
-        visit(grid, isVisited, i, j);
+  const m = grid.length;
+  const n = grid[0].length;
+  const visited = [...new Array(m)].map(() => new Array(n).fill(false));
+  let count = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === '1' && !visited[i][j]) {
+        count += 1;
+        dfs(grid, visited, m, n, i, j);
       }
     }
   }
-  return nIslands;
+  return count;
 };
-
-export default numIslands;
