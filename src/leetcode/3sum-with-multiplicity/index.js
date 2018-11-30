4a7922591caa7fde57789aaf39b2181d41330b10
Bob Wei <bob.wei@linecorp.com>
2018-12-01 01:11:32 +0800

    Update 3sum-with-multiplicity

--- src/leetcode/3sum-with-multiplicity/index.js
+++ src/leetcode/3sum-with-multiplicity/index.js
@@ -4,13 +4,14 @@
  * @return {number}
  */
 var threeSumMulti = function(A, target) {
-  const cache = {};
+  // number of combinations of 2 numbers of certain sum
+  const counts = {};
   let n = 0;
   for (let i = 0; i < A.length; i++) {
-    n += cache[target - A[i]] || 0;
+    n += counts[target - A[i]] || 0;
     for (let j = 0; j < i; j++) {
       const sum = A[i] + A[j];
-      cache[sum] = (cache[sum] || 0) + 1;
+      counts[sum] = (counts[sum] || 0) + 1;
     }
   }
   return n % (10 ** 9 + 7);
