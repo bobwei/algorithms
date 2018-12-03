35b3b8049df83e957c518181f6e8fc414bff975b
Bob Wei <bob.wei@linecorp.com>
2018-12-03 23:46:22 +0800

    Add more test cases to priority queue

--- src/practices/2018-12-03/priority-queue/index.spec.js
+++ src/practices/2018-12-03/priority-queue/index.spec.js
@@ -15,3 +15,37 @@ test('PriorityQueue', () => {
   expect(pq.dequeue()).toEqual({ id: 5 });
   expect(pq.dequeue()).toEqual({ id: 6 });
 });
+
+test('PriorityQueue', () => {
+  const pq = new PriorityQueue({
+    comparator: (a, b) => (a.id !== b.id ? a.id < b.id : a.label < b.label),
+  });
+  pq.enqueue({ id: 2 });
+  pq.enqueue({ id: 1 });
+  pq.enqueue({ id: 3 });
+  pq.enqueue({ id: 6 });
+  pq.enqueue({ id: 4 });
+  pq.enqueue({ id: 5, label: 1 });
+  pq.enqueue({ id: 5, label: 2 });
+  pq.enqueue({ id: 5, label: 3 });
+  pq.enqueue({ id: 5, label: 4 });
+  pq.enqueue({ id: 5, label: 5 });
+  const result = [];
+  const length = pq.arr.length;
+  for (let i = 0; i < length; i++) {
+    result.push(pq.dequeue());
+  }
+  const expectedResult = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5, label: 1 },
+    { id: 5, label: 2 },
+    { id: 5, label: 3 },
+    { id: 5, label: 4 },
+    { id: 5, label: 5 },
+    { id: 6 },
+  ];
+  expect(result).toEqual(expectedResult);
+});
