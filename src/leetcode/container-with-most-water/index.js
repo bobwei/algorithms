9ac7e6244cfea0ea8e31d3e5fe77dc8acea57a2e
Bob Wei <bob.wei@linecorp.com>
2018-12-08 19:02:09 +0800

    Update container-with-most-water

--- src/leetcode/container-with-most-water/index.js
+++ src/leetcode/container-with-most-water/index.js
@@ -2,25 +2,21 @@
  * @param {number[]} height
  * @return {number}
  */
-
-/*
-  https://www.cnblogs.com/zichi/p/5745992.html
-*/
-
-const maxArea = function(height) {
+var maxArea = function(height) {
+  let max = 0;
   let i = 0;
   let j = height.length - 1;
-  let max = 0;
   while (i < j) {
     const area = Math.min(height[i], height[j]) * (j - i);
     max = Math.max(max, area);
     if (height[i] < height[j]) {
       i += 1;
-    } else if (height[i] >= height[j]) {
+    } else if (height[j] < height[i]) {
+      j -= 1;
+    } else {
+      i += 1;
       j -= 1;
     }
   }
   return max;
 };
-
-export default maxArea;
