602f1bc447a4e4bab7a8d42fcec61235e7845b07
Bob Wei <bob.wei@linecorp.com>
2018-12-25 18:20:07 +0800

    Add binary-tree-postorder-traversal

---
+++ src/leetcode/binary-tree-postorder-traversal/index.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+  if (!root) {
+    return [];
+  }
+  const output = [];
+  const stack = [root];
+  while (stack.length) {
+    const node = stack.pop();
+    output.push(node.val);
+    if (node.left) {
+      stack.push(node.left);
+    }
+    if (node.right) {
+      stack.push(node.right);
+    }
+  }
+  output.reverse();
+  return output;
+};
