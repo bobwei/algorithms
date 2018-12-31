0101054d4cb73c41a227a9a5b7ad3e32c3474cda
Bob Wei <bob.wei@linecorp.com>
2019-01-01 01:21:11 +0800

    Update rotate-image

--- src/leetcode/rotate-image/index.js
+++ src/leetcode/rotate-image/index.js
@@ -2,23 +2,12 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-
-const swap = (m, i1, j1, i2, j2) => {
-  const tmp = m[i1][j1];
-  m[i1][j1] = m[i2][j2];
-  m[i2][j2] = tmp;
-};
-
 var rotate = function(matrix) {
+  matrix.reverse();
   const n = matrix.length;
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (j > i) {
-        swap(matrix, i, j, j, i);
-      }
-      if (j >= Math.floor(n / 2)) {
-        swap(matrix, i, j, i, n - 1 - j);
-      }
+    for (let j = i + 1; j < n; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
     }
   }
 };
