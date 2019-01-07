114f4929da7ed021703590a778ad74d9e7f4c5b2
Bob Wei <bob.wei@linecorp.com>
2019-01-07 13:01:19 +0800

    Update two-sum

--- src/leetcode/two-sum/index.js
+++ src/leetcode/two-sum/index.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  const cache = new Map();
+  const map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    if (cache.has(target - nums[i])) {
-      return [cache.get(target - nums[i]), i];
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
     }
-    cache.set(nums[i], i);
+    map.set(nums[i], i);
   }
 };
