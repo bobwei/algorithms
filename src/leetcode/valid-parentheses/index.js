65f07de686bd04f55855afa9dec373a9d6b81349
Bob Wei <bob.wei@linecorp.com>
2019-01-08 17:46:05 +0800

    Update valid-parentheses

--- src/leetcode/valid-parentheses/index.js
+++ src/leetcode/valid-parentheses/index.js
@@ -2,31 +2,23 @@
  * @param {string} s
  * @return {boolean}
  */
-
-const symbols = {
-  left: ['(', '[', '{'],
-  right: [')', ']', '}'],
-};
-
 var isValid = function(s) {
   const stack = [];
   for (let i = 0; i < s.length; i++) {
     const c = s[i];
-    if (symbols.left.indexOf(c) > -1) {
+    if (c === '(' || c === '[' || c === '{') {
       stack.push(c);
     } else {
-      const pre = stack.pop();
-      if (c === symbols.right[0] && pre !== symbols.left[0]) {
-        return false;
-      } else if (c === symbols.right[1] && pre !== symbols.left[1]) {
-        return false;
-      } else if (c === symbols.right[2] && pre !== symbols.left[2]) {
+      const left = stack.pop();
+      // prettier-ignore
+      const valid =
+        left ==='(' && c === ')' ||
+        left ==='[' && c === ']' ||
+        left ==='{' && c === '}';
+      if (!valid) {
         return false;
       }
     }
   }
-  if (stack.length > 0) {
-    return false;
-  }
-  return true;
+  return stack.length === 0;
 };
