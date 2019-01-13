82a6a5fc80724d40aebc9ce7e6feb2a8d4ce8683
Bob Wei <bob.wei@linecorp.com>
2019-01-14 01:03:31 +0800

    Update is-graph-bipartite

--- src/leetcode/is-graph-bipartite/index.js
+++ src/leetcode/is-graph-bipartite/index.js
@@ -3,16 +3,15 @@
  * @return {boolean}
  */
 
-const dfs = (graph, visited, sets, i) => {
+const dfs = (graph, sets, i) => {
   for (let k = 0; k < graph[i].length; k++) {
     const j = graph[i][k];
-    if (!visited[i][j]) {
-      if (sets[i] === sets[j]) {
-        return false;
-      }
+    if (sets[i] === sets[j]) {
+      return false;
+    }
+    if (sets[j] === null) {
       sets[j] = (sets[i] + 1) % 2;
-      visited[i][j] = true;
-      if (!dfs(graph, visited, sets, j)) {
+      if (!dfs(graph, sets, j)) {
         return false;
       }
     }
@@ -25,13 +24,12 @@ var isBipartite = function(graph) {
     return true;
   }
   const m = graph.length;
-  const visited = [...new Array(m)].map(() => new Array(m).fill(false));
   const sets = new Array(m).fill(null);
   for (let i = 0; i < m; i++) {
     if (sets[i] === null) {
       sets[i] = 0;
     }
-    if (!dfs(graph, visited, sets, i)) {
+    if (!dfs(graph, sets, i)) {
       return false;
     }
   }
