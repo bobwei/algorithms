358e6231d5763fb2ded05bb0ac0ca6761056bfe1
Bob Wei <bob.wei@linecorp.com>
2019-01-14 15:06:24 +0800

    Update search-in-rotated-sorted-array

--- src/leetcode/search-in-rotated-sorted-array/index.js
+++ src/leetcode/search-in-rotated-sorted-array/index.js
@@ -3,6 +3,7 @@
  * @param {number} target
  * @return {number}
  */
+
 var search = function(nums, target) {
   let left = 0;
   let right = nums.length - 1;
@@ -10,17 +11,17 @@ var search = function(nums, target) {
     const mid = Math.floor((left + right) / 2);
     if (nums[mid] === target) {
       return mid;
-    } else if (nums[mid] <= nums[right]) {
-      if (nums[mid] <= target && target <= nums[right]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
     } else if (nums[mid] >= nums[left]) {
-      if (nums[left] <= target && target <= nums[mid]) {
+      if (target >= nums[left] && target <= nums[mid]) {
         right = mid - 1;
       } else {
         left = mid + 1;
+      }
+    } else if (nums[mid] <= nums[right]) {
+      if (target >= nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
       }
     }
   }
