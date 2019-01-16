dc5ff9c752088cb3ae8785189ebb443cd7975823
Bob Wei <bob.wei@linecorp.com>
2019-01-16 14:36:42 +0800

    Update word-break-ii

--- src/leetcode/word-break-ii/index.js
+++ src/leetcode/word-break-ii/index.js
@@ -5,56 +5,37 @@
  */
 
 /*
-  s = "catsanddog"
-  wordDict = ["cat", "cats", "and", "sand", "dog"]
-
-  f(n) = {f(n) || f(n - wordDict[i].length)} for i in wordDict if isPostFix(wordDict[i])
+  dp(i) = dp(i) ||
+          dp(i - wordDict[j].length) if wordDict[j] is a postfix of s.slice(0, i)
 */
 
-const isPostFix = (str, n, target) => {
-  if (n < target.length) {
-    return false;
-  }
-  return str.slice(n - target.length, n) === target;
+const isPostFix = (s, i, w) => {
+  return s.slice(i - w.length, i) === w;
 };
 
-const dfs = (s, wordDict, dp, graph, v, selected = [], output = []) => {
-  if (selected.reduce((acc, cur) => acc + cur.length, 0) >= s.length) {
-    output.push([...selected]);
+const dfs = (dp, i, selected = '', output = []) => {
+  if (i <= 0) {
+    output.push(selected.trim());
     return output;
   }
-  for (let i = 0; i < graph[v].length; i++) {
-    const nextV = v - wordDict[graph[v][i]].length;
-    if (!dp[nextV]) {
-      continue;
-    }
-    selected.push(wordDict[graph[v][i]]);
-    dfs(s, wordDict, dp, graph, nextV, selected, output);
-    selected.pop();
+  for (let j = 0; j < dp[i].length; j++) {
+    dfs(dp, i - dp[i][j].length, dp[i][j] + ' ' + selected, output);
   }
   return output;
 };
 
-const wordBreak = function(s, wordDict) {
-  const graph = {};
-  const dp = new Array(s.length + 1).fill(false);
-  dp[0] = true;
+var wordBreak = function(s, wordDict) {
+  const dp = [...new Array(s.length + 1)].map(() => []);
   for (let i = 1; i <= s.length; i++) {
     for (let j = 0; j < wordDict.length; j++) {
-      if (isPostFix(s, i, wordDict[j])) {
-        dp[i] = dp[i] || dp[i - wordDict[j].length];
-        if (!graph[i]) {
-          graph[i] = [];
+      const w = wordDict[j];
+      if (isPostFix(s, i, w)) {
+        const index = i - wordDict[j].length;
+        if (index <= 0 || dp[index].length > 0) {
+          dp[i].push(w);
         }
-        graph[i].push(j);
       }
     }
   }
-  if (!dp[s.length]) {
-    return [];
-  }
-  const output = dfs(s, wordDict, dp, graph, s.length);
-  return output.map((words) => words.reverse()).map((words) => words.join(' '));
+  return dfs(dp, s.length);
 };
-
-export default wordBreak;
