a7cc25ab488c573177e1c5bc42af55c414b9e88f
Bob Wei <bob.wei@linecorp.com>
2019-01-16 12:16:24 +0800

    Update word-break

--- src/leetcode/word-break/index.js
+++ src/leetcode/word-break/index.js
@@ -4,31 +4,21 @@
  * @return {boolean}
  */
 
-/*
-  f(n) = f(n) || {f(n - wordDict[i].length)} for i in wordDict if postfix matched
-
-  "applepenapple"
-  ["apple", "pen"]
-*/
-
-const isPostFix = (str, n, target) => {
-  if (n < target.length) {
-    return false;
-  }
-  return str.slice(n - target.length, n - target.length + target.length) === target;
+const isPostFix = (s, i, w) => {
+  return s.slice(i - w.length, i) === w;
 };
 
-const wordBreak = function(s, wordDict) {
+var wordBreak = function(s, wordDict) {
   const dp = new Array(s.length + 1).fill(false);
   dp[0] = true;
-  for (let i = 0; i < s.length + 1; i++) {
+  for (let i = 1; i < s.length + 1; i++) {
     for (let j = 0; j < wordDict.length; j++) {
-      if (isPostFix(s, i, wordDict[j])) {
-        dp[i] = dp[i] || dp[i - wordDict[j].length];
+      const w = wordDict[j];
+      if (isPostFix(s, i, w)) {
+        dp[i] = dp[i] || dp[i - w.length];
+        if (dp[i]) break;
       }
     }
   }
   return dp[s.length];
 };
-
-export default wordBreak;
