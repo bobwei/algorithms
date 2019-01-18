305d348c128a9b06720a8fd5ffb109785e5c6f20
Bob Wei <bob.wei@linecorp.com>
2019-01-19 00:26:21 +0800

    Update roman-to-integer

--- src/leetcode/roman-to-integer/index.js
+++ src/leetcode/roman-to-integer/index.js
@@ -2,23 +2,26 @@
  * @param {string} s
  * @return {number}
  */
+
+const map = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
 var romanToInt = function(s) {
-  const map = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  };
   let output = 0;
   for (let i = 0; i < s.length; i++) {
-    if (map[s[i]] < map[s[i + 1]]) {
-      output -= map[s[i]];
-    } else {
-      output += map[s[i]];
-    }
+    output += (() => {
+      if (map[s[i]] >= (map[s[i + 1]] || 0)) {
+        return map[s[i]];
+      }
+      return -1 * map[s[i]];
+    })();
   }
   return output;
 };
