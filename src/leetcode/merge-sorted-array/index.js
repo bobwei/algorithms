622a77e00029b7a7098cb91f777128b0ce603b0d
Bob Wei <bob.wei@linecorp.com>
2019-01-19 00:14:15 +0800

    Update merge-sorted-array

--- src/leetcode/merge-sorted-array/index.js
+++ src/leetcode/merge-sorted-array/index.js
@@ -5,21 +5,16 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-const merge = function(nums1, m, nums2, n) {
-  let i = m + n - 1;
-  let j = m - 1;
-  let k = n - 1;
-  while (i >= 0) {
-    if (nums1[j] >= nums2[k] || k < 0) {
-      nums1[i] = nums1[j];
-      i -= 1;
-      j -= 1;
-    } else if (nums1[j] < nums2[k] || j < 0) {
-      nums1[i] = nums2[k];
-      i -= 1;
-      k -= 1;
+var merge = function(nums1, m, nums2, n) {
+  let p = m - 1;
+  let q = n - 1;
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (q < 0 || nums1[p] >= nums2[q]) {
+      nums1[i] = nums1[p];
+      p -= 1;
+    } else if (p < 0 || nums2[q] >= nums1[p]) {
+      nums1[i] = nums2[q];
+      q -= 1;
     }
   }
 };
-
-export default merge;
