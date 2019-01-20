f7c7bb7d5277a84a6b5678a611d98ff0fb60c455
Bob Wei <bob.wei@linecorp.com>
2019-01-20 20:21:02 +0800

    Update lowest-common-ancestor-of-a-binary-tree

--- src/leetcode/lowest-common-ancestor-of-a-binary-tree/index.js
+++ src/leetcode/lowest-common-ancestor-of-a-binary-tree/index.js
@@ -11,19 +11,15 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+
 var lowestCommonAncestor = function(root, p, q) {
-  if (!root) {
+  if (!root || root.val === p.val || root.val === q.val) {
     return root;
   }
-  if (root.val === p.val || root.val === q.val) {
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  if (left && right) {
     return root;
   }
-  const leftLCA = lowestCommonAncestor(root.left, p, q);
-  const rightLCA = lowestCommonAncestor(root.right, p, q);
-  if (leftLCA && rightLCA) {
-    return root;
-  }
-  if (leftLCA || rightLCA) {
-    return leftLCA || rightLCA;
-  }
+  return left || right;
 };
