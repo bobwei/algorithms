8e7d2b38939ff93b793686aecaa1e1c884d4dd54
Bob Wei <bob.wei@linecorp.com>
2019-01-23 23:57:05 +0800

    Update word-search

--- src/leetcode/word-search/index.js
+++ src/leetcode/word-search/index.js
@@ -4,39 +4,36 @@
  * @return {boolean}
  */
 
-const dfs = (board, i, j, word, index, selected) => {
+const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+
+const dfs = (board, word, i, j, m, n, visited, index) => {
   if (index >= word.length) {
     return true;
   }
-  if (i < 0 || j < 0 || i >= board.length || j >= board[0].length) {
+  if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || board[i][j] !== word[index]) {
     return false;
   }
-  if (selected[i][j]) {
-    return false;
+  for (const [di, dj] of dirs) {
+    visited[i][j] = true;
+    if (dfs(board, word, i + di, j + dj, m, n, visited, index + 1)) {
+      return true;
+    }
+    visited[i][j] = false;
   }
-  if (board[i][j] !== word[index]) {
-    return false;
-  }
-  selected[i][j] = true;
-  if (
-    dfs(board, i - 1, j, word, index + 1, selected) ||
-    dfs(board, i, j + 1, word, index + 1, selected) ||
-    dfs(board, i + 1, j, word, index + 1, selected) ||
-    dfs(board, i, j - 1, word, index + 1, selected)
-  ) {
-    return true;
-  }
-  selected[i][j] = false;
+  return false;
 };
 
 var exist = function(board, word) {
-  const selected = [...new Array(board.length)].map(() => new Array(board[0].length).fill(false));
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[0].length; j++) {
-      if (board[i][j] === word[0]) {
-        if (dfs(board, i, j, word, 0, selected)) {
-          return true;
-        }
+  if (!board.length || !board[0].length) {
+    return false;
+  }
+  const m = board.length;
+  const n = board[0].length;
+  const visited = [...new Array(m)].map(() => new Array(n).fill(false));
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (dfs(board, word, i, j, m, n, visited, 0)) {
+        return true;
       }
     }
   }
