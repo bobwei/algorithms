7159b08ac347c952d21e72653a484babb34e1304
Bob Wei <bob.wei@linecorp.com>
2019-01-25 12:26:54 +0800

    Update add-binary

--- src/leetcode/add-binary/index.js
+++ src/leetcode/add-binary/index.js
@@ -4,26 +4,24 @@
  * @return {string}
  */
 
-const trimZero = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-      return arr.slice(i);
-    }
-  }
-  return arr.slice(0, 1);
+const toNumArray = (str) => {
+  return str
+    .split('')
+    .reverse()
+    .map((c) => parseInt(c));
 };
 
 var addBinary = function(a, b) {
-  const output = [];
-  let j = a.length - 1;
-  let k = b.length - 1;
-  let carry = 0;
-  for (let i = 0; i < a.length + b.length; i++) {
-    const sum = parseInt(a[j] || 0, 10) + parseInt(b[k] || 0, 10) + carry;
-    output.unshift(sum % 2);
-    carry = Math.floor(sum / 2);
-    j -= 1;
-    k -= 1;
+  const arr1 = toNumArray(a);
+  const arr2 = toNumArray(b);
+  let output = '';
+  let c = 0;
+  let i = 0;
+  while (i < arr1.length || i < arr2.length || c) {
+    const sum = (arr1[i] || 0) + (arr2[i] || 0) + c;
+    output = (sum % 2) + output;
+    c = Math.floor(sum / 2);
+    i += 1;
   }
-  return trimZero(output).join('');
+  return output;
 };
