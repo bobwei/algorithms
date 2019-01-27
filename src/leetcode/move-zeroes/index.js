07999f52cbddd1c89e683c1472d04931110ae307
Bob Wei <bob.wei@linecorp.com>
2019-01-27 12:00:12 +0800

    Update move-zeroes

--- src/leetcode/move-zeroes/index.js
+++ src/leetcode/move-zeroes/index.js
@@ -3,22 +3,12 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 
-/*
-  [1,3,12,0,0]
-*/
-
-const swap = (arr, i, j) => {
-  const tmp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = tmp;
-};
-
-const moveZeroes = function(nums) {
-  let i = 0;
-  for (let j = 0; j < nums.length; j++) {
-    if (nums[j] !== 0) {
-      swap(nums, i, j);
-      i += 1;
+var moveZeroes = function(nums) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      j++;
     }
   }
 };
