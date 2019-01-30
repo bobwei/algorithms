6936a8ae5e52ceea825cd8a4753e44c33bb48e24
Bob Wei <bob.wei@linecorp.com>
2019-01-31 00:40:12 +0800

    Update binary-tree-level-order-traversal

--- src/leetcode/binary-tree-level-order-traversal/index.js
+++ src/leetcode/binary-tree-level-order-traversal/index.js
@@ -13,18 +13,21 @@ var levelOrder = function(root) {
   if (!root) {
     return [];
   }
-  const queue = [[root, 0]];
-  const levels = {};
+  const output = [];
+  let queue = [root];
   while (queue.length) {
-    const [node, lv] = queue.shift();
-    if (!(lv in levels)) levels[lv] = [];
-    levels[lv].push(node.val);
-    if (node.left) {
-      queue.push([node.left, lv + 1]);
+    output.push(queue.map((el) => el.val));
+    let q = [];
+    while (queue.length) {
+      const node = queue.shift();
+      if (node.left) {
+        q.push(node.left);
+      }
+      if (node.right) {
+        q.push(node.right);
+      }
     }
-    if (node.right) {
-      queue.push([node.right, lv + 1]);
-    }
+    queue = q;
   }
-  return Object.values(levels);
+  return output;
 };
