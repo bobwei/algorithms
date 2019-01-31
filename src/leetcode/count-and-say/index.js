0b2778bf37e02c36770141532415e41aba3e201b
Bob Wei <bob.wei@linecorp.com>
2019-01-31 23:22:13 +0800

    Add count-and-say

--- src/leetcode/count-and-say/index.js
+++ src/leetcode/count-and-say/index.js
@@ -3,23 +3,26 @@
  * @return {string}
  */
 
-var countAndSay = function(n) {
-  let output = ['1'];
-  for (let i = 1; i < n; i++) {
-    const nextOutput = [];
-    let c = output[0];
-    let length = 0;
-    for (let j = 0; j < output.length; j++) {
-      if (output[j] !== c) {
-        nextOutput.push(String(length), c);
-        c = output[j];
-        length = 1;
-      } else {
-        length += 1;
-      }
-    }
-    nextOutput.push(String(length), c);
-    output = nextOutput;
+const count = (str, i) => {
+  let n = 0;
+  let j = i;
+  while (str[j] === str[i]) {
+    n += 1;
+    j += 1;
   }
-  return output.join('');
+  return n;
+};
+
+var countAndSay = function(n) {
+  let output = '1';
+  for (let i = 1; i < n; i++) {
+    let next = '';
+    for (let j = 0; j < output.length; j++) {
+      const m = count(output, j);
+      next += m + output[j];
+      j += m - 1;
+    }
+    output = next;
+  }
+  return output;
 };
