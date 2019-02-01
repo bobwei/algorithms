87e49415253865ded22d9d6dca86b134c0661670
Bob Wei <bob.wei@linecorp.com>
2019-02-01 11:35:59 +0800

    Update subsets

--- src/leetcode/subsets/index.js
+++ src/leetcode/subsets/index.js
@@ -3,23 +3,25 @@
  * @return {number[][]}
  */
 
-const helper = (arr, start, length, selected = [], output = []) => {
-  if (selected.length >= length) {
-    output.push([...selected]);
+const dfs = (nums, start, length, selected = [], output = []) => {
+  if (selected.length >= length || start >= nums.length) {
+    if (selected.length === length) {
+      output.push([...selected]);
+    }
     return output;
   }
-  for (let i = start; i < arr.length; i++) {
-    selected.push(arr[i]);
-    helper(arr, i + 1, length, selected, output);
+  for (let i = start; i < nums.length; i++) {
+    selected.push(nums[i]);
+    dfs(nums, i + 1, length, selected, output);
     selected.pop();
   }
   return output;
 };
 
 var subsets = function(nums) {
-  const output = [[]];
-  for (let length = 1; length <= nums.length; length++) {
-    output.push(...helper(nums, 0, length));
+  const output = [];
+  for (let length = 0; length <= nums.length; length++) {
+    output.push(...dfs(nums, 0, length));
   }
   return output;
 };
