0dc8aa2ee42eef5a07d05f3abd3a837a6ce56714
Bob Wei <bob.wei@linecorp.com>
2019-02-06 01:56:36 +0800

    Update read-n-characters-given-read4-ii-call-multiple-times

--- src/leetcode/read-n-characters-given-read4-ii-call-multiple-times/index.js
+++ src/leetcode/read-n-characters-given-read4-ii-call-multiple-times/index.js
@@ -8,46 +8,26 @@
  * };
  */
 
-/*
-  buf = 'abcdefghijklx'
-  read(3)
-  read(5)
-  read(1)
-
-*/
-
 /**
  * @param {function} read4()
  * @return {function}
  */
-const MAX_N_READ = 4;
-
-const flush = (src, dst, n) => {
-  dst.push(...src.slice(0, n));
-  return src.slice(n);
-};
-
 var solution = function(read4) {
   /**
    * @param {character[]} buf Destination buffer
-   * @param {number} n Maximum number of characters to read
-   * @return {number} The number of characters read
+   * @param {number} n Number of characters to read
+   * @return {number} The number of actual characters read
    */
-  let tmp = [];
+  const arr = [];
   return function(buf, n) {
-    let total = 0;
-    let nFlush = 0;
-    let isEnded = false;
-    nFlush = Math.min(tmp.length, n - total);
-    tmp = flush(tmp, buf, nFlush);
-    total += nFlush;
-    while (total < n && !isEnded) {
+    let isEof = false;
+    while (arr.length < n && !isEof) {
+      const tmp = [];
       const nRead = read4(tmp);
-      nFlush = Math.min(nRead, n - total);
-      tmp = flush(tmp, buf, nFlush);
-      total += nFlush;
-      isEnded = nRead < MAX_N_READ;
+      arr.push(...tmp);
+      isEof = nRead < 4;
     }
-    return total;
+    buf.push(...arr.slice(0, n));
+    arr.splice(0, n);
   };
 };
