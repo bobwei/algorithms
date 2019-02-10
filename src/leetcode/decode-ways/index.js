1a948db78a2131def9e2c1d2712360e93911f66c
Bob Wei <bob.wei@linecorp.com>
2019-02-10 19:41:39 +0800

    Update decode-ways

--- src/leetcode/decode-ways/index.js
+++ src/leetcode/decode-ways/index.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var numDecodings = function(s) {
-  let output = parseInt(s[0]) > 0 ? 1 : 0;
+  const n = s.length;
   let x = 1;
-  let y = output;
-  for (let i = 1; i < s.length; i++) {
-    const d1 = parseInt(s.slice(i, i + 1));
-    const d2 = parseInt(s.slice(i - 1, i + 1));
+  let y = parseInt(s[0]) > 0 ? 1 : 0;
+  let output = y;
+  for (let i = 2; i <= n; i++) {
+    const d1 = parseInt(s.substring(i - 1, i));
+    const d2 = parseInt(s.substring(i - 2, i));
     output = 0;
     if (d2 >= 10 && d2 <= 26) {
       output += x;
