dad7203e40020f109ce5e6d53dbd32fbf600004b
Bob Wei <bob.wei@linecorp.com>
2019-02-21 23:32:27 +0800

    Update regular-expression-matching

--- src/leetcode/regular-expression-matching/index.js
+++ src/leetcode/regular-expression-matching/index.js
@@ -3,22 +3,21 @@
  * @param {string} p
  * @return {boolean}
  */
-
 var isMatch = function(s, p) {
   const m = s.length;
   const n = p.length;
   const dp = [...new Array(m + 1)].map(() => new Array(n + 1).fill(false));
   dp[0][0] = true;
-  for (let j = 2; j <= n; j++) {
+  for (let j = 1; j <= n; j++) {
     dp[0][j] = p[j - 1] === '*' && dp[0][j - 2];
   }
   for (let i = 1; i <= m; i++) {
     for (let j = 1; j <= n; j++) {
       dp[i][j] = (() => {
         if (p[j - 1] === '*') {
-          return ((p[j - 2] === s[i - 1] || p[j - 2] === '.') && dp[i - 1][j]) || dp[i][j - 2];
+          return ((s[i - 1] === p[j - 2] || p[j - 2] === '.') && dp[i - 1][j]) || dp[i][j - 2];
         }
-        return (p[j - 1] === s[i - 1] || p[j - 1] === '.') && dp[i - 1][j - 1];
+        return (s[i - 1] === p[j - 1] || p[j - 1] === '.') && dp[i - 1][j - 1];
       })();
     }
   }
