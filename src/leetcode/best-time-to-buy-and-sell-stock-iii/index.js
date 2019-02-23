25130724bc0d38b6d87507a9fc46662815bf6559
Bob Wei <bob.wei@linecorp.com>
2019-02-24 01:31:27 +0800

    Update best-time-to-buy-and-sell-stock-iii

--- src/leetcode/best-time-to-buy-and-sell-stock-iii/index.js
+++ src/leetcode/best-time-to-buy-and-sell-stock-iii/index.js
@@ -3,28 +3,22 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  if (!prices.length) {
-    return 0;
-  }
   const n = prices.length;
   const left = new Array(n).fill(0);
-  let min = prices[0];
-  for (let i = 0; i < n; i++) {
-    min = Math.min(min, prices[i]);
-    const p = prices[i] > min ? prices[i] - min : 0;
-    left[i] = Math.max(left[i - 1] || 0, p);
-  }
   const right = new Array(n).fill(0);
+  let min = prices[0];
+  for (let i = 1; i < n; i++) {
+    min = Math.min(min, prices[i]);
+    left[i] = Math.max(left[i - 1], prices[i] - min);
+  }
   let max = prices[n - 1];
-  for (let i = n - 1; i >= 0; i--) {
+  for (let i = n - 2; i >= 0; i--) {
     max = Math.max(max, prices[i]);
-    const p = prices[i] < max ? max - prices[i] : 0;
-    right[i] = Math.max(right[i + 1] || 0, p);
+    right[i] = Math.max(right[i + 1], max - prices[i]);
   }
-  let output = 0;
+  let p = 0;
   for (let i = 0; i < n; i++) {
-    const p = left[i] + right[i];
-    output = Math.max(output, p);
+    p = Math.max(p, left[i] + right[i]);
   }
-  return output;
+  return p;
 };
