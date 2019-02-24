237a2d4e8c592861edda000f918bf5dede5acab0
Bob Wei <bob.wei@linecorp.com>
2019-02-24 23:41:49 +0800

    Update best-time-to-buy-and-sell-stock-iv

--- src/leetcode/best-time-to-buy-and-sell-stock-iv/index.js
+++ src/leetcode/best-time-to-buy-and-sell-stock-iv/index.js
@@ -3,8 +3,37 @@
  * @param {number[]} prices
  * @return {number}
  */
+var maxProfit = function(kTimes, prices) {
+  if (!kTimes || !prices.length) {
+    return 0;
+  }
+  const n = prices.length;
+  if (kTimes >= n / 2) {
+    return greedy(prices);
+  }
+  return dpFn(kTimes, prices);
+};
 
-const greedy = (prices) => {
+function dpFn(kTimes, prices) {
+  const n = prices.length;
+  const dp = new Array(n).fill(0);
+  let min = prices[0];
+  for (let i = 1; i < n; i++) {
+    min = Math.min(min, prices[i]);
+    dp[i] = Math.max(dp[i - 1], prices[i] - min);
+  }
+  for (let k = 2; k <= kTimes; k++) {
+    let maxDiff = dp[0] - prices[0];
+    for (let i = 1; i < n; i++) {
+      const diff = dp[i] - prices[i];
+      dp[i] = Math.max(dp[i - 1], maxDiff + prices[i]);
+      maxDiff = Math.max(maxDiff, diff);
+    }
+  }
+  return dp[n - 1];
+}
+
+function greedy(prices) {
   let output = 0;
   for (let i = 1; i < prices.length; i++) {
     if (prices[i] > prices[i - 1]) {
@@ -12,34 +41,4 @@ const greedy = (prices) => {
     }
   }
   return output;
-};
-
-var maxProfit = function(kTimes, prices) {
-  if (!kTimes || !prices.length) {
-    return 0;
-  }
-
-  const n = prices.length;
-  if (kTimes >= n / 2) {
-    return greedy(prices);
-  }
-
-  const dp = new Array(n).fill(0);
-
-  let min = Infinity;
-  for (let j = 0; j < n; j++) {
-    min = Math.min(min, prices[j]);
-    dp[j] = Math.max(dp[j - 1] || 0, prices[j] - min);
-  }
-
-  for (let k = 2; k <= kTimes; k++) {
-    let maxDiff = dp[0] - prices[0];
-    for (let i = 1; i < n; i++) {
-      const newDiff = dp[i] - prices[i];
-      dp[i] = Math.max(dp[i - 1], maxDiff + prices[i]);
-      maxDiff = Math.max(maxDiff, newDiff);
-    }
-  }
-
-  return dp[n - 1];
-};
+}
