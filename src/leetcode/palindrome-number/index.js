2b37bea960f58474b514d83c5cf27700ab36628d
Bob Wei <bob.wei@linecorp.com>
2019-02-26 01:09:22 +0800

    Update palindrome-number

--- src/leetcode/palindrome-number/index.js
+++ src/leetcode/palindrome-number/index.js
@@ -3,14 +3,12 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-  if (x < 0) {
-    return false;
+  if (x < 0) return false;
+  let y = x;
+  let n = 0;
+  while (y) {
+    n = n * 10 + (y % 10);
+    y = Math.floor(y / 10);
   }
-  let n = x;
-  let reversedX = 0;
-  while (n > 0) {
-    reversedX = 10 * reversedX + (n % 10);
-    n = Math.floor(n / 10);
-  }
-  return reversedX === x;
+  return n === x;
 };
