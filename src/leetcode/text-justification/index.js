2868d91ad0a14f55ded1645cc46c5d5698e77df2
Bob Wei <bob.wei@linecorp.com>
2019-03-04 15:45:15 +0800

    Update text-justification

--- src/leetcode/text-justification/index.js
+++ src/leetcode/text-justification/index.js
@@ -4,32 +4,26 @@
  * @return {string[]}
  */
 var fullJustify = function(words, maxWidth) {
-  if (!words.length) {
-    return [' '.repeat(maxWidth)];
-  }
-  // prettier-ignore
-  return split(words, maxWidth)
-    .map((row, i, arr) => {
-      if (i < arr.length - 1) {
-        return centerJustify(row, maxWidth);
-      }
+  return split(words, maxWidth).map((row, i, arr) => {
+    if (i >= arr.length - 1) {
       return leftJustify(row, maxWidth);
-    });
+    }
+    return centerJustify(row, maxWidth);
+  });
 };
 
 function split(words, maxWidth) {
   const output = [];
   let start = 0;
-  let n = 0;
+  let nChars = 0;
   for (let i = 0; i < words.length; i++) {
-    // length of characters + length of minimum number of spaces
-    const rowLength = n + words[i].length + (i - start);
+    const rowLength = nChars + words[i].length + i - start;
     if (rowLength > maxWidth) {
       output.push(words.slice(start, i));
       start = i;
-      n = 0;
+      nChars = 0;
     }
-    n += words[i].length;
+    nChars += words[i].length;
   }
   if (start < words.length) {
     output.push(words.slice(start));
@@ -37,30 +31,28 @@ function split(words, maxWidth) {
   return output;
 }
 
-function centerJustify(strs, maxWidth) {
-  const nChars = strs.reduce((acc, cur) => acc + cur.length, 0);
-  const nTotalSpaces = Math.max(1, strs.length - 1);
-  let spaceLength = 0;
-  while (nChars + (spaceLength + 1) * nTotalSpaces <= maxWidth) {
-    spaceLength += 1;
-  }
-  const nExtraSpaces = maxWidth - (nChars + spaceLength * nTotalSpaces);
+function centerJustify(row, maxWidth) {
+  const nChars = row.reduce((acc, cur) => acc + cur.length, 0);
+  const nTotalSpaces = Math.max(1, row.length - 1);
+  const spaceLength = Math.floor((maxWidth - nChars) / nTotalSpaces);
+  const nExtraSpaces = (maxWidth - nChars) % nTotalSpaces;
   const nSpaces = nTotalSpaces - nExtraSpaces;
-  let output = strs[0];
+  let output = row[0];
   let i = 1;
   for (let j = 0; j < nExtraSpaces; j++) {
-    output += ' '.repeat(spaceLength + 1) + (strs[i] || '');
+    output += ' '.repeat(spaceLength + 1) + (row[i] || '');
     i += 1;
   }
   for (let j = 0; j < nSpaces; j++) {
-    output += ' '.repeat(spaceLength) + (strs[i] || '');
+    output += ' '.repeat(spaceLength) + (row[i] || '');
     i += 1;
   }
   return output;
 }
 
-function leftJustify(strs, maxWidth) {
-  const nChars = strs.reduce((acc, cur) => acc + cur.length, 0);
-  const nExtraSpaces = maxWidth - (nChars + 1 * (strs.length - 1));
-  return strs.join(' ') + ' '.repeat(nExtraSpaces);
+function leftJustify(row, maxWidth) {
+  const nChars = row.reduce((acc, cur) => acc + cur.length, 0);
+  const nTotalSpaces = row.length - 1;
+  const nSpaces = maxWidth - (nChars + nTotalSpaces);
+  return row.join(' ') + ' '.repeat(nSpaces);
 }
