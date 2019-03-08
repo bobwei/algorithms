a4887dee64223d1056198ed0987423c9b8a23573
Bob Wei <bob.wei@linecorp.com>
2019-03-08 13:03:22 +0800

    Update insert-interval

--- src/leetcode/insert-interval/index.js
+++ src/leetcode/insert-interval/index.js
@@ -18,17 +18,23 @@ var insert = function(intervals, newInterval) {
   const output = [];
   let i2 = newInterval;
   for (const i1 of intervals) {
-    const isMergeNeeded = i1.end >= i2.start && i1.start <= i2.end;
+    const isMergeNeeded = i1.end >= i2.start && i2.end >= i1.start;
     if (isMergeNeeded) {
-      const tmp = new Interval();
-      tmp.start = Math.min(i1.start, i2.start);
-      tmp.end = Math.max(i1.end, i2.end);
-      i2 = tmp;
+      i2 = merge(i1, i2);
     } else {
-      output.push(i1.start < i2.start ? i1 : i2);
-      i2 = i1.start > i2.start ? i1 : i2;
+      const min = i1.start < i2.start ? i1 : i2;
+      const max = i1.start >= i2.start ? i1 : i2;
+      output.push(min);
+      i2 = max;
     }
   }
   output.push(i2);
   return output;
 };
+
+function merge(i1, i2) {
+  const tmp = new Interval();
+  tmp.start = Math.min(i1.start, i2.start);
+  tmp.end = Math.max(i1.end, i2.end);
+  return tmp;
+}
