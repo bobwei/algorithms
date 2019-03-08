5555602d55f70434fe2e956404581a0d35382346
Bob Wei <bob.wei@linecorp.com>
2019-03-08 12:48:25 +0800

    Update longest-substring-without-repeating-characters

--- src/leetcode/longest-substring-without-repeating-characters/index.js
+++ src/leetcode/longest-substring-without-repeating-characters/index.js
@@ -2,27 +2,17 @@
  * @param {string} s
  * @return {number}
  */
-/*
-  pwxyzwkew
-*/
 var lengthOfLongestSubstring = function(s) {
-  const cache = {};
-  let length = 0;
+  const visited = new Set();
+  let start = 0;
   let max = 0;
-  let left = 0;
   for (let i = 0; i < s.length; i++) {
-    if (!cache[s[i]]) {
-      cache[s[i]] = true;
-      length += 1;
-      max = Math.max(max, length);
-      continue;
+    while (visited.has(s[i])) {
+      visited.delete(s[start]);
+      start += 1;
     }
-    while (s[left] !== s[i]) {
-      delete cache[s[left]];
-      left += 1;
-      length -= 1;
-    }
-    left += 1;
+    visited.add(s[i]);
+    max = Math.max(max, i - start + 1);
   }
   return max;
 };
