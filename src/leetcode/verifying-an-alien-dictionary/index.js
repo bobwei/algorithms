79de80cbda3a3b5b5376d857fedfe99f9cbd795f
Bob Wei <bob.wei@linecorp.com>
2019-03-13 15:40:04 +0800

    Update verifying-an-alien-dictionary

--- src/leetcode/verifying-an-alien-dictionary/index.js
+++ src/leetcode/verifying-an-alien-dictionary/index.js
@@ -4,21 +4,20 @@
  * @return {boolean}
  */
 var isAlienSorted = function(words, order) {
-  const indexes = {};
+  const map = {};
   for (let i = 0; i < order.length; i++) {
-    indexes[order[i]] = i;
+    map[order[i]] = i;
   }
   for (let i = 0; i < words.length - 1; i++) {
-    const n = Math.min(words[i].length, words[i + 1].length);
     let j = 0;
     while (words[i][j] === words[i + 1][j]) {
       j += 1;
     }
     const c1 = words[i][j];
     const c2 = words[i + 1][j];
-    if (!c1) break;
-    if (!c2) return false;
-    if (indexes[c1] > indexes[c2]) return false;
+    if (!(map[c1] <= map[c2])) {
+      return false;
+    }
   }
   return true;
 };
