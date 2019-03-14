ca5e53830ffaecafa11ed6b014aacf3a8d2e4097
Bob Wei <bob.wei@linecorp.com>
2019-03-14 18:26:51 +0800

    Update strobogrammatic-number-ii

--- src/leetcode/strobogrammatic-number-ii/index.js
+++ src/leetcode/strobogrammatic-number-ii/index.js
@@ -6,43 +6,38 @@
 const dp1 = ['0', '1', '8'];
 const dp2 = ['00', '11', '69', '88', '96'];
 
-const createNumbers = (arr, n) => {
-  while (arr[0].length < n) {
-    const c = arr.shift();
-    arr.push('0' + c + '0');
-    arr.push('1' + c + '1');
-    arr.push('6' + c + '9');
-    arr.push('8' + c + '8');
-    arr.push('9' + c + '6');
-  }
-  return arr;
-};
-
-const isValid = (s) => {
-  if (s.length <= 1) {
-    return true;
-  }
-  return s[0] !== '0';
-};
-
-const getValidNumbers = (arr) => {
-  return arr.filter(isValid);
-};
-
 var findStrobogrammatic = function(n) {
-  if (n == 1) {
-    return getValidNumbers(dp1);
+  if (n === 1) {
+    return dp1.filter(isValid);
   }
   if (n === 2) {
-    return getValidNumbers(dp2);
+    return dp2.filter(isValid);
   }
   let x = dp1;
   let y = dp2;
-  let output;
   for (let i = 3; i <= n; i++) {
-    output = createNumbers(x, i);
+    const arr = createNumbers(x);
     x = y;
-    y = output;
+    y = arr;
   }
-  return getValidNumbers(output);
+  return y.filter(isValid);
 };
+
+function createNumbers(arr) {
+  const output = [];
+  for (const num of arr) {
+    output.push('0' + num + '0');
+    output.push('1' + num + '1');
+    output.push('6' + num + '9');
+    output.push('8' + num + '8');
+    output.push('9' + num + '6');
+  }
+  return output;
+}
+
+function isValid(num) {
+  if (num[0] === '0') {
+    return num.length <= 1;
+  }
+  return true;
+}
