669bafbd64c60a8ae3c917ab35c36066afe3d44e
Bob Wei <bob.wei@linecorp.com>
2019-03-14 16:52:14 +0800

    Update maximum-sum-of-3-non-overlapping-subarrays

--- src/leetcode/maximum-sum-of-3-non-overlapping-subarrays/index.js
+++ src/leetcode/maximum-sum-of-3-non-overlapping-subarrays/index.js
@@ -4,44 +4,33 @@
  * @return {number[]}
  */
 var maxSumOfThreeSubarrays = function(nums, k) {
-  const n = nums.length;
-  const sums = new Array(n).fill(0);
-  for (let i = n - 1; i >= 0; i--) {
-    sums[i] = nums[i];
-    if (i + 1 < n) {
-      sums[i] += sums[i + 1];
-    }
-    if (i + k < n) {
-      sums[i] -= nums[i + k];
-    }
+  const m = nums.length;
+
+  const sums = new Array(m).fill(0);
+  sums[0] = nums.slice(0, k).reduce((acc, cur) => acc + cur, 0);
+  for (let i = 1; i <= m - k; i++) {
+    sums[i] = sums[i - 1] + nums[i + k - 1] - nums[i - 1];
   }
-  const left = new Array(n).fill(null);
+
+  const left = new Array(m).fill(0);
   left[k - 1] = 0;
-  for (let i = k - 1 + 1; i < n; i++) {
-    if (sums[i - k + 1] > sums[left[i - 1]]) {
-      left[i] = i - k + 1;
-    } else {
-      left[i] = left[i - 1];
-    }
+  for (let i = k; i < m; i++) {
+    left[i] = sums[i - k + 1] > sums[left[i - 1]] ? i - k + 1 : left[i - 1];
   }
-  const right = new Array(n).fill(null);
-  right[n - k] = n - k;
-  for (let i = n - k - 1; i >= 0; i--) {
-    if (sums[i] > sums[right[i + 1]]) {
-      right[i] = i;
-    } else {
-      right[i] = right[i + 1];
-    }
+
+  const right = new Array(m).fill(0);
+  right[m - k] = m - k;
+  for (let i = m - k - 1; i >= 0; i--) {
+    right[i] = sums[i] > sums[right[i + 1]] ? i : right[i + 1];
   }
-  const output = new Array(3).fill(null);
-  let max = -Infinity;
-  for (let i = k; i <= n - 2 * k; i++) {
+
+  let output;
+  let max = 0;
+  for (let i = k; i <= m - 2 * k; i++) {
     const sum = sums[left[i - 1]] + sums[i] + sums[right[i + k]];
     if (sum > max) {
       max = sum;
-      output[0] = left[i - 1];
-      output[1] = i;
-      output[2] = right[i + k];
+      output = [left[i - 1], i, right[i + k]];
     }
   }
   return output;
