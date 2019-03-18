d8d54234af18abc31f700b9062fd13014b270292
Bob Wei <bob.wei@linecorp.com>
2019-03-18 16:20:37 +0800

    Update search-in-rotated-sorted-array-ii

--- src/leetcode/search-in-rotated-sorted-array-ii/index.js
+++ src/leetcode/search-in-rotated-sorted-array-ii/index.js
@@ -1,32 +1,26 @@
 /**
  * @param {number[]} nums
  * @param {number} target
- * @return {boolean}
+ * @return {number}
  */
-
 var search = function(nums, target) {
   let left = 0;
   let right = nums.length - 1;
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) {
+    if (target === nums[mid]) {
       return true;
-    } else if (nums[mid] > nums[left]) {
-      if (target >= nums[left] && target <= nums[mid]) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
-    } else if (nums[mid] < nums[right]) {
-      if (target >= nums[mid] && target <= nums[right]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    } else if (nums[mid] === nums[left]) {
-      left += 1;
     } else if (nums[mid] === nums[right]) {
       right -= 1;
+    } else if (nums[mid] === nums[left]) {
+      left += 1;
+    } else if (
+      (nums[left] < nums[mid] && target >= nums[left] && target <= nums[mid]) ||
+      (nums[mid] < nums[right] && !(target >= nums[mid] && target <= nums[right]))
+    ) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
     }
   }
   return false;
