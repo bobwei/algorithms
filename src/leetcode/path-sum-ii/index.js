c6182925089b6058c52e376481cbfef6ce53bced
Bob Wei <bob.wei@linecorp.com>
2019-03-24 23:56:23 +0800

    Update path-sum-ii

--- src/leetcode/path-sum-ii/index.js
+++ src/leetcode/path-sum-ii/index.js
@@ -10,19 +10,19 @@
  * @param {number} sum
  * @return {number[][]}
  */
-var pathSum = function(root, sum, selected = [], currentSum = 0, output = []) {
+var pathSum = function(root, sum, curSum = 0, selected = [], output = []) {
   if (!root) {
     return output;
   }
   if (root.left || root.right) {
+    const nextSum = curSum + root.val;
     selected.push(root.val);
-    const nextSum = currentSum + root.val;
-    pathSum(root.left, sum, selected, nextSum, output);
-    pathSum(root.right, sum, selected, nextSum, output);
+    pathSum(root.left, sum, nextSum, selected, output);
+    pathSum(root.right, sum, nextSum, selected, output);
     selected.pop();
     return output;
   }
-  if (currentSum + root.val === sum) {
+  if (curSum + root.val === sum) {
     output.push([...selected, root.val]);
   }
   return output;
