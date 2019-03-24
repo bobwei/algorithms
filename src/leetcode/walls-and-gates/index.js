51c20e50841fae74eee10306adcad6177837b669
Bob Wei <bob.wei@linecorp.com>
2019-03-25 00:30:00 +0800

    Update walls-and-gates

--- src/leetcode/walls-and-gates/index.js
+++ src/leetcode/walls-and-gates/index.js
@@ -2,39 +2,41 @@
  * @param {number[][]} rooms
  * @return {void} Do not return anything, modify rooms in-place instead.
  */
-
-const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-
-const INF = 2 ** 31 - 1;
-
-const isValidIJ = (i, j, m, n) => {
-  if (i < 0 || i >= m || j < 0 || j >= n) {
-    return false;
-  }
-  return true;
-};
-
 var wallsAndGates = function(rooms) {
   if (!rooms.length || !rooms[0].length) {
     return;
   }
   const m = rooms.length;
   const n = rooms[0].length;
-  const queue = [];
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (rooms[i][j] === 0) {
-        queue.push([[i, j], 0]);
-      }
-    }
-  }
-  while (queue.length) {
-    const [[i, j], d] = queue.shift();
-    rooms[i][j] = Math.min(rooms[i][j], d);
-    for (const [di, dj] of dirs) {
-      if (isValidIJ(i + di, j + dj, m, n) && rooms[i + di][j + dj] >= INF) {
-        queue.push([[i + di, j + dj], d + 1]);
+        bfs(rooms, { i, j }, m, n);
       }
     }
   }
 };
+
+const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+
+function bfs(rooms, position, m, n) {
+  const queue = [[position, 0]];
+  while (queue.length) {
+    const [p, d] = queue.shift();
+    for (const [di, dj] of dirs) {
+      const i = p.i + di;
+      const j = p.j + dj;
+      if (isValid(i, j, m, n) && d + 1 < rooms[i][j]) {
+        rooms[i][j] = d + 1;
+        queue.push([{ i, j }, d + 1]);
+      }
+    }
+  }
+}
+
+function isValid(i, j, m, n) {
+  if (i < 0 || i >= m || j < 0 || j >= n) {
+    return false;
+  }
+  return true;
+}
