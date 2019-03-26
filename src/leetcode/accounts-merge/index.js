24127df7a2f0772e362d852b50b06bd5200cc061
Bob Wei <bob.wei@linecorp.com>
2019-03-26 22:39:01 +0800

    Update accounts-merge

--- src/leetcode/accounts-merge/index.js
+++ src/leetcode/accounts-merge/index.js
@@ -2,45 +2,47 @@
  * @param {string[][]} accounts
  * @return {string[][]}
  */
-
-const find = (roots, v) => {
-  let ptr = v;
-  while (roots[ptr] !== ptr) {
-    ptr = roots[ptr];
-  }
-  return ptr;
-};
-
-const union = (roots, r1, r2) => {
-  roots[r2] = r1;
-};
-
 var accountsMerge = function(accounts) {
-  const roots = {};
-  const owners = {};
-  for (let i = 0; i < accounts.length; i++) {
-    for (let j = 1; j < accounts[i].length; j++) {
-      roots[accounts[i][j]] = accounts[i][j];
-      owners[accounts[i][j]] = accounts[i][0];
+  const map = {};
+  const set = new DisjointSet();
+  for (const account of accounts) {
+    const [name, first, ...emails] = account;
+    map[first] = name;
+    set.find(first);
+    for (const email of emails) {
+      map[email] = name;
+      set.union(set.find(first), set.find(email));
     }
   }
-  for (let i = 0; i < accounts.length; i++) {
-    for (let j = 2; j < accounts[i].length; j++) {
-      const r1 = find(roots, accounts[i][1]);
-      const rj = find(roots, accounts[i][j]);
-      union(roots, r1, rj);
-    }
+  const groups = {};
+  for (const email in set.roots) {
+    const root = set.find(set.roots[email]);
+    if (!(root in groups)) groups[root] = [];
+    groups[root].push(email);
   }
-  const results = {};
-  for (let i = 0; i < accounts.length; i++) {
-    for (let j = 1; j < accounts[i].length; j++) {
-      const rj = find(roots, accounts[i][j]);
-      const owner = owners[rj];
-      if (!results[rj]) {
-        results[rj] = new Set();
-      }
-      results[rj].add(accounts[i][j]);
-    }
-  }
-  return Object.keys(results).map((email) => [owners[email], ...[...results[email]].sort()]);
+  // prettier-ignore
+  const output = Object
+    .entries(groups)
+    .map(([key, value]) => [map[key], ...value.sort()]);
+  return output;
 };
+
+class DisjointSet {
+  constructor() {
+    this.roots = {};
+  }
+
+  find(r) {
+    if (!(r in this.roots)) this.roots[r] = r;
+    let ptr = r;
+    while (this.roots[ptr] !== ptr) {
+      this.roots[ptr] = this.roots[this.roots[ptr]];
+      ptr = this.roots[ptr];
+    }
+    return ptr;
+  }
+
+  union(r1, r2) {
+    this.roots[r2] = r1;
+  }
+}
