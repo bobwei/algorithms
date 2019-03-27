4961c1441202a1f8619abd8a0f5198f870f8bf4f
Bob Wei <bob.wei@linecorp.com>
2019-03-27 22:50:31 +0800

    Add fruit-into-baskets

--- src/leetcode/fruit-into-baskets/index.js
+++ src/leetcode/fruit-into-baskets/index.js
@@ -3,28 +3,38 @@
  * @return {number}
  */
 var totalFruit = function(tree) {
-  const counts = {};
-  let nUniques = 0;
-  let nTotal = 0;
-  let left = 0;
-  let max = 0;
+  const counter = new Counter();
+  let start = 0;
+  let max = -Infinity;
   for (let i = 0; i < tree.length; i++) {
-    if (!counts[tree[i]]) {
-      nUniques += 1;
+    const c = tree[i];
+    counter.add(c);
+    while (counter.n > 2) {
+      counter.delete(tree[start]);
+      start += 1;
     }
-    counts[tree[i]] = (counts[tree[i]] || 0) + 1;
-    nTotal += 1;
-    if (nUniques <= 2) {
-      max = Math.max(max, nTotal);
-    }
-    while (nUniques > 2) {
-      counts[tree[left]] -= 1;
-      nTotal -= 1;
-      if (!counts[tree[left]]) {
-        nUniques -= 1;
-      }
-      left += 1;
-    }
+    max = Math.max(max, i - start + 1);
   }
   return max;
 };
+
+class Counter {
+  constructor() {
+    this.counts = {};
+    this.n = 0;
+  }
+
+  add(c) {
+    this.counts[c] = (this.counts[c] || 0) + 1;
+    if (this.counts[c] === 1) {
+      this.n += 1;
+    }
+  }
+
+  delete(c) {
+    this.counts[c] -= 1;
+    if (this.counts[c] < 1) {
+      this.n -= 1;
+    }
+  }
+}
