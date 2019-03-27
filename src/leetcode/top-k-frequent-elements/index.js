a8317454ac6cacfc536e8b67fab5f889814f63ba
Bob Wei <bob.wei@linecorp.com>
2019-03-27 22:29:30 +0800

    Update top-k-frequent-elements

--- src/leetcode/top-k-frequent-elements/index.js
+++ src/leetcode/top-k-frequent-elements/index.js
@@ -3,71 +3,26 @@
  * @param {number} k
  * @return {number[]}
  */
-
-/*
-  Min heap
-*/
-
-const swap = (arr, i, j) => {
-  const tmp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = tmp;
-};
-
-const bubbleUp = (arr, i) => {
-  if (i <= 0) {
-    return;
-  }
-  const p = Math.floor((i - 1) / 2);
-  const isValidHeap = arr[i].key >= arr[p].key;
-  if (isValidHeap) {
-    return;
-  }
-  swap(arr, p, i);
-  bubbleUp(arr, p);
-};
-
-const bubbleDown = (arr, i) => {
-  const left = 2 * i + 1;
-  const right = 2 * i + 2;
-  const isValidHeap =
-    (left >= arr.length || arr[i].key <= arr[left].key) &&
-    (right >= arr.length || arr[i].key <= arr[right].key);
-  if (isValidHeap) {
-    return;
-  }
-  const next =
-    right >= arr.length || arr[left].key < arr[right].key ? left : right;
-  swap(arr, i, next);
-  bubbleDown(arr, next);
-};
-
-const extractTop = (arr) => {
-  if (arr.length <= 1) {
-    return arr.pop();
-  }
-  arr[0] = arr.pop();
-  bubbleDown(arr, 0);
-};
-
-const topKFrequent = function(nums, k) {
-  const counts = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    counts.set(nums[i], (counts.get(nums[i]) || 0) + 1);
-  }
-  const topK = new Array(k).fill({ key: -Infinity, value: null });
-  for (const [element, count] of counts.entries()) {
-    const key = count;
-    const value = element;
-    if (key > topK[0].key) {
-      topK.push({ key, value });
-      bubbleUp(topK, topK.length - 1);
-    }
-    if (topK.length > k) {
-      extractTop(topK);
+var topKFrequent = function(nums, k) {
+  const freq = nums.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc;
+  }, {});
+  const bucket = Object.entries(freq).reduce((acc, [char, count]) => {
+    if (!(count in acc)) acc[count] = [];
+    acc[count].push(char);
+    return acc;
+  }, {});
+  const output = [];
+  let j = k;
+  for (let i = nums.length; i >= 0; i--) {
+    if (i in bucket) {
+      output.push(...bucket[i]);
+      j -= bucket[i].length;
+      if (j <= 0) {
+        return output;
+      }
     }
   }
-  return topK.map(({ value }) => value).reverse();
+  return output;
 };
-
-export default topKFrequent;
