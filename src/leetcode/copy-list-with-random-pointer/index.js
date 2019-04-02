fa98c539d5c0803ddd5b7cfb1a8f183b0b127055
Bob Wei <bob.wei@linecorp.com>
2019-04-03 00:22:10 +0800

    Update copy-list-with-random-pointer

--- src/leetcode/copy-list-with-random-pointer/index.js
+++ src/leetcode/copy-list-with-random-pointer/index.js
@@ -1,33 +1,41 @@
+/* global Node */
 /**
- * Definition for singly-linked list with a random pointer.
- * function RandomListNode(label) {
- *     this.label = label;
- *     this.next = this.random = null;
- * }
+ * // Definition for a Node.
+ * function Node(val,next,random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+/**
+ * @param {Node} head
+ * @return {Node}
  */
 
-/**
- * @param {RandomListNode} head
- * @return {RandomListNode}
- */
 var copyRandomList = function(head) {
-  const cache = {};
-  const randomPtrs = [];
-  const dummy = new RandomListNode();
-  let ptr = dummy;
-  let hptr = head;
-  while (hptr) {
-    if (hptr.random) {
-      randomPtrs.push([hptr.label, hptr.random.label]);
-    }
-    ptr.next = new RandomListNode(hptr.label);
-    ptr = ptr.next;
-    cache[ptr.label] = ptr;
-    hptr = hptr.next;
+  if (!head) {
+    return head;
   }
-  for (let i = 0; i < randomPtrs.length; i++) {
-    const [from, to] = randomPtrs[i];
-    cache[from].random = cache[to];
+  let ptr;
+  ptr = head;
+  while (ptr) {
+    const next = ptr.next;
+    ptr.next = new Node(ptr.val);
+    ptr.next.next = next;
+    ptr = next;
   }
-  return dummy.next;
+  ptr = head;
+  while (ptr) {
+    ptr.next.random = ptr.random ? ptr.random.next : null;
+    ptr = ptr.next.next;
+  }
+  const h = head.next;
+  ptr = head;
+  while (ptr) {
+    const next = ptr.next.next;
+    ptr.next.next = ptr.next.next ? ptr.next.next.next : null;
+    ptr.next = next;
+    ptr = next;
+  }
+  return h;
 };
