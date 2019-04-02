8422602cb75b8473abcbc3f235daff1ccedf56e8
Bob Wei <bob.wei@linecorp.com>
2019-04-03 01:14:56 +0800

    Update top-k-frequent-words

--- src/leetcode/top-k-frequent-words/index.js
+++ src/leetcode/top-k-frequent-words/index.js
@@ -3,77 +3,42 @@
  * @param {number} k
  * @return {string[]}
  */
-
-const comparator = (parent, child) => {
-  return parent.key < child.key || (parent.key === child.key && parent.value > child.value);
-};
-
-const swap = (arr, i, j) => {
-  const tmp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = tmp;
-};
-
-const bubbleUp = (arr, i, isValidHeap = comparator) => {
-  if (i <= 0) {
-    return;
-  }
-  const p = Math.floor((i - 1) / 2);
-  if (!isValidHeap(arr[p], arr[i])) {
-    swap(arr, p, i);
-    bubbleUp(arr, p);
-  }
-};
-
-const bubbleDown = (arr, i, isValidHeap = comparator) => {
-  const left = 2 * i + 1;
-  const right = 2 * i + 2;
-  const isValid =
-    (left >= arr.length || isValidHeap(arr[i], arr[left])) &&
-    (right >= arr.length || isValidHeap(arr[i], arr[right]));
-  if (!isValid) {
-    const target = right >= arr.length || !isValidHeap(arr[right], arr[left]) ? left : right;
-    swap(arr, i, target);
-    bubbleDown(arr, target);
-  }
-};
-
-const extractTop = (arr) => {
-  const output = arr[0];
-  if (arr.length <= 1) {
-    return output;
-  }
-  arr[0] = arr.pop();
-  bubbleDown(arr, 0);
-  return output;
-};
-
-const topKFrequent = function(words, k) {
-  const counts = {};
-  for (let i = 0; i < words.length; i++) {
-    counts[words[i]] = (counts[words[i]] || 0) + 1;
-  }
-  const arr = [];
-  const keys = Object.keys(counts);
-  for (let i = 0; i < keys.length; i++) {
-    const word = keys[i];
-    const count = counts[word];
-    arr.push({
-      key: count,
-      value: word,
-    });
-    bubbleUp(arr, arr.length - 1);
-    if (arr.length > k) {
-      extractTop(arr);
+var topKFrequent = function(words, k) {
+  const freq = words.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc;
+  }, {});
+  const arr = Object.keys(freq);
+  const compare = (k1, k2) => {
+    if (freq[k1] === freq[k2]) {
+      return k1 < k2 ? -1 : 1;
+    }
+    return freq[k2] - freq[k1];
+  };
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const p = partition(arr, left, right, compare);
+    if (k === p + 1) {
+      return arr.slice(0, k).sort(compare);
+    } else if (k <= p) {
+      right = p - 1;
+    } else {
+      left = p + 1;
     }
   }
-  const output = [];
-  for (let i = 0; i < k; i++) {
-    const { value } = extractTop(arr);
-    output.push(value);
-  }
-  output.reverse();
-  return output;
+  return [];
 };
 
-export default topKFrequent;
+function partition(arr, start, end, compare) {
+  const p = end;
+  let j = start;
+  for (let i = start; i < end; i++) {
+    if (compare(arr[i], arr[p]) < 0) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      j += 1;
+    }
+  }
+  [arr[p], arr[j]] = [arr[j], arr[p]];
+  return j;
+}
--- src/leetcode/top-k-frequent-words/index.spec.js
+++
@@ -1,9 +0,0 @@
-import fn from './index';
-
-test('top-k-frequent-words', () => {
-  expect(fn(['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd', 'e', 'e'], 2)).toEqual(['a', 'b']);
-});
-
-test('top-k-frequent-words', () => {
-  expect(fn(['a'], 1)).toEqual(['a']);
-});
