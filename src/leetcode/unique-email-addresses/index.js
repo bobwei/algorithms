2a739fe7de5d4100b489788cf08d1f8d96115996
Bob Wei <bob.wei@linecorp.com>
2019-04-07 02:03:17 +0800

    Add unique-email-addresses

--- src/leetcode/unique-email-addresses/index.js
+++ src/leetcode/unique-email-addresses/index.js
@@ -2,21 +2,11 @@
  * @param {string[]} emails
  * @return {number}
  */
-
-const normalize = (str) => {
-  const [localName, domainName] = str.split('@');
-  return localName.split('+')[0].replace(/\./g, '') + domainName;
-};
-
 var numUniqueEmails = function(emails) {
-  const cache = {};
-  let count = 0;
-  for (let i = 0; i < emails.length; i++) {
-    const email = normalize(emails[i]);
-    if (!cache[email]) {
-      cache[email] = true;
-      count += 1;
-    }
-  }
-  return count;
+  const normalizedEmails = emails.map((email) => {
+    const [local, domain] = email.split('@');
+    const normalizedLocal = local.split('+')[0].replace(/\./g, '');
+    return normalizedLocal + '@' + domain;
+  });
+  return new Set(normalizedEmails).size;
 };
