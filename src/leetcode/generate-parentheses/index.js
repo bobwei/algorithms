816fc107a6d702ca0f3c87e766e93708922e55d3
Bob Wei <bob.wei@linecorp.com>
2019-04-07 16:15:49 +0800

    Update generate-parentheses

--- src/leetcode/generate-parentheses/index.js
+++ src/leetcode/generate-parentheses/index.js
@@ -2,27 +2,16 @@
  * @param {number} n
  * @return {string[]}
  */
-
-const generateParenthesis = function(n, nLeft = n, nRight = n, selected = [], output = []) {
-  if (nLeft > nRight || nLeft < 0 || nRight < 0) {
-    return output;
-  }
+var generateParenthesis = function(n, left = 0, right = 0, selected = '', output = []) {
   if (selected.length >= n * 2) {
-    output.push(selected.join(''));
+    output.push(selected);
     return output;
   }
-  for (let i = 0; i < 2; i++) {
-    if (i % 2 === 0) {
-      selected.push('(');
-      generateParenthesis(n, nLeft - 1, nRight, selected, output);
-      selected.pop();
-    } else if (i % 2 === 1) {
-      selected.push(')');
-      generateParenthesis(n, nLeft, nRight - 1, selected, output);
-      selected.pop();
-    }
+  if (left < n) {
+    generateParenthesis(n, left + 1, right, selected + '(', output);
+  }
+  if (left > right) {
+    generateParenthesis(n, left, right + 1, selected + ')', output);
   }
   return output;
 };
-
-export default generateParenthesis;
--- src/leetcode/generate-parentheses/index.spec.js
+++
@@ -1,5 +0,0 @@
-import fn from './index';
-
-test('generate-parentheses', () => {
-  expect(fn(3)).toEqual(['((()))', '(()())', '(())()', '()(())', '()()()']);
-});
