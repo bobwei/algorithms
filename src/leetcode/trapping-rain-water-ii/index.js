f7bead592e53cc3279429b42b8c54613baa206ff
Bob Wei <bob.wei@linecorp.com>
2019-04-08 01:03:19 +0800

    Update trapping-rain-water-ii

--- src/leetcode/trapping-rain-water-ii/index.js
+++ src/leetcode/trapping-rain-water-ii/index.js
@@ -3,73 +3,7 @@
  * @return {number}
  */
 
-const swap = (arr, i, j) => {
-  const tmp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = tmp;
-};
-
-const moveUp = (arr, i, comparator) => {
-  const p = Math.floor((i - 1) / 2);
-  const isValid = p < 0 || comparator(arr[p], arr[i]);
-  if (!isValid) {
-    swap(arr, i, p);
-    moveUp(arr, p, comparator);
-  }
-};
-
-const moveDown = (arr, i, comparator) => {
-  const left = 2 * i + 1;
-  const right = 2 * i + 2;
-  const isValid =
-    (left >= arr.length || comparator(arr[i], arr[left])) &&
-    (right >= arr.length || comparator(arr[i], arr[right]));
-  if (!isValid) {
-    const next = right >= arr.length || comparator(arr[left], arr[right]) ? left : right;
-    swap(arr, i, next);
-    moveDown(arr, next, comparator);
-  }
-};
-
-class PriorityQueue {
-  constructor({ comparator }) {
-    this.arr = [];
-    this.comparator = comparator;
-  }
-
-  enqueue(element) {
-    this.arr.push(element);
-    moveUp(this.arr, this.arr.length - 1, this.comparator);
-    return this;
-  }
-
-  dequeue() {
-    if (this.arr.length <= 1) {
-      return this.arr.pop();
-    }
-    const element = this.arr.shift();
-    this.arr.unshift(this.arr.pop());
-    moveDown(this.arr, 0, this.comparator);
-    return element;
-  }
-}
-
-const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-
-const bfs = (element, m, n, pq, selected, heightMap) => {
-  for (let d = 0; d < directions.length; d++) {
-    const i = element.i + directions[d][0];
-    const j = element.j + directions[d][1];
-    if (i < 0 || i >= m || j < 0 || j >= n) {
-      continue;
-    }
-    if (selected[i][j]) {
-      continue;
-    }
-    pq.enqueue({ i, j, height: heightMap[i][j] });
-    selected[i][j] = true;
-  }
-};
+const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
 var trapRainWater = function(heightMap) {
   if (
@@ -82,33 +16,90 @@ var trapRainWater = function(heightMap) {
   }
   const m = heightMap.length;
   const n = heightMap[0].length;
-  const selected = [...new Array(m)].map(() => new Array(n).fill(false));
+  const visited = [...new Array(m)].map(() => new Array(n).fill(false));
   const pq = new PriorityQueue({
-    comparator: (a, b) => a.height < b.height,
+    comparator: (a, b) => heightMap[a[0]][a[1]] - heightMap[b[0]][b[1]],
   });
-  /* put four sides into pq */
-  for (let i = 0; i < m; i += m - 1) {
-    for (let j = 0; j < n; j++) {
-      pq.enqueue({ i, j, height: heightMap[i][j] });
-      selected[i][j] = true;
-    }
+  for (let j = 0; j < n; j++) {
+    pq.enqueue([0, j]);
+    visited[0][j] = true;
+    pq.enqueue([m - 1, j]);
+    visited[m - 1][j] = true;
   }
   for (let i = 1; i < m - 1; i++) {
-    for (let j = 0; j < n; j += n - 1) {
-      pq.enqueue({ i, j, height: heightMap[i][j] });
-      selected[i][j] = true;
-    }
+    pq.enqueue([i, 0]);
+    visited[i][0] = true;
+    pq.enqueue([i, n - 1]);
+    visited[i][n - 1] = true;
   }
   let output = 0;
-  let maxHeight = 0;
-  while (pq.arr.length) {
-    const element = pq.dequeue();
-    if (element.height < maxHeight) {
-      output += maxHeight - element.height;
+  let level = 0;
+  while (pq.length) {
+    const p = pq.dequeue();
+    const height = heightMap[p[0]][p[1]];
+    output += level > height ? level - height : 0;
+    level = Math.max(level, height);
+    for (const [di, dj] of dirs) {
+      const i = p[0] + di;
+      const j = p[1] + dj;
+      if (isValidPosition(i, j, m, n) && !visited[i][j]) {
+        pq.enqueue([i, j]);
+        visited[i][j] = true;
+      }
     }
-    maxHeight = Math.max(maxHeight, element.height);
-    /* bfs to explore surrounding */
-    bfs(element, m, n, pq, selected, heightMap);
   }
   return output;
 };
+
+function isValidPosition(i, j, m, n) {
+  if (i < 0 || i >= m || j < 0 || j >= n) {
+    return false;
+  }
+  return true;
+}
+
+class PriorityQueue {
+  constructor({ comparator }) {
+    this.comparator = comparator;
+    this.arr = [];
+  }
+
+  enqueue(element) {
+    this.arr.push(element);
+    moveUp(this.arr, this.arr.length - 1, this.comparator);
+  }
+
+  dequeue() {
+    const output = this.arr[0];
+    this.arr[0] = this.arr[this.arr.length - 1];
+    this.arr.pop();
+    moveDown(this.arr, 0, this.comparator);
+    return output;
+  }
+
+  get length() {
+    return this.arr.length;
+  }
+}
+
+function moveUp(arr, i, comparator) {
+  const p = Math.floor((i - 1) / 2);
+  const isValid = p < 0 || comparator(arr[p], arr[i]) <= 0;
+  if (!isValid) {
+    [arr[i], arr[p]] = [arr[p], arr[i]];
+    moveUp(arr, p, comparator);
+  }
+}
+
+function moveDown(arr, i, comparator) {
+  const left = 2 * i + 1;
+  const right = 2 * i + 2;
+  const isValid =
+    (left >= arr.length || comparator(arr[i], arr[left]) <= 0) &&
+    (right >= arr.length || comparator(arr[i], arr[right]) <= 0);
+  if (!isValid) {
+    const next = right >= arr.length || comparator(arr[left], arr[right]) <= 0 ? left : right;
+    [arr[i], arr[next]] = [arr[next], arr[i]];
+    moveDown(arr, next, comparator);
+  }
+}
