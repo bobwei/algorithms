f9e0539a95a70c8810e10d02e84db8d5c2b99fd1
Bob Wei <bob.wei@linecorp.com>
2019-04-12 00:12:41 +0800

    Refactor BinaryIndexedTree

--- src/data-structure/binary-indexed-tree/index.js
+++ src/data-structure/binary-indexed-tree/index.js
@@ -1,28 +1,32 @@
 class BinaryIndexedTree {
   constructor(arr) {
-    this.bit = new Array(arr.length - 1).fill(0);
-    arr.forEach((val, i) => {
-      this.update(i, val);
-    });
+    this.m = arr.length;
+    this.bit = new Array(this.m + 1).fill(0);
+    this.arr = new Array(this.m).fill(0);
+    for (let i = 0; i < arr.length; i++) {
+      this.update(i, arr[i]);
+    }
   }
 
-  getSum(index) {
-    let i = index + 1;
+  getSum(i) {
     let sum = 0;
-    while (i > 0) {
-      sum += this.bit[i];
-      i -= i & -i;
+    for (let j = i + 1; j > 0; j -= lowBit(j)) {
+      sum += this.bit[j];
     }
     return sum;
   }
 
-  update(index, delta) {
-    let i = index + 1;
-    while (i < this.bit.length) {
-      this.bit[i] += delta;
-      i = i + (i & -i);
+  update(i, val) {
+    const delta = val - this.arr[i];
+    for (let j = i + 1; j < this.m + 1; j += lowBit(j)) {
+      this.bit[j] += delta;
     }
+    this.arr[i] = val;
   }
+}
+
+function lowBit(i) {
+  return i & -i;
 }
 
 export default BinaryIndexedTree;
