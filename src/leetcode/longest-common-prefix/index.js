d6611586093d28f53c72fae8f1bded7ae0432389
Bob Wei <bob.wei@linecorp.com>
2019-04-15 12:08:59 +0800

    Update longest-common-prefix

--- src/leetcode/longest-common-prefix/index.js
+++ src/leetcode/longest-common-prefix/index.js
@@ -3,20 +3,14 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-  if (strs.length <= 0) {
+  if (!strs.length) {
     return '';
   }
-  if (strs.length === 1) {
-    return strs.pop();
-  }
-  let l = strs[0].length;
-  for (let i = 1; i < strs.length; i++) {
-    while (strs[i].slice(0, l) !== strs[0].slice(0, l)) {
-      l -= 1;
-      if (l <= 0) {
-        return '';
-      }
+  let prefix = strs[0];
+  for (const str of strs) {
+    while (prefix !== str.substring(0, prefix.length)) {
+      prefix = prefix.substring(0, prefix.length - 1);
     }
   }
-  return strs[0].slice(0, l);
+  return prefix;
 };
