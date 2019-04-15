5bd08a833c326c288b479ce7e145f2d88eea7386
Bob Wei <bob.wei@linecorp.com>
2019-04-15 12:45:31 +0800

    Update remove-invalid-parentheses

--- src/leetcode/remove-invalid-parentheses/index.js
+++ src/leetcode/remove-invalid-parentheses/index.js
@@ -2,13 +2,40 @@
  * @param {string} s
  * @return {string[]}
  */
+var removeInvalidParentheses = function(s) {
+  const output = [];
+  const visited = new Set();
+  const queue = [s];
+  while (queue.length) {
+    const str = queue.shift();
+    if (isValid(str)) {
+      const minLength = output.length > 0 ? output[0].length : -Infinity;
+      if (str.length >= minLength) {
+        output.push(str);
+      }
+    }
+    if (!output.length) {
+      for (let i = 0; i < str.length; i++) {
+        if (str[i] !== '(' && str[i] !== ')') {
+          continue;
+        }
+        const next = str.substring(0, i) + str.substring(i + 1);
+        if (!visited.has(next)) {
+          queue.push(next);
+          visited.add(next);
+        }
+      }
+    }
+  }
+  return output;
+};
 
-const isValid = (str) => {
+function isValid(str) {
   let n = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === '(') {
+  for (const c of str) {
+    if (c === '(') {
       n += 1;
-    } else if (str[i] === ')') {
+    } else if (c === ')') {
       n -= 1;
     }
     if (n < 0) {
@@ -16,30 +43,4 @@ const isValid = (str) => {
     }
   }
   return n === 0;
-};
-
-var removeInvalidParentheses = function(s) {
-  const output = [];
-  const queue = [s];
-  const cache = new Set();
-  while (queue.length) {
-    const str = queue.shift();
-    if (cache.has(str)) {
-      continue;
-    }
-    cache.add(str);
-    if (isValid(str)) {
-      output.push(str);
-    }
-    if (!output.length) {
-      for (let i = 0; i < str.length; i++) {
-        if (str[i] !== '(' && str[i] !== ')') {
-          continue;
-        }
-        const tmpStr = str.slice(0, i) + str.slice(i + 1);
-        queue.push(tmpStr);
-      }
-    }
-  }
-  return output;
-};
+}
