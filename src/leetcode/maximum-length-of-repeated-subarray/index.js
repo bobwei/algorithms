3546fb7ad5abd7cd2904c87ef87d7a6a9cf89bc8
Bob Wei <bob.wei@linecorp.com>
2019-04-16 16:31:08 +0800

    Update maximum-length-of-repeated-subarray

--- src/leetcode/maximum-length-of-repeated-subarray/index.js
+++ src/leetcode/maximum-length-of-repeated-subarray/index.js
@@ -4,17 +4,13 @@
  * @return {number}
  */
 var findLength = function(A, B) {
-  const dp = [...new Array(A.length)].map(() => new Array(B.length).fill(0));
-  for (let i = 0; i < B.length; i++) {
-    dp[0][i] = A[0] === B[i] ? 1 : 0;
-  }
-  for (let i = 0; i < A.length; i++) {
-    dp[i][0] = A[i] === B[0] ? 1 : 0;
-  }
-  let max = -Infinity;
-  for (let i = 1; i < A.length; i++) {
-    for (let j = 1; j < B.length; j++) {
-      dp[i][j] = A[i] === B[j] ? dp[i - 1][j - 1] + 1 : 0;
+  const m = A.length;
+  const n = B.length;
+  const dp = [...new Array(m + 1)].map(() => new Array(n + 1).fill(0));
+  let max = 0;
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      dp[i][j] = A[i - 1] === B[j - 1] ? dp[i - 1][j - 1] + 1 : 0;
       max = Math.max(max, dp[i][j]);
     }
   }
