dd45fefacdf81a91b95efe3fd484343a23f6dd8a
Bob Wei <bob.wei@linecorp.com>
2019-04-16 14:37:37 +0800

    Update merge-k-sorted-lists

--- src/leetcode/merge-k-sorted-lists/index.js
+++ src/leetcode/merge-k-sorted-lists/index.js
@@ -9,35 +9,36 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-
-const merge = (r1, r2) => {
-  const dummy = new ListNode();
-  let ptr = dummy;
-  let p1 = r1;
-  let p2 = r2;
-  while (p1 || p2) {
-    if (!p1 || !p2) {
-      ptr.next = p1 || p2;
-      return dummy.next;
-    } else if (p1.val <= p2.val) {
-      ptr.next = p1;
-      ptr = ptr.next;
-      p1 = p1.next;
-    } else if (p2.val <= p1.val) {
-      ptr.next = p2;
-      ptr = ptr.next;
-      p2 = p2.next;
-    }
-  }
-  return dummy.next;
-};
-
 var mergeKLists = function(lists) {
   if (!lists.length) {
     return null;
   }
   while (lists.length > 1) {
-    lists.push(merge(lists.shift(), lists.shift()));
+    const head = merge(lists.shift(), lists.shift());
+    lists.push(head);
   }
   return lists.pop();
 };
+
+function merge(l1, l2) {
+  const dummy = new ListNode();
+  let ptr = dummy;
+  let p1 = l1;
+  let p2 = l2;
+  while (p1 || p2) {
+    if (p1 && p2) {
+      if (p1.val <= p2.val) {
+        ptr.next = p1;
+        p1 = p1.next;
+      } else {
+        ptr.next = p2;
+        p2 = p2.next;
+      }
+      ptr = ptr.next;
+    } else {
+      ptr.next = p1 || p2;
+      return dummy.next;
+    }
+  }
+  return null;
+}
