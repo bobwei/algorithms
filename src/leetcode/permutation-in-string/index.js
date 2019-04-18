aa303a7770af84376307272d7eda89cf31119f9f
Bob Wei <bob.wei@linecorp.com>
2019-04-19 00:41:57 +0800

    permutation-in-string

--- src/leetcode/permutation-in-string/index.js
+++ src/leetcode/permutation-in-string/index.js
@@ -8,11 +8,12 @@ var checkInclusion = function(s1, s2) {
   let start = 0;
   for (let i = 0; i < s2.length; i++) {
     counter.add(s2[i]);
-    while (counter.n === 0) {
-      if (i - start + 1 === s1.length) {
-        return true;
-      }
-      counter.delete(s2[start++]);
+    while (counter.nRedundant > 0) {
+      counter.delete(s2[start]);
+      start += 1;
+    }
+    if (i - start + 1 === s1.length) {
+      return true;
     }
   }
   return false;
@@ -20,24 +21,27 @@ var checkInclusion = function(s1, s2) {
 
 class Counter {
   constructor(target) {
-    this.n = target.length;
-    this.counts = target.split('').reduce((acc, cur) => {
-      acc[cur] = (acc[cur] || 0) + 1;
-      return acc;
-    }, {});
+    // prettier-ignore
+    this.counts = target
+      .split('')
+      .reduce((acc, cur) => {
+        acc[cur] = (acc[cur] || 0) + 1;
+        return acc;
+      }, {});
+    this.nRedundant = 0;
   }
 
   add(c) {
     this.counts[c] = (this.counts[c] || 0) - 1;
-    if (this.counts[c] >= 0) {
-      this.n -= 1;
+    if (this.counts[c] < 0) {
+      this.nRedundant += 1;
     }
   }
 
   delete(c) {
-    this.counts[c] = this.counts[c] + 1;
-    if (this.counts[c] > 0) {
-      this.n += 1;
+    this.counts[c] += 1;
+    if (this.counts[c] <= 0) {
+      this.nRedundant -= 1;
     }
   }
 }
