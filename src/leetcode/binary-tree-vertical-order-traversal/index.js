16187eb9cfed42fd518b33b36b3ef822bca8f841
Bob Wei <bob.wei@linecorp.com>
2019-04-27 17:06:24 +0800

    Update binary-tree-vertical-order-traversal

--- src/leetcode/binary-tree-vertical-order-traversal/index.js
+++ src/leetcode/binary-tree-vertical-order-traversal/index.js
@@ -14,13 +14,9 @@ var verticalOrder = function(root) {
     return [];
   }
   const columns = {};
-  let min = 0;
-  let max = 0;
   const queue = [[root, 0]];
   while (queue.length) {
     const [node, col] = queue.shift();
-    min = Math.min(min, col);
-    max = Math.max(max, col);
     if (!(col in columns)) columns[col] = [];
     columns[col].push(node.val);
     if (node.left) {
@@ -30,9 +26,8 @@ var verticalOrder = function(root) {
       queue.push([node.right, col + 1]);
     }
   }
-  const output = [];
-  for (let i = min; i <= max; i++) {
-    output.push(columns[i]);
-  }
-  return output;
+  return Object.keys(columns)
+    .map((k) => parseInt(k))
+    .sort((a, b) => a - b)
+    .map((k) => columns[k]);
 };
