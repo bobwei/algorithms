1b1235e8c71ab65848121f4282211e93e19a86ec
Bob Wei <bob.wei@linecorp.com>
2019-05-01 00:23:05 +0800

    Update add-binary

--- src/leetcode/add-binary/index.js
+++ src/leetcode/add-binary/index.js
@@ -3,22 +3,18 @@
  * @param {string} b
  * @return {string}
  */
-
-const toNumArray = (str) => {
-  return str
-    .split('')
-    .reverse()
-    .map((c) => parseInt(c));
-};
-
 var addBinary = function(a, b) {
-  const arr1 = toNumArray(a);
-  const arr2 = toNumArray(b);
-  let output = '';
+  const m = a.length;
+  const n = b.length;
+  let i = 1;
   let c = 0;
-  let i = 0;
-  while (i < arr1.length || i < arr2.length || c) {
-    const sum = (arr1[i] || 0) + (arr2[i] || 0) + c;
+  let output = '';
+  while (c > 0 || m - i >= 0 || n - i >= 0) {
+    // prettier-ignore
+    const sum =
+      (m - i >= 0 ? parseInt(a[m - i]) : 0) +
+      (n - i >= 0 ? parseInt(b[n - i]) : 0) +
+      c;
     output = (sum % 2) + output;
     c = Math.floor(sum / 2);
     i += 1;
