27759b53ecc2981982d8689c9a36c4618f5b2411
Bob Wei <bob.wei@linecorp.com>
2019-05-02 22:47:37 +0800

    Update task-scheduler

--- src/leetcode/task-scheduler/index.js
+++ src/leetcode/task-scheduler/index.js
@@ -3,27 +3,36 @@
  * @param {number} n
  * @return {number}
  */
-
-const moveUp = (arr, i, comparator) => {
-  const p = Math.floor((i - 1) / 2);
-  const isValid = i === 0 || comparator(arr[p], arr[i]);
-  if (!isValid) {
-    [arr[p], arr[i]] = [arr[i], arr[p]];
-    moveUp(arr, p, comparator);
+var leastInterval = function(tasks, n) {
+  const freq = tasks.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc;
+  }, {});
+  const pq = new PriorityQueue({
+    comparator: (a, b) => {
+      return freq[a] >= freq[b];
+    },
+  });
+  Object.keys(freq).forEach((key) => pq.enqueue(key));
+  let output = 0;
+  for (let i = 0; i < tasks.length; ) {
+    const next = [];
+    for (let j = 0; j <= n && i < tasks.length; j++) {
+      if (pq.length) {
+        const key = pq.dequeue();
+        freq[key] -= 1;
+        if (freq[key]) {
+          next.push(key);
+        }
+        output += 1;
+        i += 1;
+      } else {
+        output += 1;
+      }
+    }
+    next.forEach((key) => pq.enqueue(key));
   }
-};
-
-const moveDown = (arr, i, comparator) => {
-  const left = 2 * i + 1;
-  const right = 2 * i + 2;
-  const isValid =
-    (left >= arr.length || comparator(arr[i], arr[left])) &&
-    (right >= arr.length || comparator(arr[i], arr[right]));
-  if (!isValid) {
-    const next = right >= arr.length || comparator(arr[left], arr[right]) ? left : right;
-    [arr[i], arr[next]] = [arr[next], arr[i]];
-    moveDown(arr, next, comparator);
-  }
+  return output;
 };
 
 class PriorityQueue {
@@ -50,35 +59,24 @@ class PriorityQueue {
   }
 }
 
-var leastInterval = function(tasks, n) {
-  const counts = {};
-  for (let i = 0; i < tasks.length; i++) {
-    counts[tasks[i]] = (counts[tasks[i]] || 0) + 1;
+function moveUp(arr, i, comparator) {
+  const p = Math.floor((i - 1) / 2);
+  const isValid = p < 0 || comparator(arr[p], arr[i]);
+  if (!isValid) {
+    [arr[i], arr[p]] = [arr[p], arr[i]];
+    moveUp(arr, p, comparator);
   }
-  const pq = new PriorityQueue({
-    comparator: (a, b) => counts[a] >= counts[b],
-  });
-  for (const t in counts) {
-    pq.enqueue(t);
+}
+
+function moveDown(arr, i, comparator) {
+  const left = 2 * i + 1;
+  const right = 2 * i + 2;
+  const isValid =
+    (left >= arr.length || comparator(arr[i], arr[left])) &&
+    (right >= arr.length || comparator(arr[i], arr[right]));
+  if (!isValid) {
+    const next = right >= arr.length || comparator(arr[left], arr[right]) ? left : right;
+    [arr[i], arr[next]] = [arr[next], arr[i]];
+    moveDown(arr, next, comparator);
   }
-  let output = 0;
-  let i = 0;
-  while (i < tasks.length) {
-    const next = [];
-    let j = 0;
-    while (j <= n && i < tasks.length) {
-      if (pq.length) {
-        const t = pq.dequeue();
-        counts[t] -= 1;
-        if (counts[t] > 0) {
-          next.push(t);
-        }
-        i += 1;
-      }
-      j += 1;
-      output += 1;
-    }
-    next.forEach((t) => pq.enqueue(t));
-  }
-  return output;
-};
+}
