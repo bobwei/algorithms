804024bcfce4dcda7b78048ae06ab2160e6116a0
Bob Wei <bob.wei@linecorp.com>
2019-05-05 19:18:15 +0800

    Update sliding-window-maximum

--- src/leetcode/sliding-window-maximum/index.js
+++ src/leetcode/sliding-window-maximum/index.js
@@ -5,18 +5,24 @@
  */
 var maxSlidingWindow = function(nums, k) {
   const output = [];
-  const deque = [];
+  const deque = new Deque();
   for (let i = 0; i < nums.length; i++) {
-    if (deque.length && deque[0] < i - k + 1) {
+    if (deque.length && deque[0] <= i - k) {
       deque.shift();
     }
-    while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+    while (deque.length && nums[i] > nums[deque.rear()]) {
       deque.pop();
     }
     deque.push(i);
-    if (i - k + 1 >= 0) {
+    if (i >= k - 1) {
       output.push(nums[deque[0]]);
     }
   }
   return output;
 };
+
+class Deque extends Array {
+  rear() {
+    return this[this.length - 1];
+  }
+}
