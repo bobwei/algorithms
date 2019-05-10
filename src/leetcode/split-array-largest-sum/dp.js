03467bdf2338f66d47ed13b3dd74774274cb5502
Bob Wei <bob.wei@linecorp.com>
2019-05-10 16:24:56 +0800

    Update split-array-largest-sum

--- src/leetcode/split-array-largest-sum/dp.js
+++ src/leetcode/split-array-largest-sum/dp.js
@@ -16,6 +16,7 @@
 var splitArray = function(nums, m) {
   const n = nums.length;
   const dp = [...new Array(m + 1)].map(() => new Array(n + 1).fill(0));
+  const sum = createSum(nums, n);
   dp[1][0] = 0;
   for (let j = 1; j <= n; j++) {
     dp[1][j] = dp[1][j - 1] + nums[j - 1];
@@ -25,7 +26,7 @@ var splitArray = function(nums, m) {
       dp[i][j] = (() => {
         let min = Infinity;
         for (let k = 1; k < j; k++) {
-          const max = Math.max(dp[i - 1][j - k], sum(nums, j - k, j));
+          const max = Math.max(dp[i - 1][j - k], sum[j - 1] - sum[j - k - 1]);
           min = Math.min(min, max);
         }
         return min;
@@ -35,10 +36,11 @@ var splitArray = function(nums, m) {
   return dp[m][n];
 };
 
-function sum(arr, start, end) {
-  let output = 0;
-  for (let i = start; i < end; i++) {
-    output += arr[i];
+function createSum(arr, n) {
+  const sum = new Array(n).fill(0);
+  sum[0] = arr[0];
+  for (let i = 1; i < n; i++) {
+    sum[i] = sum[i - 1] + arr[i];
   }
-  return output;
+  return sum;
 }
