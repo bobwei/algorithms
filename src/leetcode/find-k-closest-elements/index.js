3f48a4a54566e508ff2769508c61f11ae2765115
Bob Wei <bob.wei@linecorp.com>
2019-05-16 00:43:44 +0800

    Add find-k-closest-elements

---
+++ src/leetcode/find-k-closest-elements/index.js
@@ -0,0 +1,78 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function(arr, k, x) {
+  const pq = new PriorityQueue({
+    comparator: (a, b) => {
+      if (Math.abs(x - a) !== Math.abs(x - b)) {
+        return Math.abs(x - a) >= Math.abs(x - b);
+      }
+      return a > b;
+    },
+  });
+  for (const num of arr) {
+    pq.enqueue(num);
+    if (pq.length > k) {
+      pq.dequeue();
+    }
+  }
+  return [...pq].sort((a, b) => a - b);
+};
+
+class PriorityQueue {
+  constructor({ comparator }) {
+    this.arr = [];
+    this.comparator = comparator;
+  }
+
+  enqueue(element) {
+    this.arr.push(element);
+    moveUp(this.arr, this.arr.length - 1, this.comparator);
+  }
+
+  dequeue() {
+    const output = this.arr[0];
+    this.arr[0] = this.arr[this.arr.length - 1];
+    this.arr.pop();
+    moveDown(this.arr, 0, this.comparator);
+    return output;
+  }
+
+  get length() {
+    return this.arr.length;
+  }
+
+  [Symbol.iterator]() {
+    return {
+      next: () => ({
+        done: !this.arr.length,
+        value: this.dequeue(),
+      }),
+    };
+  }
+}
+
+function moveUp(arr, i, comparator) {
+  const p = Math.floor((i - 1) / 2);
+  const isValid = p < 0 || comparator(arr[p], arr[i]);
+  if (!isValid) {
+    [arr[i], arr[p]] = [arr[p], arr[i]];
+    moveUp(arr, p, comparator);
+  }
+}
+
+function moveDown(arr, i, comparator) {
+  const left = 2 * i + 1;
+  const right = 2 * i + 2;
+  const isValid =
+    (left >= arr.length || comparator(arr[i], arr[left])) &&
+    (right >= arr.length || comparator(arr[i], arr[right]));
+  if (!isValid) {
+    const next = right >= arr.length || comparator(arr[left], arr[right]) ? left : right;
+    [arr[i], arr[next]] = [arr[next], arr[i]];
+    moveDown(arr, next, comparator);
+  }
+}
