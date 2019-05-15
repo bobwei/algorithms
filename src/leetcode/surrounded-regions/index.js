8bfac0a57ddff5584228cf56dc0232e860c20d58
Bob Wei <bob.wei@linecorp.com>
2019-05-15 12:15:13 +0800

    Update surrounded-regions

--- src/leetcode/surrounded-regions/index.js
+++ src/leetcode/surrounded-regions/index.js
@@ -2,48 +2,55 @@
  * @param {character[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
+
+const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+const createBorders = (m, n) => [[m - 1, 0], [0, 0], [0, n - 1], [m - 1, n - 1]];
+
 var solve = function(board) {
   if (!board.length || !board[0].length) {
     return;
   }
   const m = board.length;
   const n = board[0].length;
-  const visited = [...new Array(m)].map(() => new Array(n).fill(false));
+  flipBorders(board, m, n, 'O', '1');
+  flipBody(board, m, n, 'O', 'X');
+  flipBorders(board, m, n, '1', 'O');
+};
+
+function flipBody(board, m, n, from, to) {
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-      if (board[i][j] === 'O' && !visited[i][j]) {
-        const path = [];
-        if (!dfs(board, m, n, [i, j], visited, path)) {
-          mutate(board, path, 'X');
-        }
+      if (board[i][j] === from) {
+        dfs(board, m, n, [i, j], from, to);
       }
     }
   }
-};
+}
 
-function mutate(board, path, value) {
-  for (const [i, j] of path) {
-    board[i][j] = value;
+function flipBorders(board, m, n, from, to) {
+  const borders = createBorders(m, n);
+  for (let k = 0; k < 4; k++) {
+    const [di, dj] = dirs[k];
+    let [i, j] = borders[k];
+    while (isValid(i, j, m, n)) {
+      if (board[i][j] === from) {
+        dfs(board, m, n, [i, j], from, to);
+      }
+      i += di;
+      j += dj;
+    }
   }
 }
 
-const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-
-function dfs(board, m, n, p, visited, path) {
-  path.push(p);
-  visited[p[0]][p[1]] = true;
-  let result = false;
+function dfs(board, m, n, p, from, to) {
+  board[p[0]][p[1]] = to;
   for (const [di, dj] of dirs) {
     const i = p[0] + di;
     const j = p[1] + dj;
-    if (
-      !isValid(i, j, m, n) ||
-      (!visited[i][j] && board[i][j] === 'O' && dfs(board, m, n, [i, j], visited, path))
-    ) {
-      result = true;
+    if (isValid(i, j, m, n) && board[i][j] === from) {
+      dfs(board, m, n, [i, j], from, to);
     }
   }
-  return result;
 }
 
 function isValid(i, j, m, n) {
