55b7357559c74a753a30e36d48a5fb96a31b6720
Bob Wei <bob.wei@linecorp.com>
2019-05-16 12:28:27 +0800

    Updte interval-list-intersections

--- src/leetcode/interval-list-intersections/index.js
+++ src/leetcode/interval-list-intersections/index.js
@@ -5,26 +5,28 @@
  */
 var intervalIntersection = function(A, B) {
   const output = [];
-  while (A.length && B.length) {
-    const interval = findIntersection(A[0], B[0]);
-    if (interval) {
+  let i = 0;
+  let j = 0;
+  while (i < A.length && j < B.length) {
+    const interval = [Math.max(A[i][0], B[j][0]), Math.min(A[i][1], B[j][1])];
+    if (isValid(interval)) {
       output.push(interval);
     }
-    if (A[0][1] < B[0][1]) {
-      A.shift();
+    if (A[i][1] < B[j][1]) {
+      i += 1;
+    } else if (B[j][1] < A[i][1]) {
+      j += 1;
     } else {
-      B.shift();
+      i += 1;
+      j += 1;
     }
   }
   return output;
 };
 
-function findIntersection(interval1, interval2) {
-  if (interval1[0] > interval2[0]) {
-    return findIntersection(interval2, interval1);
+function isValid([start, end]) {
+  if (end < start) {
+    return false;
   }
-  if (interval2[0] > interval1[1]) {
-    return null;
-  }
-  return [Math.max(interval1[0], interval2[0]), Math.min(interval1[1], interval2[1])];
+  return true;
 }
