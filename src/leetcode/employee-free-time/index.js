15b6be3408a9264367ad9aa03b3efc6bd9c8fb4b
Bob Wei <bob.wei@linecorp.com>
2019-05-17 16:17:04 +0800

    Update employee-free-time

--- src/leetcode/employee-free-time/index.js
+++ src/leetcode/employee-free-time/index.js
@@ -3,78 +3,33 @@
  * @return {number[][]}
  */
 var employeeFreeTime = function(schedule) {
-  if (!schedule.length) {
-    return [];
+  const timeline = createTimeline(schedule);
+  const output = [];
+  let nEvents = 0;
+  let preNEvents;
+  let preT;
+  for (const [t, delta] of timeline) {
+    nEvents += delta;
+    if (preNEvents === 0 && nEvents > 0) {
+      output.push([preT, t]);
+    }
+    preNEvents = nEvents;
+    preT = t;
   }
-  const n = schedule.length;
-  const maxEndTime = getMaxEndTime(schedule, n);
-  const freeTimes = getFreeTimes(schedule, n, maxEndTime);
-  return mergeAll(freeTimes);
+  return output.filter(isValidOutput);
 };
 
-function merge(interval1, interval2) {
-  const output = [];
-  let i = 0;
-  let j = 0;
-  while (i < interval1.length && j < interval2.length) {
-    const interval = [
-      Math.max(interval1[i][0], interval2[j][0]),
-      Math.min(interval1[i][1], interval2[j][1]),
-    ];
-    if (isValidInterval(interval)) {
-      output.push(interval);
-    }
-    if (interval1[i][1] <= interval2[j][1]) {
-      i += 1;
-    } else if (interval2[j][1] <= interval1[i][1]) {
-      j += 1;
-    }
-  }
-  return output;
-}
-
-function isValidInterval([start, end]) {
-  if (end < start) {
-    return false;
-  }
-  return true;
-}
-
 function isValidOutput([start, end]) {
-  return start > -Infinity && end - start > 0;
+  return end - start > 0;
 }
 
-function mergeAll(freeTimes) {
-  while (freeTimes.length > 1) {
-    const result = merge(freeTimes.shift(), freeTimes.shift());
-    freeTimes.push(result);
-  }
-  return freeTimes.pop().filter(isValidOutput);
-}
-
-function getFreeTimes(schedule, n, maxEndTime) {
-  const freeTimes = [...new Array(n)].map(() => []);
-  for (let i = 0; i < n; i++) {
-    const m = schedule[i].length;
-    if (m && schedule[i][0][0] > 0) {
-      freeTimes[i].push([-Infinity, schedule[i][0][0]]);
-    }
-    for (let j = 1; j < m; j++) {
-      freeTimes[i].push([schedule[i][j - 1][1], schedule[i][j][0]]);
-    }
-    if (m && schedule[i][m - 1][1] < maxEndTime) {
-      freeTimes[i].push([schedule[i][m - 1][1], maxEndTime]);
-    }
-  }
-  return freeTimes;
-}
-
-function getMaxEndTime(schedule, n) {
-  let max = 0;
+function createTimeline(schedule) {
+  const timeline = [];
   for (const employee of schedule) {
-    for (const [, end] of employee) {
-      max = Math.max(max, end);
+    for (const [start, end] of employee) {
+      timeline.push([start, 1]);
+      timeline.push([end, -1]);
     }
   }
-  return max;
+  return timeline.sort((a, b) => a[0] - b[0]);
 }
