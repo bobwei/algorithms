e45a49aab5fbadc72b356588f0ac05e2af5762b5
Bob Wei <bob.wei@linecorp.com>
2019-05-17 15:35:17 +0800

    Update find-k-closest-elements

--- src/leetcode/find-k-closest-elements/index.js
+++ src/leetcode/find-k-closest-elements/index.js
@@ -4,75 +4,19 @@
  * @param {number} x
  * @return {number[]}
  */
+/*
+  [1,2,3,4,5] 2 4
+*/
 var findClosestElements = function(arr, k, x) {
-  const pq = new PriorityQueue({
-    comparator: (a, b) => {
-      if (Math.abs(x - a) !== Math.abs(x - b)) {
-        return Math.abs(x - a) >= Math.abs(x - b);
-      }
-      return a > b;
-    },
-  });
-  for (const num of arr) {
-    pq.enqueue(num);
-    if (pq.length > k) {
-      pq.dequeue();
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (mid + k < arr.length && x - arr[mid] > arr[mid + k] - x) {
+      left = mid + 1;
+    } else {
+      right = mid;
     }
   }
-  return [...pq].sort((a, b) => a - b);
+  return arr.slice(left, left + k);
 };
-
-class PriorityQueue {
-  constructor({ comparator }) {
-    this.arr = [];
-    this.comparator = comparator;
-  }
-
-  enqueue(element) {
-    this.arr.push(element);
-    moveUp(this.arr, this.arr.length - 1, this.comparator);
-  }
-
-  dequeue() {
-    const output = this.arr[0];
-    this.arr[0] = this.arr[this.arr.length - 1];
-    this.arr.pop();
-    moveDown(this.arr, 0, this.comparator);
-    return output;
-  }
-
-  get length() {
-    return this.arr.length;
-  }
-
-  [Symbol.iterator]() {
-    return {
-      next: () => ({
-        done: !this.arr.length,
-        value: this.dequeue(),
-      }),
-    };
-  }
-}
-
-function moveUp(arr, i, comparator) {
-  const p = Math.floor((i - 1) / 2);
-  const isValid = p < 0 || comparator(arr[p], arr[i]);
-  if (!isValid) {
-    [arr[i], arr[p]] = [arr[p], arr[i]];
-    moveUp(arr, p, comparator);
-  }
-}
-
-function moveDown(arr, i, comparator) {
-  const left = 2 * i + 1;
-  const right = 2 * i + 2;
-  const isValid =
-    (left >= arr.length || comparator(arr[i], arr[left])) &&
-    (right >= arr.length || comparator(arr[i], arr[right]));
-  if (!isValid) {
-    const next = right >= arr.length || comparator(arr[left], arr[right]) ? left : right;
-    [arr[i], arr[next]] = [arr[next], arr[i]];
-    moveDown(arr, next, comparator);
-  }
-}
