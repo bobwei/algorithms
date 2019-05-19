ae4e31279d1a010aefadea7e837c102e5d9984a4
Bob Wei <bob.wei@linecorp.com>
2019-05-20 00:27:19 +0800

    Update merge-intervals

--- src/leetcode/merge-intervals/index.js
+++ src/leetcode/merge-intervals/index.js
@@ -1,30 +1,29 @@
 /**
- * Definition for an interval.
- * function Interval(start, end) {
- *     this.start = start;
- *     this.end = end;
- * }
- */
-/**
- * @param {Interval[]} intervals
- * @return {Interval[]}
+ * @param {number[][]} intervals
+ * @return {number[][]}
  */
 var merge = function(intervals) {
-  if (!intervals.length) {
-    return [];
+  if (intervals.length <= 1) {
+    return intervals;
   }
-  intervals.sort((a, b) => a.start - b.start);
+  intervals.sort((a, b) => a[0] - b[0]);
   const output = [];
-  let pre = intervals.shift();
-  while (intervals.length || pre) {
-    const next = intervals.shift();
-    if (next && pre.end >= next.start) {
-      pre.start = Math.min(pre.start, next.start);
-      pre.end = Math.max(pre.end, next.end);
+  let ptr = intervals[0];
+  for (let i = 1; i < intervals.length; i++) {
+    if (shouldMerge(ptr, intervals[i])) {
+      ptr = [Math.min(ptr[0], intervals[i][0]), Math.max(ptr[1], intervals[i][1])];
     } else {
-      output.push(pre);
-      pre = next;
+      output.push(ptr);
+      ptr = intervals[i];
     }
   }
+  output.push(ptr);
   return output;
 };
+
+function shouldMerge(interval1, interval2) {
+  if (interval1[0] > interval2[0]) {
+    return shouldMerge(interval2, interval1);
+  }
+  return interval2[0] <= interval1[1] && interval2[1] >= interval1[0];
+}
