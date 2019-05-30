c223d94e4e8bf3f497bac708ef27b6dbe4f1b146
Bob Wei <bob.wei@linecorp.com>
2019-05-30 17:19:23 +0800

    Update number-of-islands

--- src/leetcode/number-of-islands/index.js
+++ src/leetcode/number-of-islands/index.js
@@ -2,23 +2,6 @@
  * @param {character[][]} grid
  * @return {number}
  */
-
-const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-
-const dfs = (grid, visited, i, j, m, n) => {
-  if (i < 0 || i >= m || j < 0 || j >= n) {
-    return;
-  }
-  if (visited[i][j] || grid[i][j] === '0') {
-    return;
-  }
-  visited[i][j] = true;
-  for (let d = 0; d < dirs.length; d++) {
-    const [di, dj] = dirs[d];
-    dfs(grid, visited, i + di, j + dj, m, n);
-  }
-};
-
 var numIslands = function(grid) {
   if (!grid.length || !grid[0].length) {
     return 0;
@@ -26,14 +9,34 @@ var numIslands = function(grid) {
   const m = grid.length;
   const n = grid[0].length;
   const visited = [...new Array(m)].map(() => new Array(n).fill(false));
-  let count = 0;
+  let output = 0;
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j] === '1' && !visited[i][j]) {
-        dfs(grid, visited, i, j, m, n);
-        count += 1;
+        dfs(grid, m, n, [i, j], visited);
+        output += 1;
       }
     }
   }
-  return count;
+  return output;
 };
+
+const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+
+function dfs(grid, m, n, p, visited) {
+  visited[p[0]][p[1]] = true;
+  for (const [di, dj] of dirs) {
+    const i = p[0] + di;
+    const j = p[1] + dj;
+    if (isValid(i, j, m, n) && !visited[i][j] && grid[i][j] === '1') {
+      dfs(grid, m, n, [i, j], visited);
+    }
+  }
+}
+
+function isValid(i, j, m, n) {
+  if (i < 0 || i >= m || j < 0 || j >= n) {
+    return false;
+  }
+  return true;
+}
