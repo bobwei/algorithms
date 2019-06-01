3b1a071cb91b2f7ab40331f30059ed963ac6b30f
Bob Wei <bob.wei@linecorp.com>
2019-06-02 02:16:37 +0800

    Update meeting-rooms-ii

--- src/leetcode/meeting-rooms-ii/index.js
+++ src/leetcode/meeting-rooms-ii/index.js
@@ -1,35 +1,18 @@
 /**
- * Definition for an interval.
- * function Interval(start, end) {
- *     this.start = start;
- *     this.end = end;
- * }
- */
-/**
- * @param {Interval[]} intervals
+ * @param {number[][]} intervals
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-  if (!intervals.length) {
-    return 0;
-  }
-  intervals.sort((a, b) => a.start - b.start);
   const timeline = [];
-  for (let i = 0; i < intervals.length; i++) {
-    timeline.push([intervals[i].start, 1, i]);
-    timeline.push([intervals[i].end, -1, i]);
+  for (const [start, end] of intervals) {
+    timeline.push([start, 1], [end, -1]);
   }
-  timeline.sort((a, b) => {
-    if (a[0] !== b[0]) {
-      return a[0] - b[0];
-    }
-    return a[2] - b[2];
-  });
+  timeline.sort((a, b) => (a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]));
   let max = 0;
-  let n = 0;
-  for (let i = 0; i < timeline.length; i++) {
-    n += timeline[i][1];
-    max = Math.max(max, n);
+  let count = 0;
+  for (const [, delta] of timeline) {
+    count += delta;
+    max = Math.max(max, count);
   }
   return max;
 };
