fdbc11503f2e0acab0015968d52bcc085a88c926
Bob Wei <bob.wei@linecorp.com>
2019-06-06 00:44:56 +0800

    Update longest-palindromic-substring

--- src/leetcode/longest-palindromic-substring/index.js
+++ src/leetcode/longest-palindromic-substring/index.js
@@ -2,38 +2,33 @@
  * @param {string} s
  * @return {string}
  */
-
 /*
   dp[i][j] = s[i] === s[j] && dp[i + 1][j - 1];
 */
-
 var longestPalindrome = function(s) {
   const m = s.length;
   const dp = [...new Array(m)].map(() => new Array(m).fill(false));
   let output = '';
-  for (let i = 0; i < m - 0; i++) {
-    const j = i + 0;
-    dp[i][j] = true;
-    output = max(output, s, dp, i, j);
+  for (let i = 0; i < m; i++) {
+    dp[i][i] = true;
+    if (dp[i][i] && 1 > output.length) {
+      output = s.substring(i, i + 1);
+    }
   }
   for (let i = 0; i < m - 1; i++) {
-    const j = i + 1;
-    dp[i][j] = s[i] === s[j];
-    output = max(output, s, dp, i, j);
+    dp[i][i + 1] = s[i] === s[i + 1];
+    if (dp[i][i + 1] && 2 > output.length) {
+      output = s.substring(i, i + 2);
+    }
   }
-  for (let n = 2; n < m; n++) {
-    for (let i = 0; i < m - n; i++) {
-      const j = i + n;
+  for (let length = 3; length <= m; length++) {
+    for (let i = 0; i <= m - length; i++) {
+      const j = i + length - 1;
       dp[i][j] = s[i] === s[j] && dp[i + 1][j - 1];
-      output = max(output, s, dp, i, j);
+      if (dp[i][j] && j - i + 1 > output.length) {
+        output = s.substring(i, j + 1);
+      }
     }
   }
   return output;
 };
-
-function max(output, s, dp, i, j) {
-  if (dp[i][j] && j - i + 1 > output.length) {
-    return s.substring(i, j + 1);
-  }
-  return output;
-}
