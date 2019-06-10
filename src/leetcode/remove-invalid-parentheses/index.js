9a439b6681bc02c7d5990d799dd045619afa7dd7
Bob Wei <bob.wei@linecorp.com>
2019-06-11 00:41:06 +0800

    Update remove-invalid-parentheses

--- src/leetcode/remove-invalid-parentheses/index.js
+++ src/leetcode/remove-invalid-parentheses/index.js
@@ -3,28 +3,32 @@
  * @return {string[]}
  */
 var removeInvalidParentheses = function(s) {
+  let queue = [s];
   const output = [];
   const visited = new Set();
-  const queue = [s];
   while (queue.length) {
-    const str = queue.shift();
-    if (isValid(str)) {
-      const minLength = output.length > 0 ? output[0].length : -Infinity;
-      if (str.length >= minLength) {
+    const next = [];
+    while (queue.length) {
+      const str = queue.shift();
+      if (isValid(str)) {
         output.push(str);
+      }
+      if (output.length) {
+        continue;
+      }
+      for (let i = 0; i < str.length; i++) {
+        if (!(str[i] === '(' || str[i] === ')')) {
+          continue;
+        }
+        const candidate = str.substring(0, i) + str.substring(i + 1);
+        if (!visited.has(candidate)) {
+          visited.add(candidate);
+          next.push(candidate);
+        }
       }
     }
     if (!output.length) {
-      for (let i = 0; i < str.length; i++) {
-        if (str[i] !== '(' && str[i] !== ')') {
-          continue;
-        }
-        const next = str.substring(0, i) + str.substring(i + 1);
-        if (!visited.has(next)) {
-          queue.push(next);
-          visited.add(next);
-        }
-      }
+      queue = next;
     }
   }
   return output;
