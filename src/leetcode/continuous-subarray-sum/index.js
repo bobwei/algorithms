00fdc5423358063bec89359c0095323057d3b8f9
Bob Wei <bob.wei@linecorp.com>
2019-06-23 02:23:33 +0800

    Update continuous-subarray-sum

--- src/leetcode/continuous-subarray-sum/index.js
+++ src/leetcode/continuous-subarray-sum/index.js
@@ -4,26 +4,23 @@
  * @return {boolean}
  */
 var checkSubarraySum = function(nums, k) {
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length - 1; i++) {
     if (nums[i] === 0 && nums[i + 1] === 0) {
       return true;
     }
   }
-  if (k === 0) {
+  if (!k) {
     return false;
   }
-  k = Math.abs(k);
   const map = { 0: -1 };
   let sum = 0;
   for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
     const r = sum % k;
-    if (r in map) {
-      const j = map[r];
-      if (i - j >= 2) {
-        return true;
-      }
-    } else {
+    if (r in map && i - map[r] > 1) {
+      return true;
+    }
+    if (!(r in map)) {
       map[r] = i;
     }
   }
