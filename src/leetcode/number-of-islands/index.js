0c300c2d6a46d11e87c90e8622fe8782fd3ca256
Bob Wei <bob.wei@linecorp.com>
2019-06-27 14:57:35 +0800

    Update number-of-islands

--- src/leetcode/number-of-islands/index.js
+++ src/leetcode/number-of-islands/index.js
@@ -9,25 +9,25 @@ var numIslands = function(grid) {
   const m = grid.length;
   const n = grid[0].length;
   const visited = [...new Array(m)].map(() => new Array(n).fill(false));
-  let output = 0;
+  let nIslands = 0;
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j] === '1' && !visited[i][j]) {
         dfs(grid, m, n, [i, j], visited);
-        output += 1;
+        nIslands += 1;
       }
     }
   }
-  return output;
+  return nIslands;
 };
 
 const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
-function dfs(grid, m, n, p, visited) {
-  visited[p[0]][p[1]] = true;
+function dfs(grid, m, n, [x, y], visited) {
+  visited[x][y] = true;
   for (const [di, dj] of dirs) {
-    const i = p[0] + di;
-    const j = p[1] + dj;
+    const i = x + di;
+    const j = y + dj;
     if (isValid(i, j, m, n) && !visited[i][j] && grid[i][j] === '1') {
       dfs(grid, m, n, [i, j], visited);
     }
