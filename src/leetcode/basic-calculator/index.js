06cdec845d09e2d280e9bdf0554408c75f95ab1b
Bob Wei <bob.wei@linecorp.com>
2019-06-28 16:12:15 +0800

    Update basic-calculator

--- src/leetcode/basic-calculator/index.js
+++ src/leetcode/basic-calculator/index.js
@@ -2,51 +2,56 @@
  * @param {string} s
  * @return {number}
  */
+var calculate = function(s, start = 0, end = s.length - 1, map = findParentheses(s)) {
+  const nums = [];
+  let op = 1;
+  for (let i = start; i <= end; i++) {
+    if (s[i] === '(') {
+      const result = calculate(s, i + 1, map[i] - 1, map);
+      nums.push(op * result);
+      i = map[i];
+    } else if (s[i] === '+') {
+      op = 1;
+    } else if (s[i] === '-') {
+      op = -1;
+    } else if (isNumber(s[i])) {
+      const str = parseNum(s, i);
+      const result = parseInt(str);
+      nums.push(op * result);
+      i += str.length - 1;
+    }
+    if (nums.length >= 2) {
+      const result = nums.shift() + nums.shift();
+      nums.push(result);
+    }
+  }
+  return nums.reduce((acc, cur) => acc + cur, 0);
+};
 
-/*
-  '1+2+3-4-5'
-  '(1 + 2 + 3 + ( 4 + 5 ))'
-*/
+function isNumber(str) {
+  return /[0-9]+/.test(str);
+}
 
-const createParentheses = (s) => {
+function parseNum(s, start) {
+  let i = start;
+  let output = '';
+  while (isNumber(s[i])) {
+    output += s[i];
+    i += 1;
+  }
+  return output;
+}
+
+function findParentheses(s) {
   const map = {};
   const stack = [];
   for (let i = 0; i < s.length; i++) {
     if (s[i] === '(') {
       stack.push(i);
     } else if (s[i] === ')') {
-      map[stack.pop()] = i;
+      const left = stack.pop();
+      map[left] = i;
     }
   }
   return map;
-};
-
-const isNumber = (c) => /[0-9]/.test(c);
-
-const parseNumberSubstring = (s, start) => {
-  let i = start;
-  while (isNumber(s[i])) {
-    i += 1;
-  }
-  return s.slice(start, i);
-};
-
-var calculate = function(s, parentheses = createParentheses(s), start = 0, end = s.length - 1) {
-  let output = 0;
-  let num = 1;
-  for (let i = start; i <= end; i++) {
-    if (isNumber(s[i])) {
-      const str = parseNumberSubstring(s, i);
-      output += num * parseInt(str);
-      i += str.length - 1;
-    } else if (s[i] === '+') {
-      num = 1;
-    } else if (s[i] === '-') {
-      num = -1;
-    } else if (s[i] === '(') {
-      output += num * calculate(s, parentheses, i + 1, parentheses[i] - 1);
-      i = parentheses[i];
-    }
-  }
-  return output;
-};
+}
