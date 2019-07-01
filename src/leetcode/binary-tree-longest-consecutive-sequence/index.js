660aa15f4a1d93ffe717c352c20d0bc60e76019e
Bob Wei <bob.wei@linecorp.com>
2019-07-01 17:05:16 +0800

    Update binary-tree-longest-consecutive-sequence

--- src/leetcode/binary-tree-longest-consecutive-sequence/index.js
+++ src/leetcode/binary-tree-longest-consecutive-sequence/index.js
@@ -13,10 +13,17 @@ var longestConsecutive = function(root, pre = null, n = 0, max = 0) {
   if (!root) {
     return max;
   }
-  const nConsecutive = pre && root.val === pre.val + 1 ? n + 1 : 1;
+  const nConsecutive = isSeq(pre, root) ? n + 1 : 1;
   max = Math.max(max, nConsecutive);
   return Math.max(
     longestConsecutive(root.left, root, nConsecutive, max),
     longestConsecutive(root.right, root, nConsecutive, max),
   );
 };
+
+function isSeq(pre, root) {
+  if (!pre) {
+    return false;
+  }
+  return pre.val + 1 === root.val;
+}
