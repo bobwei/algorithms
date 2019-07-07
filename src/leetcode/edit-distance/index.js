c6dcf1eed9befb3f76d3b52b052f3674b01e3528
Bob Wei <bob.wei@linecorp.com>
2019-07-08 00:19:15 +0800

    Update edit-distance

--- src/leetcode/edit-distance/index.js
+++ src/leetcode/edit-distance/index.js
@@ -6,17 +6,20 @@
 var minDistance = function(word1, word2) {
   const m = word1.length;
   const n = word2.length;
-  let dp = [...new Array(n + 1)].map((_, i) => i);
+  let dp = new Array(m + 1).fill(0);
+  for (let j = 0; j <= n; j++) {
+    dp[j] = j;
+  }
   for (let i = 1; i <= m; i++) {
-    const arr = new Array(n + 1).fill(0);
-    arr[0] = i;
+    const next = new Array(m + 1).fill(0);
+    next[0] = i;
     for (let j = 1; j <= n; j++) {
       // prettier-ignore
-      arr[j] = word1[i - 1] === word2[j - 1]
+      next[j] = word1[i - 1] === word2[j - 1]
         ? dp[j - 1]
-        : Math.min(arr[j - 1] + 1, dp[j] + 1, dp[j - 1] + 1);
+        : 1 + Math.min(dp[j], next[j - 1], dp[j - 1]);
     }
-    dp = arr;
+    dp = next;
   }
   return dp[n];
 };
