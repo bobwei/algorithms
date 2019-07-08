ae5cf5ff8ffaa6a71eafdcf1d8522f83f72ded1a
Bob Wei <bob.wei@linecorp.com>
2019-07-08 16:14:50 +0800

    Update kth-largest-element-in-a-stream

--- src/leetcode/kth-largest-element-in-a-stream/index.js
+++ src/leetcode/kth-largest-element-in-a-stream/index.js
@@ -2,57 +2,15 @@
  * @param {number} k
  * @param {number[]} nums
  */
-
-const swap = (arr, i, j) => {
-  const tmp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = tmp;
-};
-
-const bubbleUp = (arr, i) => {
-  if (!i) {
-    return;
-  }
-  const p = Math.floor((i - 1) / 2);
-  if (arr[p] > arr[i]) {
-    swap(arr, p, i);
-    bubbleUp(arr, p);
-  }
-};
-
-const bubbleDown = (arr, i) => {
-  const left = 2 * i + 1;
-  const right = 2 * i + 2;
-  const isValidHeap =
-    (left >= arr.length || arr[i] <= arr[left]) &&
-    (right >= arr.length || arr[i] <= arr[right]);
-  if (isValidHeap) {
-    return;
-  }
-  const next = arr[left] < arr[right] || right >= arr.length ? left : right;
-  swap(arr, i, next);
-  bubbleDown(arr, next);
-};
-
-const extractTop = (arr) => {
-  if (arr.length <= 1) {
-    return arr.pop();
-  }
-  arr[0] = arr.pop();
-  bubbleDown(arr, 0);
-};
-
-const KthLargest = function(k, nums) {
-  this.arr = new Array(k).fill(-Infinity);
+var KthLargest = function(k, nums) {
   this.k = k;
-  for (let i = 0; i < nums.length; i++) {
-    const val = nums[i];
-    if (!this.arr.length || val > this.arr[0]) {
-      this.arr.push(val);
-      bubbleUp(this.arr, this.arr.length - 1);
-      if (this.arr.length > this.k) {
-        extractTop(this.arr);
-      }
+  this.pq = new PriorityQueue({
+    comparator: (a, b) => a < b,
+  });
+  for (const num of nums) {
+    this.pq.enqueue(num);
+    if (this.pq.length > k) {
+      this.pq.dequeue();
     }
   }
 };
@@ -62,20 +20,65 @@ const KthLargest = function(k, nums) {
  * @return {number}
  */
 KthLargest.prototype.add = function(val) {
-  if (val > this.arr[0]) {
-    this.arr.push(val);
-    bubbleUp(this.arr, this.arr.length - 1);
-    if (this.arr.length > this.k) {
-      extractTop(this.arr);
-    }
+  this.pq.enqueue(val);
+  if (this.pq.length > this.k) {
+    this.pq.dequeue();
   }
-  return this.arr[0];
+  return this.pq.peek();
 };
 
 /**
  * Your KthLargest object will be instantiated and called as such:
- * var obj = Object.create(KthLargest).createNew(k, nums)
+ * var obj = new KthLargest(k, nums)
  * var param_1 = obj.add(val)
  */
 
-export default KthLargest;
+class PriorityQueue {
+  constructor({ comparator }) {
+    this.comparator = comparator;
+    this.arr = [];
+  }
+
+  enqueue(element) {
+    this.arr.push(element);
+    bubbleUp(this.arr, this.arr.length - 1, this.comparator);
+  }
+
+  dequeue() {
+    const output = this.arr[0];
+    this.arr[0] = this.arr[this.arr.length - 1];
+    this.arr.pop();
+    bubbleDown(this.arr, 0, this.comparator);
+    return output;
+  }
+
+  peek() {
+    return this.arr[0];
+  }
+
+  get length() {
+    return this.arr.length;
+  }
+}
+
+function bubbleUp(arr, i, comparator) {
+  const p = Math.floor((i - 1) / 2);
+  const isValid = p < 0 || comparator(arr[p], arr[i]);
+  if (!isValid) {
+    [arr[i], arr[p]] = [arr[p], arr[i]];
+    bubbleUp(arr, p, comparator);
+  }
+}
+
+function bubbleDown(arr, i, comparator) {
+  const left = 2 * i + 1;
+  const right = 2 * i + 2;
+  const isValid =
+    (left >= arr.length || comparator(arr[i], arr[left])) &&
+    (right >= arr.length || comparator(arr[i], arr[right]));
+  if (!isValid) {
+    const next = right >= arr.length || comparator(arr[left], arr[right]) ? left : right;
+    [arr[i], arr[next]] = [arr[next], arr[i]];
+    bubbleDown(arr, next, comparator);
+  }
+}
