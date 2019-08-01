9a32ff8b1d761e01d7850d37d9031146d51d4498
Bob Wei <bob.wei@linecorp.com>
2019-08-01 16:36:18 +0800

    Update search-a-2d-matrix

--- src/leetcode/search-a-2d-matrix/index.js
+++ src/leetcode/search-a-2d-matrix/index.js
@@ -7,39 +7,38 @@ var searchMatrix = function(matrix, target) {
   if (!matrix.length || !matrix[0].length) {
     return false;
   }
-  const row = rangeSearch(matrix.map((r) => r[0]), target);
-  const col = binarySearch(matrix[row], target);
-  return col >= 0;
+  const m = matrix.length;
+  const n = matrix[0].length;
+  const i = lowerBound(matrix.map((row) => row[n - 1]), target);
+  return binarySearch(matrix[i], target);
 };
 
-function rangeSearch(arr, target) {
+function lowerBound(arr, target) {
   let left = 0;
-  let right = arr.length - 1;
-  while (left <= right) {
+  let right = arr.length;
+  while (left < right) {
     const mid = Math.floor((left + right) / 2);
-    if (target === arr[mid]) {
-      return mid;
-    } else if (target < arr[mid]) {
-      right = mid - 1;
-    } else {
+    if (target > arr[mid]) {
       left = mid + 1;
+    } else {
+      right = mid;
     }
   }
-  return Math.max(0, right);
+  return left < arr.length ? left : arr.length - 1;
 }
 
 function binarySearch(arr, target) {
   let left = 0;
-  let right = arr.length - 1;
-  while (left <= right) {
+  let right = arr.length;
+  while (left < right) {
     const mid = Math.floor((left + right) / 2);
     if (target === arr[mid]) {
-      return mid;
-    } else if (target < arr[mid]) {
-      right = mid - 1;
-    } else {
+      return true;
+    } else if (target > arr[mid]) {
       left = mid + 1;
+    } else {
+      right = mid;
     }
   }
-  return -1;
+  return false;
 }
