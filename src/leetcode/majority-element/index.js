aa49c8a893d425d31904f5d7f03765e0ced5a69b
Bob Wei <bob.wei@linecorp.com>
2019-08-01 18:21:45 +0800

    Update majority-element

--- src/leetcode/majority-element/index.js
+++ src/leetcode/majority-element/index.js
@@ -3,17 +3,14 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-  let candidate;
-  let count = 0;
-  for (const num of nums) {
-    if (num === candidate) {
-      count += 1;
-    } else if (count === 0) {
-      candidate = num;
+  let el = nums[0];
+  let count = 1;
+  for (let i = 1; i < nums.length; i++) {
+    count += nums[i] === el ? 1 : -1;
+    if (count === 0) {
+      el = nums[i];
       count = 1;
-    } else {
-      count -= 1;
     }
   }
-  return candidate;
+  return el;
 };
