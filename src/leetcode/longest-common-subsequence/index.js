2341dd0c5f570e4aa07d4826316b4ecc9caeaa66
Bob Wei <bob.wei@linecorp.com>
2019-08-05 17:01:03 +0800

    Update longest-common-subsequence

--- src/leetcode/longest-common-subsequence/index.js
+++ src/leetcode/longest-common-subsequence/index.js
@@ -10,14 +10,16 @@ var longestCommonSubsequence = function(text1, text2) {
   for (let i = 1; i <= m; i++) {
     const next = new Array(n + 1).fill(0);
     for (let j = 1; j <= n; j++) {
-      next[j] = (() => {
-        if (text1[i - 1] === text2[j - 1]) {
-          return dp[j - 1] + 1;
-        }
-        return Math.max(dp[j], next[j - 1], dp[j - 1]);
-      })();
+      next[j] = helper(dp, next, text1, text2, i, j);
     }
     dp = next;
   }
   return dp[n];
 };
+
+function helper(dp, next, text1, text2, i, j) {
+  if (text1[i - 1] === text2[j - 1]) {
+    return dp[j - 1] + 1;
+  }
+  return Math.max(dp[j], next[j - 1], dp[j - 1]);
+}
