2f0bffc46a48b5639f7840f9142fadcd1b0ba7de
Bob Wei <bob.wei@linecorp.com>
2019-08-06 14:54:45 +0800

    Update multiply-strings

--- src/leetcode/multiply-strings/index.js
+++ src/leetcode/multiply-strings/index.js
@@ -3,39 +3,34 @@
  * @param {string} num2
  * @return {string}
  */
-
-const toNumArray = (str) => {
-  return str
-    .split('')
-    .map((c) => parseInt(c))
-    .reverse();
-};
-
-const trim = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-      return arr.slice(i);
-    }
-  }
-  return arr.slice(0, 1);
-};
-
 var multiply = function(num1, num2) {
+  if (!num1 || !num2) {
+    return '0';
+  }
   const m = num1.length;
   const n = num2.length;
-  const arr1 = toNumArray(num1);
-  const arr2 = toNumArray(num2);
-  const arr = new Array(m + n).fill(0);
+  const output = new Array(m + n).fill(0);
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-      arr[i + j] += arr1[i] * arr2[j];
+      output[i + j] += parseInt(num1[m - 1 - i]) * parseInt(num2[n - 1 - j]);
     }
   }
   let c = 0;
-  for (let i = 0; i < arr.length; i++) {
-    const sum = arr[i] + c;
-    arr[i] = sum % 10;
+  for (let i = 0; i < output.length; i++) {
+    const sum = output[i] + c;
+    output[i] = sum % 10;
     c = Math.floor(sum / 10);
   }
-  return trim(arr.reverse()).join('');
+  return trimZero(output.reverse()).join('');
 };
+
+function trimZero(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let i = 0;
+  while (arr[i] === 0) {
+    i += 1;
+  }
+  return i < arr.length ? arr.slice(i) : [0];
+}
