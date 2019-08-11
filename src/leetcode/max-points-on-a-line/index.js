0182fb4d161adcef3cd487e5da13a390b1198364
Bob Wei <bob.wei@linecorp.com>
2019-08-11 19:25:55 +0800

    Add max-points-on-a-line

--- src/leetcode/max-points-on-a-line/index.js
+++ src/leetcode/max-points-on-a-line/index.js
@@ -3,39 +3,37 @@
  * @return {number}
  */
 var maxPoints = function(points) {
-  if (points.length <= 2) {
-    return points.length;
+  if (!points.length) {
+    return 0;
   }
   const m = points.length;
   let max = 0;
   for (let i = 0; i < m; i++) {
-    const count = {};
-    let nSamePoints = 1;
+    const freq = {};
+    let nEquals = 0;
+    let maxFreq = 0;
     for (let j = i + 1; j < m; j++) {
-      if (encode(points[i]) === encode(points[j])) {
-        nSamePoints += 1;
+      if (isEqual(points[i], points[j])) {
+        nEquals += 1;
         continue;
       }
       const slope = getSlope(points[i], points[j]);
-      count[slope] = (count[slope] || 0) + 1;
+      freq[slope] = (freq[slope] || 0) + 1;
+      maxFreq = Math.max(maxFreq, freq[slope]);
     }
-    // prettier-ignore
-    const localMax = Object
-      .values(count)
-      .reduce((acc, cur) => Math.max(acc, cur), 0);
-    max = Math.max(max, localMax + nSamePoints);
+    max = Math.max(max, 1 + maxFreq + nEquals);
   }
   return max;
 };
-
-function encode(point) {
-  return point + '';
-}
 
 function getSlope(p1, p2) {
   if (p1[0] === p2[0]) {
     return Infinity;
   }
-  // `1000000000000`: hack for overflow issue with large numbers
-  return (1000000000000 * (p2[1] - p1[1])) / (p2[0] - p1[0]);
+  // overcome javascript division precision issue by multipling with a very large number
+  return (1000000000000 * (p1[1] - p2[1])) / (p1[0] - p2[0]);
+}
+
+function isEqual(p1, p2) {
+  return p1[0] === p2[0] && p1[1] === p2[1];
 }
