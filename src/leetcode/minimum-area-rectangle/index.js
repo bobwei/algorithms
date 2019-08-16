1f7eb2f3eef47df5e14495804c072107bc124145
Bob Wei <bob.wei@linecorp.com>
2019-08-16 17:03:18 +0800

    Update minimum-area-rectangle

--- src/leetcode/minimum-area-rectangle/index.js
+++ src/leetcode/minimum-area-rectangle/index.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 var minAreaRect = function(points) {
-  const map = createMap(points);
+  const set = new Set(points.map(createKey));
   let min = Infinity;
   for (const [x1, y1] of points) {
     for (const [x2, y2] of points) {
-      if (x1 === x2 && y1 === y2) {
-        continue;
-      }
-      if (map['x' + x2].has(y1) && map['y' + y2].has(x1)) {
+      if (set.has(createKey([x1, y2])) && set.has(createKey([x2, y1]))) {
         const area = Math.abs(x1 - x2) * Math.abs(y1 - y2);
         if (area) {
           min = Math.min(min, area);
@@ -21,15 +18,6 @@ var minAreaRect = function(points) {
   return min < Infinity ? min : 0;
 };
 
-function createMap(points) {
-  const map = {};
-  for (const [x, y] of points) {
-    const xKey = 'x' + x;
-    const yKey = 'y' + y;
-    if (!(xKey in map)) map[xKey] = new Set();
-    if (!(yKey in map)) map[yKey] = new Set();
-    map[xKey].add(y);
-    map[yKey].add(x);
-  }
-  return map;
+function createKey([x, y]) {
+  return x + ',' + y;
 }
