b9dff1482c18729cfbb51cc3680720c8543f12c7
Bob Wei <bob.wei@linecorp.com>
2019-08-16 17:39:49 +0800

    Update k-closest-points-to-origin

--- src/leetcode/k-closest-points-to-origin/index.js
+++ src/leetcode/k-closest-points-to-origin/index.js
@@ -7,7 +7,7 @@ var kClosest = function(points, K) {
   let left = 0;
   let right = points.length;
   while (left < right) {
-    const p = partition(points, left, right);
+    const p = partition(points, left, right, (a, b) => getDistance(a) < getDistance(b));
     if (K === p + 1) {
       return points.slice(0, K);
     } else if (K > p) {
@@ -19,19 +19,19 @@ var kClosest = function(points, K) {
   return [];
 };
 
-function partition(arr, start, end, compare = (a, b) => dist(a) < dist(b)) {
+function getDistance([x, y]) {
+  return Math.sqrt(x ** 2 + y ** 2);
+}
+
+function partition(arr, start, end, comparator) {
   const p = end - 1;
   let j = start;
-  for (let i = start; i < end - 1; i++) {
-    if (compare(arr[i], arr[p])) {
+  for (let i = start; i < end; i++) {
+    if (comparator(arr[i], arr[p])) {
       [arr[i], arr[j]] = [arr[j], arr[i]];
       j += 1;
     }
   }
   [arr[j], arr[p]] = [arr[p], arr[j]];
   return j;
-}
-
-function dist([x, y]) {
-  return Math.sqrt(x ** 2 + y ** 2);
 }
