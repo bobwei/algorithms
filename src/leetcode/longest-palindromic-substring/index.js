481c545eaca8d001337f5241b970270713773fab
Bob Wei <bob.wei@linecorp.com>
2019-08-19 18:22:00 +0800

    Update longest-palindromic-substring

--- src/leetcode/longest-palindromic-substring/index.js
+++ src/leetcode/longest-palindromic-substring/index.js
@@ -19,7 +19,7 @@ function helper(s, start) {
   while (s[right + 1] === s[left]) {
     right += 1;
   }
-  while (s[left - 1] === s[right + 1] && left >= 0 && right < s.length) {
+  while (s[left - 1] === s[right + 1] && left - 1 >= 0 && right + 1 < s.length) {
     left -= 1;
     right += 1;
   }
