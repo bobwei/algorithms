742d41b3574a069cb8255888c855ba80eaa923ae
Bob Wei <bob61016@gmail.com>
2019-08-28 00:18:08 +0800

    Update perfect-squares

--- src/leetcode/perfect-squares/index.js
+++ src/leetcode/perfect-squares/index.js
@@ -2,33 +2,21 @@
  * @param {number} n
  * @return {number}
  */
-
-/*
-  f(i) = Math.min{f(j^2) + f(i - j^2)} for j in i, break if j^2 > i
-  f(1) = 1;
-  f(2) = 2;
-  f(3) = Math.min{f(1) + f(2)} = 3
-*/
-
-const numSquares = function(n) {
-  const dp = new Array(n + 1).fill(Infinity);
-  dp[0] = 0;
-  for (let i = 1; i <= n; i++) {
-    if (i * i > n) {
-      break;
-    }
-    dp[i * i] = 1;
-  }
-  for (let i = 1; i <= n; i++) {
-    for (let j = 0; j <= i; j++) {
-      if (j * j > i) {
-        break;
-      }
-      const val = dp[j * j] + dp[i - j * j];
-      dp[i] = Math.min(dp[i], val);
-    }
+var numSquares = function(n) {
+  const dp = new Array(n + 1).fill(0);
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = helper(i, dp);
   }
   return dp[n];
 };
 
-export default numSquares;
+function helper(i, dp) {
+  let min = Infinity;
+  let j = 1;
+  while (i - j ** 2 >= 0) {
+    min = Math.min(min, 1 + dp[i - j ** 2]);
+    j += 1;
+  }
+  return min;
+}
