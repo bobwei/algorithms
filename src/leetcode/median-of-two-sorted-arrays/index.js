5002843cc1df47550159f8a1a7dccb8c032009fe
Bob Wei <bob61016@gmail.com>
2019-09-01 00:23:45 +0800

    Update median-of-two-sorted-arrays

--- src/leetcode/median-of-two-sorted-arrays/index.js
+++ src/leetcode/median-of-two-sorted-arrays/index.js
@@ -3,32 +3,35 @@
  * @param {number[]} nums2
  * @return {number}
  */
+var findMedianSortedArrays = function(nums1, nums2) {
+  const n = nums1.length + nums2.length;
+  const k = Math.ceil(n / 2);
+  if (n % 2 === 1) {
+    return findKth(nums1, nums2, k);
+  }
+  return (findKth(nums1, nums2, k) + findKth(nums1, nums2, k + 1)) / 2;
+};
 
-const findKth = (arr1, arr2, k) => {
+function findKth(arr1, arr2, k) {
   if (arr1.length > arr2.length) {
     return findKth(arr2, arr1, k);
   }
   let left = 0;
-  let right = arr1.length - 1;
-  while (right >= left - 1) {
-    const p1 = Math.floor((left + right) / 2);
-    const p2 = k - p1 - 2;
-    const maxLeft = Math.max(arr1[p1] || -Infinity, arr2[p2] || -Infinity);
-    const minRight = Math.min(arr1[p1 + 1] || Infinity, arr2[p2 + 1] || Infinity);
-    if (maxLeft <= minRight) {
-      return maxLeft;
-    } else if (arr1[p1] > arr2[p2 + 1]) {
-      right = p1 - 1;
-    } else if (arr2[p2] > arr1[p1 + 1]) {
-      left = p1 + 1;
+  let right = arr1.length;
+  while (left < right) {
+    const m = Math.floor((left + right) / 2);
+    const n = k - m - 2;
+    const max1 = m in arr1 ? arr1[m] : -Infinity;
+    const max2 = n in arr2 ? arr2[n] : -Infinity;
+    const min1 = m + 1 in arr1 ? arr1[m + 1] : Infinity;
+    const min2 = n + 1 in arr2 ? arr2[n + 1] : Infinity;
+    if (Math.max(max1, max2) <= Math.min(min1, min2)) {
+      return Math.max(max1, max2);
+    } else if (arr1[m + 1] < arr2[n]) {
+      left = m + 1;
+    } else {
+      right = m;
     }
   }
-};
-
-var findMedianSortedArrays = function(nums1, nums2) {
-  const n = nums1.length + nums2.length;
-  const k = Math.ceil(n / 2);
-  return n % 2 === 0
-    ? (findKth(nums1, nums2, k) + findKth(nums1, nums2, k + 1)) / 2
-    : findKth(nums1, nums2, k);
-};
+  return arr2[k - 1];
+}
