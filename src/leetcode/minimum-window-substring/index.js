e68ed9c451ee0afc5e09c2a67916d63639e06e53
Bob Wei <bob.wei@linecorp.com>
2019-09-23 18:53:25 +0800

    Update minimum-window-substring

--- src/leetcode/minimum-window-substring/index.js
+++ src/leetcode/minimum-window-substring/index.js
@@ -6,42 +6,47 @@
 var minWindow = function(s, t) {
   const counter = new Counter(t);
   let start = 0;
-  let min = Infinity;
-  let str = '';
+  let output = s.repeat(2);
   for (let i = 0; i < s.length; i++) {
     counter.add(s[i]);
-    while (counter.nRemaining <= 0) {
-      if (i - start + 1 < min) {
-        min = i - start + 1;
-        str = s.substring(start, i + 1);
+    while (counter.nIncomplete <= 0) {
+      const substringLength = i - start + 1;
+      if (substringLength < output.length) {
+        output = s.substring(start, i + 1);
       }
       counter.delete(s[start]);
       start += 1;
     }
   }
-  return min < Infinity ? str : '';
+  return output.length <= s.length ? output : '';
 };
 
 class Counter {
-  constructor(t) {
-    this.nRemaining = t.length;
+  constructor(target) {
     this.freq = {};
-    for (const c of t) {
+    this.nIncomplete = target.length;
+    for (const c of target) {
       this.freq[c] = (this.freq[c] || 0) + 1;
     }
   }
 
   add(c) {
-    this.freq[c] = (this.freq[c] || 0) - 1;
+    if (!(c in this.freq)) {
+      return;
+    }
+    this.freq[c] -= 1;
     if (this.freq[c] >= 0) {
-      this.nRemaining -= 1;
+      this.nIncomplete -= 1;
     }
   }
 
   delete(c) {
+    if (!(c in this.freq)) {
+      return;
+    }
     this.freq[c] += 1;
     if (this.freq[c] > 0) {
-      this.nRemaining += 1;
+      this.nIncomplete += 1;
     }
   }
 }
