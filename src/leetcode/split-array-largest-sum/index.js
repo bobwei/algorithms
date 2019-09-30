c7c1978c24e705e52faee2f14c53dec048a4f770
Bob Wei <bob.wei@linecorp.com>
2019-10-01 02:01:11 +0800

    Update split-array-largest-sum

--- src/leetcode/split-array-largest-sum/index.js
+++ src/leetcode/split-array-largest-sum/index.js
@@ -4,26 +4,17 @@
  * @return {number}
  */
 var splitArray = function(nums, m) {
-  const [max, sum] = findMaxAndSum(nums);
-  return binarySearch(nums, m, max, sum);
+  const max = Math.max(...nums);
+  const sum = nums.reduce((acc, cur) => acc + cur, 0);
+  return binarySearch(nums, max, sum, m);
 };
 
-function findMaxAndSum(nums) {
-  let max = -Infinity;
-  let sum = 0;
-  for (const num of nums) {
-    max = Math.max(max, num);
-    sum += num;
-  }
-  return [max, sum];
-}
-
-function binarySearch(nums, m, min, max) {
+function binarySearch(nums, min, max, m) {
   let left = min;
-  let right = max;
+  let right = max + 1;
   while (left < right) {
     const mid = Math.floor((left + right) / 2);
-    if (isValid(nums, m, mid)) {
+    if (canSplit(nums, mid, m)) {
       right = mid;
     } else {
       left = mid + 1;
@@ -32,18 +23,18 @@ function binarySearch(nums, m, min, max) {
   return left;
 }
 
-function isValid(nums, m, max) {
+function canSplit(nums, max, m) {
   let sum = 0;
-  let count = 1;
+  let n = 1;
   for (const num of nums) {
-    sum += num;
-    if (sum > max) {
-      sum = num;
-      count += 1;
-      if (count > m) {
+    if (sum + num > max) {
+      sum = 0;
+      n += 1;
+      if (n > m) {
         return false;
       }
     }
+    sum += num;
   }
   return true;
 }
