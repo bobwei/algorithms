09c4de80fd1934e7f6c55192d9bf92655c503819
Bob Wei <bob.wei@linecorp.com>
2019-10-09 12:00:44 +0800

    Update minimum-domino-rotations-for-equal-row

--- src/leetcode/minimum-domino-rotations-for-equal-row/index.js
+++ src/leetcode/minimum-domino-rotations-for-equal-row/index.js
@@ -6,21 +6,22 @@
 var minDominoRotations = function(A, B) {
   // prettier-ignore
   const min = Math.min(
-    count(A[0], A, B),
-    count(B[0], A, B),
-    count(A[0], B, A),
-    count(B[0], B, A),
+    count(A, B, A[0]),
+    count(A, B, B[0]),
+    count(B, A, A[0]),
+    count(B, A, B[0]),
   );
   return min < Infinity ? min : -1;
 };
 
-function count(target, A, B) {
+function count(arr1, arr2, target) {
+  const m = arr1.length;
   let n = 0;
-  for (let i = 0; i < A.length; i++) {
-    if (A[i] !== target) {
-      if (B[i] !== target) {
-        return Infinity;
-      }
+  for (let i = 0; i < m; i++) {
+    if (arr1[i] !== target && arr2[i] !== target) {
+      return Infinity;
+    }
+    if (arr1[i] !== target) {
       n += 1;
     }
   }
