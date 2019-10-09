1b99718e53f2795db41db32725d9e87b19a01d95
Bob Wei <bob.wei@linecorp.com>
2019-10-09 12:26:58 +0800

    Update insert-interval

--- src/leetcode/insert-interval/index.js
+++ src/leetcode/insert-interval/index.js
@@ -1,40 +1,32 @@
-/* global Interval */
 /**
- * Definition for an interval.
- * function Interval(start, end) {
- *     this.start = start;
- *     this.end = end;
- * }
- */
-/**
- * @param {Interval[]} intervals
- * @param {Interval} newInterval
- * @return {Interval[]}
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-  if (!intervals.length) {
-    return [newInterval];
-  }
   const output = [];
-  let i2 = newInterval;
-  for (const i1 of intervals) {
-    const isMergeNeeded = i1.end >= i2.start && i2.end >= i1.start;
-    if (isMergeNeeded) {
-      i2 = merge(i1, i2);
+  let ptr = newInterval;
+  for (const interval of intervals) {
+    if (isOverlapped(interval, ptr)) {
+      ptr = merge(interval, ptr);
     } else {
-      const min = i1.start < i2.start ? i1 : i2;
-      const max = i1.start >= i2.start ? i1 : i2;
+      const min = interval[0] < ptr[0] ? interval : ptr;
+      const max = interval[0] > ptr[0] ? interval : ptr;
       output.push(min);
-      i2 = max;
+      ptr = max;
     }
   }
-  output.push(i2);
+  output.push(ptr);
   return output;
 };
 
-function merge(i1, i2) {
-  const tmp = new Interval();
-  tmp.start = Math.min(i1.start, i2.start);
-  tmp.end = Math.max(i1.end, i2.end);
-  return tmp;
+function merge(interval1, interval2) {
+  return [Math.min(interval1[0], interval2[0]), Math.max(interval1[1], interval2[1])];
+}
+
+function isOverlapped(interval1, interval2) {
+  if (interval1[0] > interval2[0]) {
+    return isOverlapped(interval2, interval1);
+  }
+  return interval2[0] <= interval1[1] && interval2[1] >= interval1[0];
 }
