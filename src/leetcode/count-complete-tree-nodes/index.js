4cb4417a3290aa0b692a350ba81476b545697110
Bob Wei <bob.wei@linecorp.com>
2019-10-15 01:27:49 +0800

    Update count-complete-tree-nodes

--- src/leetcode/count-complete-tree-nodes/index.js
+++ src/leetcode/count-complete-tree-nodes/index.js
@@ -13,29 +13,26 @@ var countNodes = function(root) {
   if (!root) {
     return 0;
   }
-  const result = isFullTree(root);
-  if (result.isFullTree) {
-    return 2 ** result.height - 1;
+  const [nLeft, nRight] = helper(root);
+  if (nLeft === nRight) {
+    return 2 ** nLeft - 1;
   }
   return 1 + countNodes(root.left) + countNodes(root.right);
 };
 
-function isFullTree(root) {
+function helper(root) {
   let ptr;
   ptr = root;
-  let left = 0;
+  let nLeft = 0;
   while (ptr) {
-    left += 1;
     ptr = ptr.left;
+    nLeft += 1;
   }
   ptr = root;
-  let right = 0;
+  let nRight = 0;
   while (ptr) {
-    right += 1;
     ptr = ptr.right;
+    nRight += 1;
   }
-  return {
-    height: Math.max(left, right),
-    isFullTree: left === right,
-  };
+  return [nLeft, nRight];
 }
