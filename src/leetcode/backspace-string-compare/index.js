060fdc8653dffd5a11b0bdc5aa54103e36f5ff49
Bob Wei <bob.wei@linecorp.com>
2019-10-15 01:23:19 +0800

    Update backspace-string-compare

--- src/leetcode/backspace-string-compare/index.js
+++ src/leetcode/backspace-string-compare/index.js
@@ -3,45 +3,31 @@
  * @param {string} T
  * @return {boolean}
  */
-/*
-  S = "xywrrmp", T = "xywrrmu#p"
-*/
-const backspaceCompare = function(S, T) {
+var backspaceCompare = function(S, T) {
   let i = S.length - 1;
   let j = T.length - 1;
   while (i >= 0 || j >= 0) {
-    let skips;
-    skips = 0;
-    while (i >= 0) {
-      if (S[i] === '#') {
-        i -= 1;
-        skips += 1;
-      } else if (skips > 0) {
-        i -= 1;
-        skips -= 1;
-      } else {
-        break;
-      }
-    }
-    skips = 0;
-    while (j >= 0) {
-      if (T[j] === '#') {
-        j -= 1;
-        skips += 1;
-      } else if (skips > 0) {
-        j -= 1;
-        skips -= 1;
-      } else {
-        break;
-      }
-    }
+    i -= count(S, i);
+    j -= count(T, j);
     if (S[i] !== T[j]) {
       return false;
     }
     i -= 1;
     j -= 1;
   }
-  return true;
+  return i < 0 && j < 0;
 };
 
-export default backspaceCompare;
+function count(str, start) {
+  let i = start;
+  let n = 0;
+  while (str[i] === '#' || n > 0) {
+    if (str[i] === '#') {
+      n += 1;
+    } else {
+      n -= 1;
+    }
+    i -= 1;
+  }
+  return start - i;
+}
--- src/leetcode/backspace-string-compare/index.spec.js
+++
@@ -1,9 +0,0 @@
-import fn from './index';
-
-test('backspace-string-compare', () => {
-  expect(fn('ab#c', 'ad#c')).toEqual(true);
-});
-
-test('backspace-string-compare', () => {
-  expect(fn('xywrrmp', 'xywrrmu#p')).toEqual(true);
-});
