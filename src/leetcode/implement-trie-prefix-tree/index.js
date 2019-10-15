8d9a40056dc0dcde4d680eaa2c05350d4a274d4b
Bob Wei <bob.wei@linecorp.com>
2019-10-15 16:24:01 +0800

    Update implement-trie-prefix-tree

--- src/leetcode/implement-trie-prefix-tree/index.js
+++ src/leetcode/implement-trie-prefix-tree/index.js
@@ -1,41 +1,69 @@
-const Node = function() {
-  this.chars = {};
-  this.isWord = false;
-};
+class Node {
+  constructor() {
+    this.chars = {};
+    this.isEnd = false;
+  }
+}
 
+/**
+ * Initialize your data structure here.
+ */
 var Trie = function() {
   this.root = new Node();
 };
 
+/**
+ * Inserts a word into the trie.
+ * @param {string} word
+ * @return {void}
+ */
 Trie.prototype.insert = function(word) {
   let ptr = this.root;
-  for (let i = 0; i < word.length; i++) {
-    if (!ptr.chars[word[i]]) {
-      ptr.chars[word[i]] = new Node();
+  for (const c of word) {
+    if (!(c in ptr.chars)) {
+      ptr.chars[c] = new Node();
     }
-    ptr = ptr.chars[word[i]];
+    ptr = ptr.chars[c];
   }
-  ptr.isWord = true;
+  ptr.isEnd = true;
 };
 
+/**
+ * Returns if the word is in the trie.
+ * @param {string} word
+ * @return {boolean}
+ */
 Trie.prototype.search = function(word) {
   let ptr = this.root;
-  for (let i = 0; i < word.length; i++) {
-    if (!ptr.chars[word[i]]) {
+  for (const c of word) {
+    if (!(c in ptr.chars)) {
       return false;
     }
-    ptr = ptr.chars[word[i]];
+    ptr = ptr.chars[c];
   }
-  return ptr.isWord;
+  return ptr.isEnd;
 };
 
+/**
+ * Returns if there is any word in the trie that starts with the given prefix.
+ * @param {string} prefix
+ * @return {boolean}
+ */
 Trie.prototype.startsWith = function(prefix) {
   let ptr = this.root;
-  for (let i = 0; i < prefix.length; i++) {
-    if (!ptr.chars[prefix[i]]) {
+  for (const c of prefix) {
+    if (!(c in ptr.chars)) {
       return false;
     }
-    ptr = ptr.chars[prefix[i]];
+    ptr = ptr.chars[c];
   }
   return true;
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
