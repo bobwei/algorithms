3f44694ff432be6ddb79922fe2062a6ee9883bff
Bob Wei <bob.wei@linecorp.com>
2019-10-20 22:47:15 +0800

    Update remove-sub-folders-from-the-filesystem

--- src/leetcode/remove-sub-folders-from-the-filesystem/index.js
+++ src/leetcode/remove-sub-folders-from-the-filesystem/index.js
@@ -3,45 +3,14 @@
  * @return {string[]}
  */
 var removeSubfolders = function(folder) {
-  const trie = new Trie();
+  folder.sort();
+  const output = [];
+  let parent = null;
   for (const path of folder) {
-    trie.add(path.substring(1).split('/'));
+    if (!path.startsWith(parent)) {
+      output.push(path);
+      parent = path + '/';
+    }
   }
-  return folder.filter((path) => trie.search(path.substring(1).split('/')));
+  return output;
 };
-
-class Node {
-  constructor() {
-    this.chars = {};
-    this.isEnd = false;
-  }
-}
-
-class Trie {
-  constructor() {
-    this.root = new Node();
-  }
-
-  add(word) {
-    let ptr = this.root;
-    for (const c of word) {
-      if (!(c in ptr.chars)) {
-        ptr.chars[c] = new Node();
-      }
-      ptr = ptr.chars[c];
-    }
-    ptr.isEnd = true;
-  }
-
-  search(word) {
-    let ptr = this.root;
-    for (let i = 0; i < word.length; i++) {
-      const c = word[i];
-      ptr = ptr.chars[c];
-      if (ptr.isEnd && i !== word.length - 1) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
