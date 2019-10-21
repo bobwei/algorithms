652faa43f01041b8af6c98a9a3c1df492808750d
Bob Wei <bob.wei@linecorp.com>
2019-10-21 17:53:52 +0800

    Update bulls-and-cows

--- src/leetcode/bulls-and-cows/index.js
+++ src/leetcode/bulls-and-cows/index.js
@@ -5,26 +5,26 @@
  */
 var getHint = function(secret, guess) {
   const freq = createFreq(secret);
-  const aSet = new Set();
+  let nA = 0;
   for (let i = 0; i < guess.length; i++) {
-    if (secret[i] === guess[i]) {
-      aSet.add(i);
+    if (guess[i] === secret[i]) {
+      nA += 1;
       freq[guess[i]] -= 1;
     }
   }
   let nB = 0;
   for (let i = 0; i < guess.length; i++) {
-    if (!aSet.has(i) && freq[guess[i]] > 0) {
+    if (guess[i] !== secret[i] && freq[guess[i]] > 0) {
       nB += 1;
       freq[guess[i]] -= 1;
     }
   }
-  return `${aSet.size}A${nB}B`;
+  return `${nA}A${nB}B`;
 };
 
-function createFreq(str) {
+function createFreq(secret) {
   const freq = {};
-  for (const c of str) {
+  for (const c of secret) {
     freq[c] = (freq[c] || 0) + 1;
   }
   return freq;
