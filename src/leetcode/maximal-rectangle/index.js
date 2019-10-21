a444795b31d79a51d0a2c7cc883c7e9713eebd99
Bob Wei <bob.wei@linecorp.com>
2019-10-21 23:46:36 +0800

    Update maximal-rectangle

--- src/leetcode/maximal-rectangle/index.js
+++ src/leetcode/maximal-rectangle/index.js
@@ -8,22 +8,22 @@ var maximalRectangle = function(matrix) {
   }
   const m = matrix.length;
   const n = matrix[0].length;
-  const histogram = matrix[0].map((c) => parseInt(c));
-  let max = getLargestRectangleInHistogram(histogram);
-  for (let i = 1; i < m; i++) {
+  const arr = [...new Array(n).fill(0)];
+  let max = 0;
+  for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-      histogram[j] = matrix[i][j] === '1' ? histogram[j] + 1 : 0;
+      arr[j] = matrix[i][j] === '1' ? arr[j] + 1 : 0;
     }
-    max = Math.max(max, getLargestRectangleInHistogram(histogram));
+    max = Math.max(max, findMax(arr));
   }
   return max;
 };
 
-function getLargestRectangleInHistogram(heights) {
-  const stack = new Stack();
+function findMax(heights) {
+  const stack = [];
   let max = 0;
   for (let i = 0; i < heights.length; i++) {
-    while (stack.length && heights[stack.peek] > heights[i]) {
+    while (stack.length && heights[i] < heights[stack[stack.length - 1]]) {
       const area = getArea(heights, stack, i);
       max = Math.max(max, area);
     }
@@ -37,14 +37,8 @@ function getLargestRectangleInHistogram(heights) {
 }
 
 function getArea(heights, stack, i) {
-  const j = stack.pop();
-  const width = stack.length ? i - stack.peek - 1 : i;
-  const height = heights[j];
-  return width * height;
-}
-
-class Stack extends Array {
-  get peek() {
-    return this[this.length - 1];
-  }
+  const height = heights[stack.pop()];
+  const width = stack.length ? i - stack[stack.length - 1] - 1 : i;
+  const area = width * height;
+  return area;
 }
