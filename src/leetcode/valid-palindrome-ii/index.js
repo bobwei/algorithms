f4fa284e7230ade00e855674b0efa6c5101e369a
Bob Wei <bob.wei@linecorp.com>
2019-10-22 11:25:48 +0800

    Update valid-palindrome-ii

--- src/leetcode/valid-palindrome-ii/index.js
+++ src/leetcode/valid-palindrome-ii/index.js
@@ -2,20 +2,21 @@
  * @param {string} s
  * @return {boolean}
  */
-
-var validPalindrome = function(s, start = 0, end = s.length - 1, nDeletes = 1) {
+var validPalindrome = function(s, start = 0, end = s.length - 1, nDeletions = 1) {
   let left = start;
   let right = end;
   while (left < right) {
-    if (s[left] === s[right]) {
-      left += 1;
-      right -= 1;
-    } else {
-      if (nDeletes > 0) {
-        return validPalindrome(s, left + 1, right, 0) || validPalindrome(s, left, right - 1, 0);
+    if (s[left] !== s[right]) {
+      if (nDeletions <= 0) {
+        return false;
       }
-      return false;
+      return (
+        validPalindrome(s, left, right - 1, nDeletions - 1) ||
+        validPalindrome(s, left + 1, right, nDeletions - 1)
+      );
     }
+    left += 1;
+    right -= 1;
   }
   return true;
 };
