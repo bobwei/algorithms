46a1206108cd70a8bb4da15e320aa1611b14c0f7
Bob Wei <bob.wei@linecorp.com>
2019-10-26 17:24:08 +0800

    Update student-attendance-record-i

--- src/leetcode/student-attendance-record-i/index.js
+++ src/leetcode/student-attendance-record-i/index.js
@@ -7,10 +7,8 @@ var checkRecord = function(s) {
   for (let i = 0; i < s.length; i++) {
     if (s[i] === 'A') {
       nA += 1;
-      if (nA > 1) {
-        return false;
-      }
-    } else if (s[i] === 'L' && s[i - 1] === 'L' && s[i - 2] === 'L') {
+    }
+    if ((i >= 2 && s[i - 2] === 'L' && s[i - 1] === 'L' && s[i] === 'L') || nA > 1) {
       return false;
     }
   }
