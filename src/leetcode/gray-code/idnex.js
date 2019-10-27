57099fbc2be6e57f02e88a9d0926ae62854412cb
Bob Wei <bob.wei@linecorp.com>
2019-10-27 16:21:20 +0800

    Update gray-code

--- src/leetcode/gray-code/idnex.js
+++ src/leetcode/gray-code/idnex.js
@@ -2,32 +2,14 @@
  * @param {number} n
  * @return {number[]}
  */
-
-const toDec = (arr) => {
-  let output = 0;
-  for (let i = 0; i < arr.length; i++) {
-    output += 2 ** i * arr[i];
-  }
-  return output;
-};
-
-const helper = (n, arr = new Array(n).fill(0), output = new Set()) => {
-  if (output.size >= 2 ** n) {
-    return output;
-  }
-  const num = toDec(arr);
-  if (output.has(num)) {
-    return output;
-  }
-  output.add(num);
-  for (let i = 0; i < n; i++) {
-    arr[i] = arr[i] ^ 1;
-    helper(n, arr, output);
-    arr[i] = arr[i] ^ 1;
-  }
-  return output;
-};
-
 var grayCode = function(n) {
-  return [...helper(n)];
+  const dp = [0];
+  for (let i = 0; i < n; i++) {
+    const next = dp.slice().reverse();
+    for (let j = 0; j < next.length; j++) {
+      next[j] += 2 ** i;
+    }
+    dp.push(...next);
+  }
+  return dp;
 };
