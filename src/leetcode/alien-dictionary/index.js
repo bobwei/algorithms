0118c7a1aba00243e30a50ab153b2ecbbc618650
Bob Wei <bob.wei@linecorp.com>
2019-10-28 18:06:09 +0800

    Update alien-dictionary

--- src/leetcode/alien-dictionary/index.js
+++ src/leetcode/alien-dictionary/index.js
@@ -5,49 +5,45 @@
 var alienOrder = function(words) {
   const graph = createGraph(words);
   const visited = new Set();
-  const stack = new Set();
+  const output = new Set();
   for (const u in graph) {
-    if (hasCycle(graph, u, visited, stack)) {
+    if (hasCycle(graph, u, visited, output)) {
       return '';
     }
   }
-  return [...stack].reverse().join('');
+  return [...output].reverse().join('');
 };
+
+function hasCycle(graph, u, visited, output) {
+  if (visited.has(u)) {
+    return !output.has(u);
+  }
+  visited.add(u);
+  for (const v of graph[u]) {
+    if (hasCycle(graph, v, visited, output)) {
+      return true;
+    }
+  }
+  output.add(u);
+  return false;
+}
 
 function createGraph(words) {
   const graph = {};
   for (const word of words) {
-    for (const c of word.split('')) {
-      if (!(c in graph)) {
-        graph[c] = [];
-      }
+    for (const c of word) {
+      graph[c] = [];
     }
   }
-  const n = words.length;
-  for (let i = 0; i <= n - 2; i++) {
+  for (let i = 0; i < words.length - 1; i++) {
     let j = 0;
-    while (words[i][j] && words[i + 1][j] && words[i][j] === words[i + 1][j]) {
+    while (j < words[i].length && j < words[i + 1].length && words[i][j] === words[i + 1][j]) {
       j += 1;
     }
-    const c1 = words[i][j];
-    const c2 = words[i + 1][j];
-    if (c1 && c2 && c1 !== c2) {
-      graph[c1].push(c2);
+    if (j < words[i].length) {
+      const c = words[i][j];
+      graph[c].push(words[i + 1][j]);
     }
   }
   return graph;
-}
-
-function hasCycle(graph, u, visited, stack) {
-  if (visited.has(u)) {
-    return !stack.has(u);
-  }
-  visited.add(u);
-  for (const v of graph[u]) {
-    if (hasCycle(graph, v, visited, stack)) {
-      return true;
-    }
-  }
-  stack.add(u);
-  return false;
 }
