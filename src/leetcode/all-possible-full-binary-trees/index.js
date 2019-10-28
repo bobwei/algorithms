317e2577f77d1aabb53ab98683305bfe850ba1ed
Bob Wei <bob61016@gmail.com>
2019-10-29 00:40:26 +0800

    Update all-possible-full-binary-trees

--- src/leetcode/all-possible-full-binary-trees/index.js
+++ src/leetcode/all-possible-full-binary-trees/index.js
@@ -17,14 +17,14 @@ var allPossibleFBT = function(N) {
     return [new TreeNode(0)];
   }
   const output = [];
-  for (let i = 1; i <= N - 2; i += 2) {
-    const left = allPossibleFBT(i);
-    const right = allPossibleFBT(N - 1 - i);
-    for (const leftNode of left) {
-      for (const rightNode of right) {
+  for (let i = 1; i <= N - 1; i += 2) {
+    const lefts = allPossibleFBT(i);
+    const rights = allPossibleFBT(N - 1 - i);
+    for (const left of lefts) {
+      for (const right of rights) {
         const root = new TreeNode(0);
-        root.left = leftNode;
-        root.right = rightNode;
+        root.left = left;
+        root.right = right;
         output.push(root);
       }
     }
