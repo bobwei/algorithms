09feece497a4ad5a8393287b66fba0af46f18c25
Bob Wei <bob.wei@linecorp.com>
2019-10-31 12:02:57 +0800

    Fix incorrect answer

--- src/practices/count-integer-partitions/index.js
+++ src/practices/count-integer-partitions/index.js
@@ -1,9 +1,12 @@
-function countIntegerPartitions(n) {
+function countIntegerPartitions2(n) {
+  const dp = new Array(n + 1).fill(0);
   const sum = new Array(n + 1).fill(0);
+  dp[1] = 1;
   sum[1] = 1;
   for (let i = 2; i <= n; i++) {
     const min = Math.floor(i / 2);
-    sum[i] = sum[i - 1] - sum[min - 1];
+    dp[i] = sum[i - 1] - sum[min - 1];
+    sum[i] = sum[i - 1] + dp[i];
   }
-  return sum[n];
+  return dp[n];
 }
