692341473579fe94400578bc8199314994f5ace6
Bob Wei <bob.wei@linecorp.com>
2019-11-02 15:26:36 +0800

    Update decode-ways

--- src/leetcode/decode-ways/index.js
+++ src/leetcode/decode-ways/index.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var numDecodings = function(s) {
+  let dp = parseInt(s[0]) > 0 ? 1 : 0;
   let x = 1;
-  let y = parseInt(s[0]) > 0 ? 1 : 0;
-  let output = y;
+  let y = dp;
   for (let i = 2; i <= s.length; i++) {
     // prettier-ignore
-    output = (isDecodable(s.substring(i - 1, i)) ? y : 0) +
+    dp = (isDecodable(s.substring(i - 1, i)) ? y : 0) +
       (isDecodable(s.substring(i - 2, i)) ? x : 0);
     x = y;
-    y = output;
+    y = dp;
   }
-  return output;
+  return dp;
 };
 
 function isDecodable(s) {
