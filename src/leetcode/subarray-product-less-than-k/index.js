5f3f5fffc3b4b3ad0c063bf0f27085d9cda54379
Bob Wei <bob.wei@linecorp.com>
2019-11-04 18:07:53 +0800

    Update subarray-product-less-than-k

--- src/leetcode/subarray-product-less-than-k/index.js
+++ src/leetcode/subarray-product-less-than-k/index.js
@@ -3,29 +3,20 @@
  * @param {number} k
  * @return {number}
  */
-
-/*
-  nums = [10, 5, 2, 6]
-  k = 100
-*/
-
 var numSubarrayProductLessThanK = function(nums, k) {
   if (k <= 1) {
     return 0;
   }
+  let start = 0;
   let product = 1;
   let count = 0;
-  let left = 0;
-  let length = 0;
   for (let i = 0; i < nums.length; i++) {
     product *= nums[i];
-    length += 1;
     while (product >= k) {
-      product /= nums[left];
-      left += 1;
-      length -= 1;
+      product /= nums[start];
+      start += 1;
     }
-    count += length;
+    count += i - start + 1;
   }
   return count;
 };
