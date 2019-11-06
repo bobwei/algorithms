ae5ce72f0edcef51a76dd6bac2a1bf6b9638f230
Bob Wei <bob.wei@linecorp.com>
2019-11-07 01:27:34 +0800

    Update regular-expression-matching

--- src/leetcode/regular-expression-matching/index.js
+++ src/leetcode/regular-expression-matching/index.js
@@ -8,22 +8,25 @@ var isMatch = function(s, p) {
   const n = p.length;
   let dp = new Array(n + 1).fill(false);
   dp[0] = true;
-  for (let j = 2; j <= n; j++) {
+  for (let j = 1; j <= n; j++) {
     dp[j] = p[j - 1] === '*' && dp[j - 2];
   }
   for (let i = 1; i <= m; i++) {
     const next = new Array(n + 1).fill(false);
     for (let j = 1; j <= n; j++) {
-      next[j] = (() => {
-        if (p[j - 1] === '*') {
-          // prettier-ignore
-          return (s[i - 1] === p[j - 2] || p[j - 2] === '.') && dp[j]
-            || next[j - 2];
-        }
-        return (s[i - 1] === p[j - 1] || p[j - 1] === '.') && dp[j - 1];
-      })();
+      next[j] = helper(s, p, i, j, dp, next);
     }
     dp = next;
   }
   return dp[n];
 };
+
+function helper(s, p, i, j, dp, next) {
+  if (s[i - 1] === p[j - 1] || p[j - 1] === '.') {
+    return dp[j - 1];
+  }
+  if (p[j - 1] === '*') {
+    return ((s[i - 1] === p[j - 2] || p[j - 2] === '.') && dp[j]) || next[j - 2];
+  }
+  return false;
+}
