5098a9fc0e1a08f17e76123bda263d4c7419aefe
Bob Wei <bob.wei@linecorp.com>
2019-11-07 11:16:07 +0800

    Update merge-sort

--- src/algo/merge-sort/index.js
+++ src/algo/merge-sort/index.js
@@ -1,10 +1,10 @@
-const sort = (arr, start = 0, end = arr.length, comparator = (a, b) => a - b) => {
+function mergeSort(arr, start = 0, end = arr.length, comparator = defaultComparator) {
   if (end - start <= 1) {
     return arr.slice(start, end);
   }
   const mid = Math.floor((start + end) / 2);
-  const left = sort(arr, start, mid);
-  const right = sort(arr, mid, end);
+  const left = mergeSort(arr, start, mid, comparator);
+  const right = mergeSort(arr, mid, end, comparator);
   const output = [];
   while (left.length && right.length) {
     if (comparator(left[0], right[0]) <= 0) {
@@ -13,9 +13,12 @@ const sort = (arr, start = 0, end = arr.length, comparator = (a, b) => a - b) =>
       output.push(right.shift());
     }
   }
-  output.push(...left);
-  output.push(...right);
+  output.push(...left, ...right);
   return output;
-};
+}
 
-export default sort;
+function defaultComparator(a, b) {
+  return a - b;
+}
+
+export default mergeSort;
