f98f2d8f482f9f12eb218563f0a24f01fc9d494c
Bob Wei <bob.wei@linecorp.com>
2019-11-11 23:59:20 +0800

    Update maximum-size-subarray-sum-equals-k

--- src/leetcode/maximum-size-subarray-sum-equals-k/index.js
+++ src/leetcode/maximum-size-subarray-sum-equals-k/index.js
@@ -5,16 +5,16 @@
  */
 var maxSubArrayLen = function(nums, k) {
   const map = { 0: -1 };
-  let output = 0;
+  let max = 0;
   let sum = 0;
   for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
     if (sum - k in map) {
-      output = Math.max(output, i - map[sum - k]);
+      max = Math.max(max, i - map[sum - k]);
     }
     if (!(sum in map)) {
       map[sum] = i;
     }
   }
-  return output;
+  return max;
 };
