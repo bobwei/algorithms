d9792fd3b8d9b165f6837d47207559834cc825fa
Bob Wei <bob.wei@linecorp.com>
2019-11-29 15:51:02 +0800

    Update coin-change

--- src/leetcode/coin-change/index.js
+++ src/leetcode/coin-change/index.js
@@ -3,30 +3,14 @@
  * @param {number} amount
  * @return {number}
  */
-/*
-  f(n) = Math.min{f(n - coins[i]) + 1} for i in coins
-
-  coins = [2, 5]
-  f(1) = Math.min{f(1 - 2) + 1, f(1 - 5) + 1}
-       = 1
-*/
-const coinChange = function(coins, amount) {
-  if (!amount) {
-    return 0;
-  }
-  if (!coins.length) {
-    return -1;
-  }
-  const f = new Array(amount + 1).fill(Infinity);
-  f[0] = 0;
-  for (let n = 0; n <= amount; n++) {
-    for (let i = 0; i < coins.length; i++) {
-      if (n - coins[i] >= 0) {
-        f[n] = Math.min(f[n], f[n - coins[i]] + 1);
-      }
+var coinChange = function(coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 1; i <= amount; i++) {
+    for (const coin of coins) {
+      const value = i - coin >= 0 ? dp[i - coin] + 1 : Infinity;
+      dp[i] = Math.min(dp[i], value);
     }
   }
-  return f[amount] < Infinity ? f[amount] : -1;
+  return dp[amount] < Infinity ? dp[amount] : -1;
 };
-
-export default coinChange;
--- src/leetcode/coin-change/index.spec.js
+++
@@ -1,9 +0,0 @@
-import fn from './index';
-
-test('coin-change', () => {
-  expect(fn([1, 2, 5], 11)).toEqual(3);
-});
-
-test('coin-change', () => {
-  expect(fn([2, 5], 11)).toEqual(4);
-});
