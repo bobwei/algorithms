57a3eb54dfb236243b7a8b90e94ae1401cce6703
Bob Wei <bob.wei@linecorp.com>
2019-12-11 17:31:58 +0800

    Update knight-dialer

--- src/leetcode/knight-dialer/index.js
+++ src/leetcode/knight-dialer/index.js
@@ -2,33 +2,31 @@
  * @param {number} N
  * @return {number}
  */
-const map = {
-  1: [6, 8],
-  2: [7, 9],
-  3: [4, 8],
-  4: [0, 3, 9],
-  5: [],
-  6: [0, 1, 7],
-  7: [2, 6],
-  8: [1, 3],
-  9: [2, 4],
-  0: [4, 6],
-};
-
 var knightDialer = function(N) {
-  let pre = new Array(10).fill(1);
+  const map = {
+    0: [4, 6],
+    1: [6, 8],
+    2: [7, 9],
+    3: [4, 8],
+    4: [3, 9, 0],
+    5: [],
+    6: [1, 7, 0],
+    7: [2, 6],
+    8: [1, 3],
+    9: [2, 4],
+  };
+  let dp = new Array(10).fill(1);
   for (let i = 1; i < N; i++) {
     const next = new Array(10).fill(0);
     for (let j = 0; j < 10; j++) {
-      for (const neighbor of map[j]) {
-        next[j] += mod(pre[neighbor]);
-      }
+      next[j] = map[j].map((k) => dp[k]).reduce(addAndMod, 0);
     }
-    pre = next;
+    dp = next;
   }
-  return mod(pre.reduce((acc, cur) => acc + cur, 0));
+  return dp.reduce(addAndMod, 0);
 };
 
-function mod(n) {
-  return n % (10 ** 9 + 7);
+function addAndMod(a, b) {
+  const M = 10 ** 9 + 7;
+  return (a + b) % M;
 }
