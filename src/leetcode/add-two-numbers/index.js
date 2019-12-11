eb7b04ac413cc717bd7f638e0c04ac1a3931a5ca
Bob Wei <bob.wei@linecorp.com>
2019-12-12 00:55:23 +0800

    Update add-two-numbers

--- src/leetcode/add-two-numbers/index.js
+++ src/leetcode/add-two-numbers/index.js
@@ -11,18 +11,18 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-  let p1 = l1;
-  let p2 = l2;
   const dummy = new ListNode();
   let ptr = dummy;
+  let p1 = l1;
+  let p2 = l2;
   let c = 0;
   while (p1 || p2 || c) {
-    const sum = ((p1 && p1.val) || 0) + ((p2 && p2.val) || 0) + c;
-    ptr.next = new ListNode(sum % 10);
+    const sum = (p1 ? p1.val : 0) + (p2 ? p2.val : 0) + c;
     c = Math.floor(sum / 10);
-    if (p1) p1 = p1.next;
-    if (p2) p2 = p2.next;
+    ptr.next = new ListNode(sum % 10);
     ptr = ptr.next;
+    p1 = p1 ? p1.next : p1;
+    p2 = p2 ? p2.next : p2;
   }
   return dummy.next;
 };
