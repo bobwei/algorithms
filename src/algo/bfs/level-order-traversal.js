606080ecc040f7bb240725be94724d8bc9802611
Bob Wei <bob.wei@linecorp.com>
2019-12-17 12:10:26 +0800

    Add level-order-traversal

---
+++ src/algo/bfs/level-order-traversal.js
@@ -0,0 +1,10 @@
+function bfs(root) {
+  let queue = [root];
+  while (queue.length) {
+    const next = [];
+    while (queue.length) {
+      // add next level nodes to next queue
+    }
+    queue = next;
+  }
+}
