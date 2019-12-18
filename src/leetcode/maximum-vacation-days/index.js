27d0fa9393f1f81e89d168feafa8b4a6352917c5
Bob Wei <bob.wei@linecorp.com>
2019-12-18 15:02:18 +0800

    Add maximum-vacation-days

--- src/leetcode/maximum-vacation-days/index.js
+++ src/leetcode/maximum-vacation-days/index.js
@@ -7,24 +7,24 @@ var maxVacationDays = function(flights, days) {
   const n = flights.length;
   const k = days[0].length;
   let dp = new Array(n).fill(-Infinity);
-  dp[0] = days[0][0];
-  for (let p = 1; p < n; p++) {
-    dp[p] = flights[0][p] ? days[p][0] : -Infinity;
-  }
-  let max = 0;
-  for (let i = 1; i < k; i++) {
+  for (let i = 0; i < k; i++) {
     const next = new Array(n).fill(-Infinity);
     for (let p = 0; p < n; p++) {
       for (let q = 0; q < n; q++) {
-        // prettier-ignore
-        const val = (flights[q][p] === 1 || p === q)
-          ? (i - 1 >= 0 ? dp[q] : 0) + days[p][i]
-          : -Infinity;
-        next[p] = Math.max(next[p], val);
-        max = Math.max(max, next[p]);
+        next[p] = Math.max(next[p], helper({ dp, flights, days, i, p, q }));
       }
     }
     dp = next;
   }
-  return max;
+  return Math.max(...dp);
 };
+
+function helper({ dp, flights, days, i, p, q }) {
+  if (i === 0) {
+    return p === 0 || flights[0][p] ? days[p][i] : -Infinity;
+  }
+  if (flights[q][p] || p === q) {
+    return dp[q] + days[p][i];
+  }
+  return -Infinity;
+}
