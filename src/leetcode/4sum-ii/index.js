beb4566e233920877b881c7a0796886fa6373028
Bob Wei <bob.wei@linecorp.com>
2019-12-23 15:05:13 +0800

    Update 4sum-ii

--- src/leetcode/4sum-ii/index.js
+++ src/leetcode/4sum-ii/index.js
@@ -6,24 +6,24 @@
  * @return {number}
  */
 var fourSumCount = function(A, B, C, D) {
-  const sums1 = createCombinationSums(A, B);
-  const sums2 = createCombinationSums(C, D);
-  let count = 0;
-  for (const sum in sums1) {
-    if (0 - sum in sums2) {
-      count += sums1[sum] * sums2[0 - sum];
+  const sumFreq1 = createSumFreq(A, B);
+  const sumFreq2 = createSumFreq(C, D);
+  let n = 0;
+  for (const s1 in sumFreq1) {
+    if (0 - s1 in sumFreq2) {
+      n += sumFreq1[s1] * sumFreq2[0 - s1];
     }
   }
-  return count;
+  return n;
 };
 
-function createCombinationSums(arr1, arr2) {
-  const map = {};
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      const sum = arr1[i] + arr2[j];
-      map[sum] = (map[sum] || 0) + 1;
+function createSumFreq(arr1, arr2) {
+  const freq = {};
+  for (const val1 of arr1) {
+    for (const val2 of arr2) {
+      const sum = val1 + val2;
+      freq[sum] = (freq[sum] || 0) + 1;
     }
   }
-  return map;
+  return freq;
 }
