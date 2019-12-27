05e469143fb9e1cb01a53a1212fa433c56b8f004
Bob Wei <bob.wei@linecorp.com>
2019-12-27 14:48:28 +0800

    Add unique-paths

--- src/leetcode/unique-paths/index.js
+++ src/leetcode/unique-paths/index.js
@@ -3,25 +3,14 @@
  * @param {number} n
  * @return {number}
  */
-
-const memorize = (fn) => {
-  const cache = new Map();
-  return (n) => {
-    if (cache.has(n)) {
-      return cache.get(n);
-    }
-    const result = fn(n);
-    cache.set(n, result);
-    return result;
-  };
-};
-
-const factorial = memorize((n) => {
-  if (n <= 1) return 1;
-  return n * factorial(n - 1);
-});
-
 var uniquePaths = function(m, n) {
-  if (!m || !n) return 0;
-  return factorial(m - 1 + n - 1) / factorial(m - 1) / factorial(n - 1);
+  let dp = new Array(m).fill(1);
+  for (let i = 1; i < n; i++) {
+    const next = new Array(m).fill(1);
+    for (let j = 0; j < m; j++) {
+      next[j] = dp[j] + (j - 1 >= 0 ? next[j - 1] : 0);
+    }
+    dp = next;
+  }
+  return dp[m - 1];
 };
