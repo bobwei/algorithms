ec3d90fdbcb06f648732036ce061ab1c4745b6bd
Bob Wei <bob.wei@linecorp.com>
2020-01-01 02:18:58 +0800

    Update single-element-in-a-sorted-array

--- src/leetcode/single-element-in-a-sorted-array/index.js
+++ src/leetcode/single-element-in-a-sorted-array/index.js
@@ -6,14 +6,21 @@ var singleNonDuplicate = function(nums) {
   let left = 0;
   let right = nums.length;
   while (left < right) {
-    let m = Math.floor((left + right) / 2);
-    if (nums[m] === nums[m - 1]) m -= 1;
+    const m = Math.floor((left + right) / 2);
     if (nums[m] !== nums[m - 1] && nums[m] !== nums[m + 1]) {
       return nums[m];
-    } else if ((m - left) % 2 === 1) {
-      right = m;
+    }
+    const m1 = nums[m] === nums[m - 1] ? m - 1 : m;
+    const m2 = nums[m] === nums[m - 1] ? m : m + 1;
+    if (isOdd(right - m2 - 1)) {
+      left = m2 + 1;
     } else {
-      left = m + 2;
+      right = m1;
     }
   }
+  return nums[left];
 };
+
+function isOdd(n) {
+  return n % 2 === 1;
+}
