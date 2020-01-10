95a167bee5d4d242ba129c181d3b50ef8241db3d
Bob Wei <bob.wei@linecorp.com>
2020-01-10 17:04:57 +0800

    Update longest-palindromic-subsequence

--- src/leetcode/longest-palindromic-subsequence/index.js
+++ src/leetcode/longest-palindromic-subsequence/index.js
@@ -2,31 +2,23 @@
  * @param {string} s
  * @return {number}
  */
-/*
-  dp[i][j] = s[i] === s[j]
-    ? dp[i + 1][j - 1] + 2
-    : Math.max(dp[i][j - 1], dp[i + 1][j]);
-*/
 var longestPalindromeSubseq = function(s) {
   const m = s.length;
-  const dp = [...new Array(m)].map(() => new Array(m).fill(0));
-  let max = 0;
+  const dp = new Array(m).fill(null).map(() => new Array(m).fill(0));
   for (let i = 0; i < m; i++) {
-    const j = i;
-    dp[i][j] = 1;
-    max = Math.max(max, dp[i][j]);
-  }
-  for (let i = 0; i < m - 1; i++) {
-    const j = i + 1;
-    dp[i][i + 1] = s[i] === s[j] ? 2 : 1;
-    max = Math.max(max, dp[i][j]);
-  }
-  for (let length = 3; length <= m; length++) {
-    for (let i = 0; i <= m - length; i++) {
-      const j = i + length - 1;
-      dp[i][j] = s[i] === s[j] ? dp[i + 1][j - 1] + 2 : Math.max(dp[i][j - 1], dp[i + 1][j]);
-      max = Math.max(max, dp[i][j]);
+    dp[i][i] = 1;
+    if (i + 1 < m) {
+      dp[i][i + 1] = s[i] === s[i + 1] ? 2 : 1;
     }
   }
-  return max;
+  for (let length = 3; length <= m; length++) {
+    for (let i = 0; i + length - 1 < m; i++) {
+      const j = i + length - 1;
+      // prettier-ignore
+      dp[i][j] = s[i] === s[j]
+        ? dp[i + 1][j - 1] + 2
+        : Math.max(dp[i + 1][j], dp[i][j - 1]);
+    }
+  }
+  return dp[0][m - 1];
 };
