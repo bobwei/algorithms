729c9371da2ad1c0b62f0f2048605c4276ac9b3a
Bob Wei <bob.wei@linecorp.com>
2020-02-06 00:17:03 +0800

    Update verifying-an-alien-dictionary

--- src/leetcode/verifying-an-alien-dictionary/index.js
+++ src/leetcode/verifying-an-alien-dictionary/index.js
@@ -4,21 +4,26 @@
  * @return {boolean}
  */
 var isAlienSorted = function(words, order) {
-  // prettier-ignore
-  const map = order
-    .split('')
-    .reduce((acc, cur, i) => ({ ...acc, [cur]: i }), {});
-  for (let i = 0; i <= words.length - 2; i++) {
+  const indices = createIndices(order);
+  for (let i = 0; i < words.length - 1; i++) {
     let j = 0;
-    while (words[i][j] === words[i + 1][j]) {
+    while (j < words[i].length && j < words[i + 1].length && words[i][j] === words[i + 1][j]) {
       j += 1;
     }
-    // prettier-ignore
-    const isInvalid = j >= words[i + 1].length && i < words[i].length ||
-      map[words[i][j]] > map[words[i + 1][j]];
-    if (isInvalid) {
+    if (j < words[i].length && j >= words[i + 1].length) {
+      return false;
+    }
+    if (indices[words[i][j]] > indices[words[i + 1][j]]) {
       return false;
     }
   }
   return true;
 };
+
+function createIndices(str) {
+  const indices = {};
+  for (let i = 0; i < str.length; i++) {
+    indices[str[i]] = i;
+  }
+  return indices;
+}
