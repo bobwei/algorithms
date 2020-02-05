f63a9301911f7fa8b9aeac8da5b2214135796da8
Bob Wei <bob.wei@linecorp.com>
2020-02-05 15:27:57 +0800

    Update meeting-rooms-ii

--- src/leetcode/meeting-rooms-ii/index.js
+++ src/leetcode/meeting-rooms-ii/index.js
@@ -4,20 +4,16 @@
  */
 var minMeetingRooms = function(intervals) {
   const timeline = [];
-  for (const [start, end] of intervals) {
-    timeline.push([start, 1], [end, -1]);
+  for (const [s, e] of intervals) {
+    timeline.push([s, 1]);
+    timeline.push([e, -1]);
   }
-  timeline.sort((a, b) => {
-    if (a[0] !== b[0]) {
-      return a[0] - b[0];
-    }
-    return a[1] - b[1];
-  });
+  timeline.sort((a, b) => (a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]));
+  let count = 0;
   let max = 0;
-  let nRooms = 0;
-  for (const [, delta] of timeline) {
-    nRooms += delta;
-    max = Math.max(max, nRooms);
+  for (const [t, delta] of timeline) {
+    count += delta;
+    max = Math.max(max, count);
   }
   return max;
 };
