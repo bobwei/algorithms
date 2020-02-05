9a1abced66b072cb2bd3965dad589e0ab2956610
Bob Wei <bob.wei@linecorp.com>
2020-02-05 15:42:44 +0800

    Update remove-invalid-parentheses

--- src/leetcode/remove-invalid-parentheses/index.js
+++ src/leetcode/remove-invalid-parentheses/index.js
@@ -4,44 +4,39 @@
  */
 var removeInvalidParentheses = function(s) {
   const output = [];
+  const queue = [s];
   const visited = new Set();
-  let queue = [s];
   while (queue.length) {
-    const next = [];
-    while (queue.length) {
-      const str = queue.shift();
-      if (isValid(str)) {
-        output.push(str);
-      }
-      if (!output.length) {
-        for (let i = 0; i < str.length; i++) {
-          if (!(str[i] === '(' || str[i] === ')')) {
-            continue;
-          }
-          const candidate = str.substring(0, i) + str.substring(i + 1);
-          if (!visited.has(candidate)) {
-            visited.add(candidate);
-            next.push(candidate);
+    const str = queue.shift();
+    if (isValid(str)) {
+      output.push(str);
+    }
+    if (!output.length) {
+      for (let i = 0; i < str.length; i++) {
+        if (str[i] === '(' || str[i] === ')') {
+          const next = str.substring(0, i) + str.substring(i + 1);
+          if (!visited.has(next)) {
+            visited.add(next);
+            queue.push(next);
           }
         }
       }
     }
-    queue = next;
   }
   return output;
 };
 
 function isValid(str) {
-  let n = 0;
+  let count = 0;
   for (const c of str) {
     if (c === '(') {
-      n += 1;
+      count += 1;
     } else if (c === ')') {
-      n -= 1;
+      count -= 1;
     }
-    if (n < 0) {
+    if (count < 0) {
       return false;
     }
   }
-  return n === 0;
+  return count === 0;
 }
