413e6daade5a00a0104284479d27d04853a12dd3
Bob Wei <bob.wei@linecorp.com>
2020-02-05 16:31:57 +0800

    Update word-break

--- src/leetcode/word-break/index.js
+++ src/leetcode/word-break/index.js
@@ -3,22 +3,15 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-
-const isPostFix = (s, i, w) => {
-  return s.slice(i - w.length, i) === w;
-};
-
 var wordBreak = function(s, wordDict) {
-  const dp = new Array(s.length + 1).fill(false);
+  const m = s.length;
+  const dp = new Array(m + 1).fill(false);
   dp[0] = true;
-  for (let i = 1; i < s.length + 1; i++) {
-    for (let j = 0; j < wordDict.length; j++) {
-      const w = wordDict[j];
-      if (isPostFix(s, i, w)) {
-        dp[i] = dp[i] || dp[i - w.length];
-        if (dp[i]) break;
-      }
+  for (let i = 1; i <= m; i++) {
+    for (let j = 0; j < wordDict.length && dp[i] === false; j++) {
+      const isBreakable = s.substring(i - wordDict[j].length, i) === wordDict[j];
+      dp[i] = isBreakable && dp[i - wordDict[j].length];
     }
   }
-  return dp[s.length];
+  return dp[m];
 };
