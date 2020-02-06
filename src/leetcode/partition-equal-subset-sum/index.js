013fb276d332354a0e0238bd3cec4da9ab87e46d
Bob Wei <bob.wei@linecorp.com>
2020-02-06 18:51:14 +0800

    Update partition-equal-subset-sum

--- src/leetcode/partition-equal-subset-sum/index.js
+++ src/leetcode/partition-equal-subset-sum/index.js
@@ -2,22 +2,29 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+var canPartition = function(nums) {
+  const total = nums.reduce((acc, cur) => acc + cur, 0);
+  if (total % 2 === 1) {
+    return false;
+  }
+  nums.sort((a, b) => a - b);
+  return helper(nums, total / 2, total);
+};
 
-const helper = (nums, target, start, leftTotal) => {
-  if (target <= 0) return target === 0;
-  if (leftTotal < target) return false;
+function helper(nums, target, remaining, start = 0) {
+  if (target <= 0) {
+    return target === 0;
+  }
+  if (remaining < target) {
+    return false;
+  }
   for (let i = start; i < nums.length; i++) {
-    if (i > start && nums[i] === nums[i - 1]) continue;
-    if (helper(nums, target - nums[i], i + 1, leftTotal - nums[i])) {
+    if (i > start && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    if (helper(nums, target - nums[i], remaining - nums[i], i + 1)) {
       return true;
     }
   }
   return false;
-};
-
-var canPartition = function(nums) {
-  const total = nums.reduce((acc, cur) => acc + cur, 0);
-  if (total % 2 === 1) return false;
-  nums.sort();
-  return helper(nums, total / 2, 0, total);
-};
+}
