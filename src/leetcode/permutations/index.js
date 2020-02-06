dc0caf48fa08a64492a3b67667f8e2ebceec9402
Bob Wei <bob.wei@linecorp.com>
2020-02-06 15:21:05 +0800

    Update permutations

--- src/leetcode/permutations/index.js
+++ src/leetcode/permutations/index.js
@@ -3,15 +3,16 @@
  * @return {number[][]}
  */
 var permute = function(nums, selected = new Set(), output = []) {
-  if (selected.size >= nums.length) {
+  if (selected.size === nums.length) {
     output.push([...selected].map((i) => nums[i]));
     return output;
   }
   for (let i = 0; i < nums.length; i++) {
-    if (selected.has(i)) continue;
-    selected.add(i);
-    permute(nums, selected, output);
-    selected.delete(i);
+    if (!selected.has(i)) {
+      selected.add(i);
+      permute(nums, selected, output);
+      selected.delete(i);
+    }
   }
   return output;
 };
