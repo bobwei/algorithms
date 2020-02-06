81eb9dfe0213124aeb1d6d3e896141808093209e
Bob Wei <bob.wei@linecorp.com>
2020-02-06 18:52:15 +0800

    Update find-all-anagrams-in-a-string

--- src/leetcode/find-all-anagrams-in-a-string/index.js
+++ src/leetcode/find-all-anagrams-in-a-string/index.js
@@ -4,42 +4,42 @@
  * @return {number[]}
  */
 var findAnagrams = function(s, p) {
-  const output = [];
   const counter = new Counter(p);
+  const output = [];
   let start = 0;
   for (let i = 0; i < s.length; i++) {
     counter.add(s[i]);
-    while (counter.nRedundant > 0) {
+    while (counter.nRemaining === 0) {
+      if (i - start + 1 === p.length) {
+        output.push(start);
+      }
       counter.delete(s[start]);
       start += 1;
-    }
-    if (i - start + 1 === p.length) {
-      output.push(start);
     }
   }
   return output;
 };
 
 class Counter {
-  constructor(str) {
-    this.counter = {};
-    for (const c of str) {
-      this.counter[c] = (this.counter[c] || 0) + 1;
+  constructor(target) {
+    this.freq = {};
+    for (const c of target) {
+      this.freq[c] = (this.freq[c] || 0) + 1;
     }
-    this.nRedundant = 0;
+    this.nRemaining = target.length;
   }
 
   add(c) {
-    this.counter[c] = (this.counter[c] || 0) - 1;
-    if (this.counter[c] < 0) {
-      this.nRedundant += 1;
+    this.freq[c] = (this.freq[c] || 0) - 1;
+    if (this.freq[c] >= 0) {
+      this.nRemaining -= 1;
     }
   }
 
   delete(c) {
-    this.counter[c] += 1;
-    if (this.counter[c] === 0) {
-      this.nRedundant -= 1;
+    this.freq[c] = (this.freq[c] || 0) + 1;
+    if (this.freq[c] > 0) {
+      this.nRemaining += 1;
     }
   }
 }
