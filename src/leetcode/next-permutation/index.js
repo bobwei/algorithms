26ba5529eef1d2ef268c572b830db81a09ebd503
Bob Wei <bob.wei@linecorp.com>
2020-02-06 13:12:15 +0800

    Update next-permutation

--- src/leetcode/next-permutation/index.js
+++ src/leetcode/next-permutation/index.js
@@ -3,39 +3,37 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-  const i = findNextIncrementIndex(nums);
-  if (i < 0) {
-    return nums.reverse();
+  const i = findFirstDesc(nums);
+  if (i >= 0) {
+    const j = findMinGreater(nums, i + 1, nums[i]);
+    [nums[i], nums[j]] = [nums[j], nums[i]];
   }
-  const j = findGreaterIndex(nums, i);
-  [nums[i], nums[j]] = [nums[j], nums[i]];
   reverse(nums, i + 1);
 };
 
-function findNextIncrementIndex(nums) {
-  const n = nums.length;
-  for (let i = n - 2; i >= 0; i--) {
-    if (nums[i] < nums[i + 1]) {
+function findMinGreater(nums, start, target) {
+  for (let i = nums.length - 1; i >= start; i--) {
+    if (nums[i] > target) {
       return i;
     }
   }
   return -1;
 }
 
-function findGreaterIndex(nums, index) {
-  for (let i = nums.length - 1; i > index; i--) {
-    if (nums[i] > nums[index]) {
-      return i;
-    }
+function findFirstDesc(nums) {
+  let i = nums.length - 2;
+  while (nums[i] >= nums[i + 1]) {
+    i -= 1;
   }
+  return i;
 }
 
-function reverse(nums, start, end = nums.length - 1) {
-  let left = start;
-  let right = end;
-  while (left < right) {
-    [nums[left], nums[right]] = [nums[right], nums[left]];
-    left += 1;
-    right -= 1;
+function reverse(nums, start) {
+  let i = start;
+  let j = nums.length - 1;
+  while (i < j) {
+    [nums[i], nums[j]] = [nums[j], nums[i]];
+    i += 1;
+    j -= 1;
   }
 }
