62d6d51cbf0051deed7dff09687df50d6be577a2
Bob Wei <bob.wei@linecorp.com>
2020-02-06 15:41:59 +0800

    Update palindrome-permutation

--- src/leetcode/palindrome-permutation/index.js
+++ src/leetcode/palindrome-permutation/index.js
@@ -2,15 +2,14 @@
  * @param {string} s
  * @return {boolean}
  */
-const canPermutePalindrome = function(s) {
-  const counts = {};
-  for (let i = 0; i < s.length; i++) {
-    counts[s[i]] = (counts[s[i]] || 0) + 1;
+var canPermutePalindrome = function(s) {
+  const freq = {};
+  for (const c of s) {
+    freq[c] = (freq[c] || 0) + 1;
   }
-  const values = Object.values(counts);
   let nOdds = 0;
-  for (let i = 0; i < values.length; i++) {
-    if (values[i] % 2 === 1) {
+  for (const c in freq) {
+    if (freq[c] % 2 === 1) {
       nOdds += 1;
     }
     if (nOdds > 1) {
@@ -19,5 +18,3 @@ const canPermutePalindrome = function(s) {
   }
   return true;
 };
-
-export default canPermutePalindrome;
