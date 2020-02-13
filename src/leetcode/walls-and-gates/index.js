8dbb28459851eb88794ecf86da4765dd43607bcd
Bob Wei <bob.wei@linecorp.com>
2020-02-13 11:30:34 +0800

    Update walls-and-gates

--- src/leetcode/walls-and-gates/index.js
+++ src/leetcode/walls-and-gates/index.js
@@ -8,33 +8,30 @@ var wallsAndGates = function(rooms) {
   }
   const m = rooms.length;
   const n = rooms[0].length;
+  const queue = [];
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (rooms[i][j] === 0) {
-        bfs(rooms, { i, j }, m, n);
+        queue.push([[i, j], 0]);
+      }
+    }
+  }
+  // prettier-ignore
+  const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+  while (queue.length) {
+    const [[x, y], d] = queue.shift();
+    for (const [di, dj] of dirs) {
+      const i = x + di;
+      const j = y + dj;
+      if (isValid(m, n, i, j) && d + 1 < rooms[i][j]) {
+        rooms[i][j] = d + 1;
+        queue.push([[i, j], d + 1]);
       }
     }
   }
 };
 
-const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-
-function bfs(rooms, position, m, n) {
-  const queue = [[position, 0]];
-  while (queue.length) {
-    const [p, d] = queue.shift();
-    for (const [di, dj] of dirs) {
-      const i = p.i + di;
-      const j = p.j + dj;
-      if (isValid(i, j, m, n) && d + 1 < rooms[i][j]) {
-        rooms[i][j] = d + 1;
-        queue.push([{ i, j }, d + 1]);
-      }
-    }
-  }
-}
-
-function isValid(i, j, m, n) {
+function isValid(m, n, i, j) {
   if (i < 0 || i >= m || j < 0 || j >= n) {
     return false;
   }
