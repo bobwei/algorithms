25b36e8c4c9dce0ea7f14e1e00750ff211943947
Bob Wei <bob.wei@linecorp.com>
2020-02-13 11:42:13 +0800

    Update 01-matrix

--- src/leetcode/01-matrix/index.js
+++ src/leetcode/01-matrix/index.js
@@ -5,26 +5,35 @@
 var updateMatrix = function(matrix) {
   const m = matrix.length;
   const n = matrix[0].length;
-  const dp = [...new Array(m)].map(() => new Array(n).fill(Infinity));
+  const dist = new Array(m).fill(null).map(() => new Array(n).fill(Infinity));
+  const queue = [];
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-      dp[i][j] = Math.min(
-        matrix[i][j] === 0 ? 0 : Infinity,
-        dp[i][j],
-        i - 1 >= 0 ? dp[i - 1][j] + 1 : Infinity,
-        j - 1 >= 0 ? dp[i][j - 1] + 1 : Infinity,
-      );
+      if (matrix[i][j] === 0) {
+        dist[i][j] = 0;
+        queue.push([i, j]);
+      }
     }
   }
-  for (let i = m - 1; i >= 0; i--) {
-    for (let j = n - 1; j >= 0; j--) {
-      dp[i][j] = Math.min(
-        matrix[i][j] === 0 ? 0 : Infinity,
-        dp[i][j],
-        i + 1 < m ? dp[i + 1][j] + 1 : Infinity,
-        j + 1 < n ? dp[i][j + 1] + 1 : Infinity,
-      );
+  // prettier-ignore
+  const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+  while (queue.length) {
+    const [x, y] = queue.shift();
+    for (const [di, dj] of dirs) {
+      const i = x + di;
+      const j = y + dj;
+      if (isValid(m, n, i, j) && dist[x][y] + 1 < dist[i][j]) {
+        dist[i][j] = dist[x][y] + 1;
+        queue.push([i, j]);
+      }
     }
   }
-  return dp;
+  return dist;
 };
+
+function isValid(m, n, i, j) {
+  if (i < 0 || i >= m || j < 0 || j >= n) {
+    return false;
+  }
+  return true;
+}
