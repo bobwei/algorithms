7a81dfbd9d376ef66f792c62640307d102683413
Bob Wei <bob.wei@linecorp.com>
2020-02-13 12:17:35 +0800

    Update movies-on-flight

--- src/practices/movies-on-flight/index.js
+++ src/practices/movies-on-flight/index.js
@@ -1,17 +1,50 @@
-const moviesOnFlight = (durations, t) => {
-  durations.sort((a, b) => a - b);
-  const m = durations.length;
-  const output = [-Infinity, -Infinity];
-  let j = m - 1;
-  for (let i = 0; i < m - 1; i++) {
-    while (durations[i] + durations[j] > t - 30) {
+function movieOnFlight(durations, d) {
+  const arr = durations.map((dur, i) => [dur, i]).sort((a, b) => a[0] - b[0]);
+  const output = [[], 0, 0];
+  let i = 0;
+  let j = arr.length - 1;
+  while (i < j) {
+    const sum = arr[i][0] + arr[j][0];
+    if (sum > d - 30) {
       j -= 1;
-    }
-    if (durations[i] + durations[j] > output[0] + output[1]) {
-      [output[0], output[1]] = [durations[i], durations[j]];
+    } else {
+      if (sum > output[1] || Math.max(arr[i][0], arr[j][0]) > output[2]) {
+        output[0] = [arr[i][1], arr[j][1]];
+        output[1] = sum;
+        output[2] = Math.max(arr[i][0], arr[j][0]);
+      }
+      i += 1;
     }
   }
-  return output;
-};
+  return output[0];
+}
 
-export default moviesOnFlight;
+(() => {
+  const durations = [90, 85, 75, 60, 120, 150, 125];
+  const d = 250;
+  console.log(movieOnFlight(durations, d));
+})();
+
+(() => {
+  const durations = [90, 85, 75, 60, 120, 110, 110, 150, 125];
+  const d = 250;
+  console.log(movieOnFlight(durations, d));
+})();
+
+(() => {
+  const durations = [90, 85, 75, 60, 120, 150, 125];
+  const d = 50;
+  console.log(movieOnFlight(durations, d));
+})();
+
+(() => {
+  const durations = [90, 85, 75, 60, 120, 150, 125];
+  const d = 220;
+  console.log(movieOnFlight(durations, d));
+})();
+
+(() => {
+  const durations = [10, 50, 60];
+  const d = 120;
+  console.log(movieOnFlight(durations, d));
+})();
--- src/practices/movies-on-flight/index.spec.js
+++
@@ -1,5 +0,0 @@
-import moviesOnFlight from './index';
-
-test('moviesOnFlight', () => {
-  expect(moviesOnFlight([90, 85, 75, 60, 120, 150, 125], 250)).toEqual([90, 125]);
-});
