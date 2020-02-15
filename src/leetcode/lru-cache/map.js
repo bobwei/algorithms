e5705ee0492758301c2afc01cf6e8ad22856e9d8
Bob Wei <bob.wei@linecorp.com>
2020-02-16 00:52:14 +0800

    Add lru-cache

---
+++ src/leetcode/lru-cache/map.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+  this.capacity = capacity;
+  this.map = new Map();
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+  if (!this.map.has(key)) {
+    return -1;
+  }
+  const val = this.map.get(key);
+  this.map.delete(key);
+  this.map.set(key, val);
+  return val;
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+  this.map.delete(key);
+  this.map.set(key, value);
+  if (this.map.size > this.capacity) {
+    this.map.delete(this.map.keys().next().value);
+  }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
