19a4f91820be53d0360e371e7d29328ec9b66992
Bob Wei <bob.wei@linecorp.com>
2020-02-15 15:03:48 +0800

    Update trapping-rain-water

--- src/leetcode/trapping-rain-water/index.js
+++ src/leetcode/trapping-rain-water/index.js
@@ -5,19 +5,15 @@
 var trap = function(height) {
   const m = height.length;
   const left = new Array(m).fill(0);
-  const right = new Array(m).fill(0);
   left[0] = height[0];
-  right[m - 1] = height[m - 1];
   for (let i = 1; i < m; i++) {
     left[i] = Math.max(left[i - 1], height[i]);
   }
-  for (let i = m - 2; i >= 0; i--) {
-    right[i] = Math.max(right[i + 1], height[i]);
-  }
   let sum = 0;
-  for (let i = 0; i < m; i++) {
-    const delta = Math.min(left[i], right[i]) - height[i];
-    sum += delta >= 0 ? delta : 0;
+  let right = height[m - 1];
+  for (let i = m - 2; i >= 0; i--) {
+    right = Math.max(right, height[i]);
+    sum += Math.min(left[i], right) - height[i];
   }
   return sum;
 };
