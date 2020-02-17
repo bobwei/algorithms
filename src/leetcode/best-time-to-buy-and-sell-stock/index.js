effdd638b5737dd804d7d1ff42cfb21a1fe3e4b2
Bob Wei <bob.wei@linecorp.com>
2020-02-17 15:55:41 +0800

    Update best-time-to-buy-and-sell-stock

--- src/leetcode/best-time-to-buy-and-sell-stock/index.js
+++ src/leetcode/best-time-to-buy-and-sell-stock/index.js
@@ -2,21 +2,12 @@
  * @param {number[]} prices
  * @return {number}
  */
-
-/*
-  [7,1,5,3,6,4,9,0,9]
-  [7,1,5,5,6,6,9,9,9]
-  [7,1,1,1,1,1,1,0,0]
-*/
-
-const maxProfit = function(prices) {
-  let min = Infinity;
-  let profit = 0;
-  for (let i = 0; i < prices.length; i++) {
-    min = Math.min(min, prices[i]);
-    profit = Math.max(profit, prices[i] - min);
+var maxProfit = function(prices) {
+  let max = 0;
+  let minSoFar = Infinity;
+  for (const price of prices) {
+    minSoFar = Math.min(minSoFar, price);
+    max = Math.max(max, price - minSoFar);
   }
-  return profit;
+  return max;
 };
-
-export default maxProfit;
--- src/leetcode/best-time-to-buy-and-sell-stock/index.spec.js
+++
@@ -1,9 +0,0 @@
-import fn from './index';
-
-test('best-time-to-buy-and-sell-stock', () => {
-  expect(fn([7, 1, 5, 3, 6, 4])).toEqual(5);
-});
-
-test('best-time-to-buy-and-sell-stock', () => {
-  expect(fn([7, 6, 4, 3, 1])).toEqual(0);
-});
