d8d3dd765164219cd27c4b4693841043135d1594
Bob Wei <bob.wei@linecorp.com>
2020-02-19 00:33:38 +0800

    Update word-ladder-ii

--- src/leetcode/word-ladder-ii/index.js
+++ src/leetcode/word-ladder-ii/index.js
@@ -5,64 +5,66 @@
  * @return {string[][]}
  */
 var findLadders = function(beginWord, endWord, wordList) {
-  const graph = bfs(beginWord, endWord, wordList);
-  return dfs(graph, beginWord, endWord);
+  if (wordList.indexOf(endWord) < 0) {
+    return [];
+  }
+  const arr = [...new Set([beginWord, ...wordList])];
+  const m = arr.length;
+  const graph = createGraph(arr, m);
+  const dist = new Array(m).fill(null).map(() => [Infinity, []]);
+  dist[0] = [0, []];
+  const queue = [0];
+  while (queue.length) {
+    const u = queue.shift();
+    if (arr[u] !== endWord || (arr[u] === endWord && dist[u][0] < Infinity)) {
+      for (const v of graph[u]) {
+        if (dist[u][0] + 1 < dist[v][0]) {
+          dist[v][0] = dist[u][0] + 1;
+          dist[v][1] = [];
+          queue.push(v);
+        }
+        if (dist[u][0] + 1 === dist[v][0]) {
+          dist[v][1].push(u);
+        }
+      }
+    }
+  }
+  return dfs(dist, arr.indexOf(endWord), arr);
 };
 
-function dfs(graph, beginWord, endWord, selected = [beginWord], output = []) {
-  if (selected[selected.length - 1] === endWord) {
-    output.push([...selected]);
+function dfs(graph, u, arr, selected = [], output = []) {
+  if (u === 0) {
+    output.push([...selected, arr[u]].reverse());
     return output;
   }
-  for (const u of graph[beginWord]) {
-    selected.push(u);
-    dfs(graph, u, endWord, selected, output);
+  for (const v of graph[u][1]) {
+    selected.push(arr[u]);
+    dfs(graph, v, arr, selected, output);
     selected.pop();
   }
   return output;
 }
 
-function bfs(beginWord, endWord, wordList) {
-  const graph = createGraph([beginWord, ...wordList]);
-  const distance = { [beginWord]: 1 };
-  const visited = new Set();
-  const queue = [beginWord];
-  while (queue.length) {
-    const w = queue.shift();
-    for (const word of wordList) {
-      if (isTransformedWord(w, word)) {
-        if (distance[w] + 1 <= (distance[word] || Infinity)) {
-          distance[word] = distance[w] + 1;
-          graph[w].add(word);
-        }
-        if (!visited.has(word) && !(endWord in distance)) {
-          visited.add(word);
-          queue.push(word);
-        }
+function createGraph(arr, m) {
+  const graph = new Array(m).fill(null).map(() => []);
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < i; j++) {
+      if (canTransform(arr[i], arr[j])) {
+        graph[i].push(j);
+        graph[j].push(i);
       }
     }
   }
   return graph;
 }
 
-function createGraph(arr) {
-  const graph = {};
-  for (const node of arr) {
-    graph[node] = new Set();
-  }
-  return graph;
-}
-
-function isTransformedWord(w1, w2) {
-  if (w1 === w2) {
-    return false;
-  }
-  let nDiffs = 0;
-  for (let i = 0; i < w1.length; i++) {
-    if (w1[i] !== w2[i]) {
-      nDiffs += 1;
+function canTransform(str1, str2) {
+  let nDiff = 0;
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) {
+      nDiff += 1;
     }
-    if (nDiffs > 1) {
+    if (nDiff > 1) {
       return false;
     }
   }
