823acfb83469a9ed965724c8c4ebc764183681f3
Bob Wei <bob.wei@linecorp.com>
2020-02-19 00:33:54 +0800

    Rm unused

--- src/leetcode/word-ladder/index.spec.js
+++
@@ -1,9 +0,0 @@
-import fn from './index';
-
-test('word-ladder', () => {
-  expect(fn('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog'])).toEqual(5);
-});
-
-test('word-ladder', () => {
-  expect(fn('a', 'c', ['a', 'b', 'c'])).toEqual(2);
-});
