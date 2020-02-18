3870f1d883d76edd7fbde4dcebbb22bee5035938
Bob Wei <bob.wei@linecorp.com>
2020-02-18 22:36:40 +0800

    Update word-ladder

--- src/leetcode/word-ladder/index.js
+++ src/leetcode/word-ladder/index.js
@@ -5,30 +5,50 @@
  * @return {number}
  */
 var ladderLength = function(beginWord, endWord, wordList) {
-  const visited = new Set([beginWord]);
-  const queue = [[beginWord, 1]];
+  if (wordList.indexOf(endWord) < 0) {
+    return 0;
+  }
+  const arr = [beginWord, ...wordList];
+  const m = arr.length;
+  const graph = createGraph(arr, m);
+  const dist = new Array(m).fill(Infinity);
+  dist[0] = 0;
+  const queue = [0];
   while (queue.length) {
-    const [word, length] = queue.shift();
-    if (word === endWord) {
-      return length;
+    const u = queue.shift();
+    if (arr[u] === endWord) {
+      return dist[u] + 1;
     }
-    for (const w of wordList) {
-      if (isValid(word, w) && !visited.has(w)) {
-        visited.add(w);
-        queue.push([w, length + 1]);
+    for (const v of graph[u]) {
+      if (dist[u] + 1 < dist[v]) {
+        dist[v] = dist[u] + 1;
+        queue.push(v);
       }
     }
   }
   return 0;
 };
 
-function isValid(w1, w2) {
-  let diff = 0;
-  for (let i = 0; i < w1.length; i++) {
-    if (w1[i] !== w2[i]) {
-      diff += 1;
+function createGraph(arr, m) {
+  const graph = new Array(m).fill(null).map(() => []);
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < i; j++) {
+      if (canTransform(arr[i], arr[j])) {
+        graph[i].push(j);
+        graph[j].push(i);
+      }
     }
-    if (diff > 1) {
+  }
+  return graph;
+}
+
+function canTransform(str1, str2) {
+  let nDiff = 0;
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) {
+      nDiff += 1;
+    }
+    if (nDiff > 1) {
       return false;
     }
   }
