f2e25bf9d29cf7376dd5c759d440df4e074313f4
Bob Wei <bob.wei@linecorp.com>
2020-02-21 14:23:49 +0800

    Update meeting-scheduler

--- src/leetcode/meeting-scheduler/index.js
+++ src/leetcode/meeting-scheduler/index.js
@@ -5,30 +5,27 @@
  * @return {number[]}
  */
 var minAvailableDuration = function(slots1, slots2, duration) {
-  sort(slots1);
-  sort(slots2);
-  let i = 0;
-  let j = 0;
-  while (i < slots1.length && j < slots2.length) {
-    const t = merge(slots1[i], slots2[j]);
-    if (t[1] - t[0] >= duration) {
-      t[1] = t[0] + duration;
-      return t;
-    } else if (slots1[i][0] < slots2[j][0] || slots1[i][1] < slots2[j][1]) {
-      i += 1;
+  slots1.sort(comparator);
+  slots2.sort(comparator);
+  let t1 = 0;
+  let t2 = 0;
+  while (t1 < slots1.length && t2 < slots2.length) {
+    const merged = [Math.max(slots1[t1][0], slots2[t2][0]), Math.min(slots1[t1][1], slots2[t2][1])];
+    if (merged[0] < merged[1] && merged[1] - merged[0] >= duration) {
+      return [merged[0], merged[0] + duration];
+    }
+    if (slots1[t1][1] < slots2[t2][1]) {
+      t1 += 1;
     } else {
-      j += 1;
+      t2 += 1;
     }
   }
   return [];
 };
 
-function merge([s1, e1], [s2, e2]) {
-  return [Math.max(s1, s2), Math.min(e1, e2)];
-}
-
-function sort(arr) {
-  return arr.sort((a, b) => {
+function comparator(a, b) {
+  if (a[0] !== b[0]) {
     return a[0] - b[0];
-  });
+  }
+  return a[1] - b[1];
 }
