f89002424e098b97a7817403eedf2bffdc79cee6
Bob Wei <bob.wei@linecorp.com>
2020-02-21 15:11:01 +0800

    Update summary-ranges

--- src/leetcode/summary-ranges/index.js
+++ src/leetcode/summary-ranges/index.js
@@ -4,15 +4,13 @@
  */
 var summaryRanges = function(nums) {
   const output = [];
-  for (let i = 0; i < nums.length; ) {
-    let j = i + 1;
-    while (nums[j] === nums[j - 1] + 1) {
+  for (let i = 0; i < nums.length; i++) {
+    let j = i;
+    while (j < nums.length && nums[j + 1] === nums[j] + 1) {
       j += 1;
     }
-    // prettier-ignore
-    const range = nums[i] + '' +
-      (j - i > 1 ? '->' + nums[j - 1] : '');
-    output.push(range);
+    const str = j > i ? nums[i] + '->' + nums[j] : nums[i] + '';
+    output.push(str);
     i = j;
   }
   return output;
