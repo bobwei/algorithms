6a9b4bdf48ace5de18526546ec79befdd200864d
Bob Wei <bob.wei@linecorp.com>
2020-02-24 17:04:09 +0800

    Update valid-palindrome

--- src/leetcode/valid-palindrome/index.js
+++ src/leetcode/valid-palindrome/index.js
@@ -2,26 +2,26 @@
  * @param {string} s
  * @return {boolean}
  */
-
-const isCharacter = (c) => {
-  return /[a-zA-Z0-9]/.test(c);
-};
-
 var isPalindrome = function(s) {
-  let left = 0;
-  let right = s.length - 1;
-  while (left < right) {
-    while (!isCharacter(s[left])) {
-      left += 1;
+  const m = s.length;
+  let i = 0;
+  let j = m - 1;
+  while (i < j) {
+    while (!isAlphanumeric(s[i]) && i < m) {
+      i += 1;
     }
-    while (!isCharacter(s[right])) {
-      right -= 1;
+    while (!isAlphanumeric(s[j]) && j >= 0) {
+      j -= 1;
     }
-    if (left < s.length && right >= 0 && s[left].toLowerCase() !== s[right].toLowerCase()) {
+    if (i < m && j >= 0 && s[i].toLowerCase() !== s[j].toLowerCase()) {
       return false;
     }
-    left += 1;
-    right -= 1;
+    i += 1;
+    j -= 1;
   }
   return true;
 };
+
+function isAlphanumeric(c) {
+  return /[a-zA-Z0-9]+/.test(c);
+}
