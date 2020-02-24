79fb5ad011eddd071b64ef2da1a82efdbb8d9d99
Bob Wei <bob.wei@linecorp.com>
2020-02-24 19:00:15 +0800

    Update add-binary

--- src/leetcode/add-binary/index.js
+++ src/leetcode/add-binary/index.js
@@ -4,17 +4,14 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-  let output = '';
   let i = a.length - 1;
   let j = b.length - 1;
   let c = 0;
+  let output = '';
   while (i >= 0 || j >= 0 || c > 0) {
-    // prettier-ignore
-    const sum = (i >= 0 ? parseInt(a[i]) : 0)
-      + (j >= 0 ? parseInt(b[j]) : 0)
-      + c;
-    output = (sum & 1) + output;
-    c = sum >> 1;
+    const sum = parseInt(a[i] || '0') + parseInt(b[j] || '0') + c;
+    output = (sum % 2) + output;
+    c = Math.floor(sum / 2);
     i -= 1;
     j -= 1;
   }
