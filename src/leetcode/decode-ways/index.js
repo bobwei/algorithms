6014c95b57c4ca56ac2e539b2500a72b66851204
Bob Wei <bob.wei@linecorp.com>
2020-03-05 12:25:31 +0800

    Update decode-ways

--- src/leetcode/decode-ways/index.js
+++ src/leetcode/decode-ways/index.js
@@ -3,23 +3,25 @@
  * @return {number}
  */
 var numDecodings = function(s) {
-  let dp = parseInt(s[0]) > 0 ? 1 : 0;
   let x = 1;
-  let y = dp;
-  for (let i = 2; i <= s.length; i++) {
-    // prettier-ignore
-    dp = (isDecodable(s.substring(i - 1, i)) ? y : 0) +
-      (isDecodable(s.substring(i - 2, i)) ? x : 0);
+  let y = isDecodable(s[0]) ? 1 : 0;
+  let dp = y;
+  for (let i = 1; i < s.length; i++) {
+    const suffix2 = s.substring(i - 1, i + 1);
+    const suffix1 = s.substring(i, i + 1);
+    dp = (isDecodable(suffix2) ? x : 0) + (isDecodable(suffix1) ? y : 0);
     x = y;
     y = dp;
   }
   return dp;
 };
 
-function isDecodable(s) {
-  const num = parseInt(s);
-  if (s.length === 1) {
+function isDecodable(str) {
+  const num = parseInt(str);
+  if (str.length === 1) {
     return num > 0;
   }
-  return num >= 10 && num <= 26;
+  if (str.length === 2) {
+    return num >= 10 && num <= 26;
+  }
 }
