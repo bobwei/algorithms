6a9e10311964071cef91afb7ba2245fc0d1455f1
Bob Wei <bob.wei@linecorp.com>
2020-03-06 18:31:55 +0800

    Update goat-latin

--- src/leetcode/goat-latin/index.js
+++ src/leetcode/goat-latin/index.js
@@ -4,27 +4,19 @@
  */
 var toGoatLatin = function(S) {
   return S.split(' ')
-    .filter((str) => str.length > 0)
     .map(mapper)
     .join(' ');
 };
 
-function mapper(str, i) {
-  if (isVowel(str)) {
-    return str + 'ma' + times('a', i + 1);
+function mapper(word, i) {
+  if (isVowel(word[0])) {
+    return word + 'ma' + 'a'.repeat(i + 1);
   }
-  return str.substring(1) + str[0] + 'ma' + times('a', i + 1);
+  const [first, ...rest] = word;
+  return rest.join('') + first + 'ma' + 'a'.repeat(i + 1);
 }
 
-const vowel = new Set(['a', 'e', 'i', 'o', 'u']);
-function isVowel(str) {
-  return vowel.has(str[0].toLowerCase());
-}
-
-function times(str, n) {
-  let output = '';
-  for (let i = 0; i < n; i++) {
-    output += str;
-  }
-  return output;
+function isVowel(c) {
+  const set = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+  return set.has(c);
 }
