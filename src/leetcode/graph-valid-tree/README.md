759e02afda9b4504ca10ad3883508dc0f94321f1
Bob Wei <bob.wei@linecorp.com>
2020-03-12 18:19:05 +0800

    Update graph-valid-tree

---
+++ src/leetcode/graph-valid-tree/README.md
@@ -0,0 +1,12 @@
+# README
+
+## Algorithm
+
+A graph is a tree if:
+
+- No cycle
+- Connected
+
+So we are going to check no cycle and all nodes have been visited.
+
+We can test `hasCycle` from any node since any node in a tree can be the root. So we can start from any node.
--- src/leetcode/graph-valid-tree/index.js
+++ src/leetcode/graph-valid-tree/index.js
@@ -3,38 +3,34 @@
  * @param {number[][]} edges
  * @return {boolean}
  */
+var validTree = function(n, edges) {
+  const graph = createGraph(n, edges);
+  const visited = new Set();
+  if (hasCycle(graph, 0, null, visited)) {
+    return false;
+  }
+  return visited.size === n;
+};
 
-const hasCycle = (graph, v, pre, visited) => {
-  if (visited[v]) {
+function hasCycle(graph, u, pre, visited) {
+  if (visited.has(u)) {
     return true;
   }
-  visited[v] = true;
-  for (let i = 0; i < graph[v].length; i++) {
-    if (graph[v][i] === pre) {
-      continue;
-    }
-    if (hasCycle(graph, graph[v][i], v, visited)) {
+  visited.add(u);
+  for (const v of graph[u]) {
+    if (v === pre) continue;
+    if (hasCycle(graph, v, u, visited)) {
       return true;
     }
   }
   return false;
-};
+}
 
-var validTree = function(n, edges) {
-  const graph = [...new Array(n)].map(() => []);
-  for (let i = 0; i < edges.length; i++) {
-    const [v1, v2] = edges[i];
-    graph[v1].push(v2);
-    graph[v2].push(v1);
+function createGraph(n, edges) {
+  const graph = new Array(n).fill(null).map(() => []);
+  for (const [u, v] of edges) {
+    graph[u].push(v);
+    graph[v].push(u);
   }
-  const visited = new Array(n).fill(false);
-  if (hasCycle(graph, 0, null, visited)) {
-    return false;
-  }
-  for (let i = 0; i < visited.length; i++) {
-    if (!visited[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+  return graph;
+}
