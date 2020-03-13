e89d851cd585e3e5128b1126cdbee30fecdb51f9
Bob Wei <bob.wei@linecorp.com>
2020-03-13 15:25:41 +0800

    Update number-of-closed-islands

--- src/leetcode/number-of-closed-islands/index.js
+++ src/leetcode/number-of-closed-islands/index.js
@@ -5,40 +5,32 @@
 var closedIsland = function(grid) {
   const m = grid.length;
   const n = grid[0].length;
-  const visited = new Array(m).fill(null).map(() => new Array(n).fill(false));
-  let nClosed = 0;
+  let nClosedIsland = 0;
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-      if (grid[i][j] === 0 && !visited[i][j]) {
-        if (isClosed(grid, m, n, [i, j], visited)) {
-          nClosed += 1;
-        }
+      if (grid[i][j] === 0 && helper(grid, m, n, i, j)) {
+        nClosedIsland += 1;
       }
     }
   }
-  return nClosed;
+  return nClosedIsland;
 };
 
+// prettier-ignore
 const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
-function isClosed(grid, m, n, [x, y], visited) {
-  visited[x][y] = true;
-  let result = true;
+function helper(grid, m, n, x, y) {
+  grid[x][y] = 1;
+  let isClosed = true;
   for (const [di, dj] of dirs) {
     const i = x + di;
     const j = y + dj;
-    if (isValid(i, j, m, n)) {
-      if (!visited[i][j] && grid[i][j] === 0) {
-        result = isClosed(grid, m, n, [i, j], visited) && result;
-      }
-    } else {
-      result = false;
-    }
+    isClosed &= isValid(m, n, i, j) && (grid[i][j] === 1 || helper(grid, m, n, i, j));
   }
-  return result;
+  return isClosed;
 }
 
-function isValid(i, j, m, n) {
+function isValid(m, n, i, j) {
   if (i < 0 || i >= m || j < 0 || j >= n) {
     return false;
   }
