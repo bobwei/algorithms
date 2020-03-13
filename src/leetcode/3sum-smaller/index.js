36690fef2d312916540a9f9de682accaf1a9c760
Bob Wei <bob.wei@linecorp.com>
2020-03-14 00:04:33 +0800

    Update 3sum-smaller

--- src/leetcode/3sum-smaller/index.js
+++ src/leetcode/3sum-smaller/index.js
@@ -5,19 +5,19 @@
  */
 var threeSumSmaller = function(nums, target) {
   nums.sort((a, b) => a - b);
-  let n = 0;
+  let nSmallers = 0;
   for (let i = 0; i < nums.length; i++) {
     let j = i + 1;
     let k = nums.length - 1;
     while (j < k) {
       const sum = nums[i] + nums[j] + nums[k];
       if (sum < target) {
-        n += k - j;
+        nSmallers += k - j;
         j += 1;
       } else {
         k -= 1;
       }
     }
   }
-  return n;
+  return nSmallers;
 };
