13f03353a861a3b1c3ba795349d600c7412cd7e2
Bob Wei <bob.wei@linecorp.com>
2020-03-16 14:59:11 +0800

    Update 3sum-closest

--- src/leetcode/3sum-closest/index.js
+++ src/leetcode/3sum-closest/index.js
@@ -5,26 +5,21 @@
  */
 var threeSumClosest = function(nums, target) {
   nums.sort((a, b) => a - b);
-  let min = Infinity;
-  let closetSum;
+  let output = Infinity;
   for (let i = 0; i < nums.length; i++) {
     let j = i + 1;
     let k = nums.length - 1;
     while (j < k) {
       const sum = nums[i] + nums[j] + nums[k];
-      const diff = Math.abs(sum - target);
-      if (diff < min) {
-        min = diff;
-        closetSum = sum;
-      }
-      if (sum > target) {
-        k -= 1;
+      output = Math.abs(sum - target) < Math.abs(output - target) ? sum : output;
+      if (sum === target) {
+        return sum;
       } else if (sum < target) {
         j += 1;
       } else {
-        return target;
+        k -= 1;
       }
     }
   }
-  return closetSum;
+  return output;
 };
