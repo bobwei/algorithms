75afb536e1fbcdecd6386eb853e8dd0cd8aaae4a
Bob Wei <bob.wei@linecorp.com>
2020-03-16 11:22:24 +0800

    Update find-all-anagrams-in-a-string

--- src/leetcode/find-all-anagrams-in-a-string/index.js
+++ src/leetcode/find-all-anagrams-in-a-string/index.js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 var findAnagrams = function(s, p) {
-  const counter = new Counter(p);
   const output = [];
+  const counter = new Counter(p);
   let start = 0;
   for (let i = 0; i < s.length; i++) {
     counter.add(s[i]);
-    while (counter.nRedundant > 0) {
+    while (counter.nUnmatched > 0) {
       counter.delete(s[start]);
       start += 1;
     }
@@ -26,20 +26,20 @@ class Counter {
     for (const c of target) {
       this.freq[c] = (this.freq[c] || 0) + 1;
     }
-    this.nRedundant = 0;
+    this.nUnmatched = 0;
   }
 
   add(c) {
     this.freq[c] = (this.freq[c] || 0) - 1;
     if (this.freq[c] < 0) {
-      this.nRedundant += 1;
+      this.nUnmatched += 1;
     }
   }
 
   delete(c) {
     this.freq[c] += 1;
     if (this.freq[c] <= 0) {
-      this.nRedundant -= 1;
+      this.nUnmatched -= 1;
     }
   }
 }
