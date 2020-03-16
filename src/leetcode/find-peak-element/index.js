ec6afdbf4088387d23f5a4a1a133fc87b1bdd59a
Bob Wei <bob.wei@linecorp.com>
2020-03-16 11:50:02 +0800

    Update find-peak-element

--- src/leetcode/find-peak-element/index.js
+++ src/leetcode/find-peak-element/index.js
@@ -4,17 +4,18 @@
  */
 var findPeakElement = function(nums) {
   let left = 0;
-  let right = nums.length - 1;
+  let right = nums.length;
   while (left < right) {
     const mid = Math.floor((left + right) / 2);
-    const mid2 = mid + 1;
-    if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+    const leftVal = mid - 1 >= 0 ? nums[mid - 1] : -Infinity;
+    const rightVal = mid + 1 < nums.length ? nums[mid + 1] : -Infinity;
+    if (nums[mid] > leftVal && nums[mid] > rightVal) {
       return mid;
-    } else if (nums[mid2] > nums[mid]) {
-      left = mid2;
-    } else {
+    } else if (nums[mid] < nums[mid - 1]) {
       right = mid;
+    } else {
+      left = mid + 1;
     }
   }
-  return left;
+  return left < nums.length ? left : left - 1;
 };
