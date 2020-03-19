9e25c7593d54bae7f90800e8a5aa6fa0fd6dd336
Bob Wei <bob.wei@linecorp.com>
2020-03-19 23:14:42 +0800

    Update minimum-size-subarray-sum

--- src/leetcode/minimum-size-subarray-sum/index.js
+++ src/leetcode/minimum-size-subarray-sum/index.js
@@ -4,16 +4,16 @@
  * @return {number}
  */
 var minSubArrayLen = function(s, nums) {
-  let start = 0;
   let sum = 0;
-  let output = Infinity;
+  let start = 0;
+  let min = Infinity;
   for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
     while (sum >= s) {
-      output = Math.min(output, i - start + 1);
+      min = Math.min(min, i - start + 1);
       sum -= nums[start];
       start += 1;
     }
   }
-  return output < Infinity ? output : 0;
+  return min < Infinity ? min : 0;
 };
--- src/leetcode/minimum-size-subarray-sum/index.spec.js
+++
@@ -1,5 +0,0 @@
-import fn from './index';
-
-test('minimum-size-subarray-sum', () => {
-  expect(fn(10, [2, 2, 2, 10, 2, 2, 6, 4, 10])).toEqual(1);
-});
