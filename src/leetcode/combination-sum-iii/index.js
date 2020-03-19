7ce2e710a35fcdf42c1f764261d97606a00e5307
Bob Wei <bob.wei@linecorp.com>
2020-03-19 22:55:35 +0800

    Update combination-sum-iii

--- src/leetcode/combination-sum-iii/index.js
+++ src/leetcode/combination-sum-iii/index.js
@@ -3,14 +3,14 @@
  * @param {number} n
  * @return {number[][]}
  */
-var combinationSum3 = function(k, n, start = 1, sum = 0, selected = [], output = []) {
+var combinationSum3 = function(k, n, num = 1, sum = 0, selected = [], output = []) {
   if (selected.length >= k) {
-    if (sum === n) {
+    if (selected.length === k && sum === n) {
       output.push([...selected]);
     }
     return output;
   }
-  for (let i = start; i <= 9; i++) {
+  for (let i = num; i <= 9; i++) {
     if (sum + i > n) break;
     selected.push(i);
     combinationSum3(k, n, i + 1, sum + i, selected, output);
