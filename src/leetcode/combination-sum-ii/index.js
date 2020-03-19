bca251650d7038cd34040948bc30495527a2f150
Bob Wei <bob.wei@linecorp.com>
2020-03-19 22:44:59 +0800

    Update combination-sum-ii

--- src/leetcode/combination-sum-ii/index.js
+++ src/leetcode/combination-sum-ii/index.js
@@ -3,29 +3,23 @@
  * @param {number} target
  * @return {number[][]}
  */
-
-const helper = (candidates, target, start = 0, selected = new Set(), output = []) => {
-  if (target <= 0) {
-    if (target === 0) {
-      output.push([...selected].map((i) => candidates[i]));
-    }
-    return output;
-  }
-  for (let i = start; i < candidates.length; i++) {
-    if (target - candidates[i] < 0) {
-      break;
-    }
-    if (candidates[i] === candidates[i - 1] && !selected.has(i - 1)) {
-      continue;
-    }
-    selected.add(i);
-    helper(candidates, target - candidates[i], i + 1, selected, output);
-    selected.delete(i);
-  }
-  return output;
-};
-
 var combinationSum2 = function(candidates, target) {
   candidates.sort((a, b) => a - b);
   return helper(candidates, target);
 };
+
+function helper(candidates, target, index = 0, sum = 0, selected = [], output = []) {
+  if (sum >= target) {
+    if (sum === target) {
+      output.push([...selected]);
+    }
+    return output;
+  }
+  for (let i = index; i < candidates.length; i++) {
+    if (i > index && candidates[i] === candidates[i - 1]) continue;
+    selected.push(candidates[i]);
+    helper(candidates, target, i + 1, sum + candidates[i], selected, output);
+    selected.pop();
+  }
+  return output;
+}
