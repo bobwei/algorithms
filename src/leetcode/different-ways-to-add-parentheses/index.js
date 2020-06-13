0ba93d406fab8eeddf6601d023c7f8fa97c6ddb6
Bob Wei <bob61016@gmail.com>
2020-06-13 11:14:54 +0800

    Update index.js

--- src/leetcode/different-ways-to-add-parentheses/index.js
+++ src/leetcode/different-ways-to-add-parentheses/index.js
@@ -2,44 +2,42 @@
  * @param {string} input
  * @return {number[]}
  */
-var diffWaysToCompute = function(input) {
-  return helper(input, 0, input.length);
-};
-
-function helper(s, start, end, cache = {}) {
-  const key = s.substring(start, end);
+var diffWaysToCompute = function(input, start = 0, end = input.length, cache = {}) {
+  const key = input.substring(start, end);
   if (key in cache) {
     return cache[key];
   }
   const output = [];
   for (let i = start; i < end; i++) {
-    if (isOperator(s[i])) {
-      const left = helper(s, start, i);
-      const right = helper(s, i + 1, end);
-      for (const nLeft of left) {
-        for (const nRight of right) {
-          output.push(compute(nLeft, nRight, s[i]));
+    if (isOperator(input[i])) {
+      const left = diffWaysToCompute(input, start, i);
+      const right = diffWaysToCompute(input, i + 1, end);
+      for (const n1 of left) {
+        for (const n2 of right) {
+          output.push(compute(n1, input[i], n2));
         }
       }
     }
   }
   if (!output.length) {
-    output.push(parseInt(s.substring(start, end)));
+    output.push(parseInt(input.substring(start, end)));
   }
   cache[key] = output;
   return output;
-}
+};
 
 function isOperator(c) {
   return c === '+' || c === '-' || c === '*';
 }
 
-function compute(n1, n2, op) {
+function compute(n1, op, n2) {
   if (op === '+') {
     return n1 + n2;
-  } else if (op === '-') {
+  }
+  if (op === '-') {
     return n1 - n2;
-  } else if (op === '*') {
+  }
+  if (op === '*') {
     return n1 * n2;
   }
 }
