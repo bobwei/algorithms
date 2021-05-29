e96ac2e7b5a05beff9d3195612976f115c0d591a
Bob Wei <bob61016@gmail.com>
2021-05-29 15:33:09 +0800

    Update k-closest-points-to-origin

--- src/leetcode/k-closest-points-to-origin/index.js
+++ src/leetcode/k-closest-points-to-origin/index.js
@@ -1,37 +1,37 @@
 /**
  * @param {number[][]} points
- * @param {number} K
+ * @param {number} k
  * @return {number[][]}
  */
-var kClosest = function(points, K) {
+var kClosest = function(points, k) {
   let left = 0;
   let right = points.length;
   while (left < right) {
     const p = partition(points, left, right);
-    if (K === p + 1) {
-      return points.slice(0, K);
-    } else if (K < p + 1) {
-      right = p;
-    } else {
+    if (k === p + 1) {
+      return points.slice(0, k);
+    } else if (k > p + 1) {
       left = p + 1;
+    } else {
+      right = p;
     }
   }
-  return [];
+  return points;
 };
 
-function getDist([x1, y1]) {
-  return Math.sqrt(x1 ** 2 + y1 ** 2);
-}
-
-function partition(arr, start, end, comparator = (a, b) => getDist(a) < getDist(b)) {
+function partition(points, start, end) {
   const p = end - 1;
   let j = start;
-  for (let i = start; i < end; i++) {
-    if (comparator(arr[i], arr[p])) {
-      [arr[i], arr[j]] = [arr[j], arr[i]];
+  for (let i = start; i < p; i++) {
+    if (getDistance(points[i]) <= getDistance(points[p])) {
+      [points[i], points[j]] = [points[j], points[i]];
       j += 1;
     }
   }
-  [arr[j], arr[p]] = [arr[p], arr[j]];
+  [points[j], points[p]] = [points[p], points[j]];
   return j;
+}
+
+function getDistance(p1, p2 = [0, 0]) {
+  return Math.sqrt(p1[0] ** 2 + p1[1] ** 2);
 }
