3feee55a6e0e1266bfb626073cea77f2c0c6a011
Bob Wei <bob61016@gmail.com>
2021-05-29 22:23:51 +0800

    Update subarray-sum-equals-k

--- src/leetcode/subarray-sum-equals-k/index.js
+++ src/leetcode/subarray-sum-equals-k/index.js
@@ -5,12 +5,12 @@
  */
 var subarraySum = function(nums, k) {
   const freq = { 0: 1 };
-  let nSubarrs = 0;
+  let output = 0;
   let sum = 0;
   for (const num of nums) {
     sum += num;
-    nSubarrs += freq[sum - k] || 0;
+    output += freq[sum - k] || 0;
     freq[sum] = (freq[sum] || 0) + 1;
   }
-  return nSubarrs;
+  return output;
 };
