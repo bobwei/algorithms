c12cc248ba005dc49de69ea16cb160baced78db1
Bob Wei <bob61016@gmail.com>
2021-06-01 00:13:36 +0800

    Update binary-tree-right-side-view

--- src/leetcode/binary-tree-right-side-view/index.js
+++ src/leetcode/binary-tree-right-side-view/index.js
@@ -1,8 +1,9 @@
 /**
  * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
  * }
  */
 /**
