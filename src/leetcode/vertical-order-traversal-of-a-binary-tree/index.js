3e49276471c0b9b6f10ef9698fc44d68ed0d658a
Bob Wei <bob61016@gmail.com>
2021-06-02 00:07:31 +0800

    Update vertical-order-traversal-of-a-binary-tree

--- src/leetcode/vertical-order-traversal-of-a-binary-tree/index.js
+++ src/leetcode/vertical-order-traversal-of-a-binary-tree/index.js
@@ -1,8 +1,9 @@
 /**
  * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
  * }
  */
 /**
@@ -13,26 +14,28 @@ var verticalTraversal = function(root) {
   if (!root) {
     return [];
   }
+  const map = {};
   const queue = [[root, 0, 0]];
-  const cols = {};
   while (queue.length) {
-    const [node, x, y] = queue.shift();
-    if (!(x in cols)) cols[x] = [];
-    cols[x].push([node.val, y]);
+    const [node, i, j] = queue.shift();
+    if (!(j in map)) {
+      map[j] = [];
+    }
+    map[j].push([node.val, i]);
     if (node.left) {
-      queue.push([node.left, x - 1, y + 1]);
+      queue.push([node.left, i + 1, j - 1]);
     }
     if (node.right) {
-      queue.push([node.right, x + 1, y + 1]);
+      queue.push([node.right, i + 1, j + 1]);
     }
   }
-  const keys = Object.keys(cols)
-    .map((c) => parseInt(c))
-    .sort((a, b) => a - b);
+  const cols = Object.keys(map).sort((a, b) => parseInt(a) - parseInt(b));
   const output = [];
-  for (const x of keys) {
-    cols[x].sort((a, b) => (a[1] !== b[1] ? a[1] - b[1] : a[0] - b[0]));
-    output.push(cols[x].map(([val]) => val));
+  for (const col of cols) {
+    const arr = map[col]
+      .sort((a, b) => (a[1] !== b[1] ? a[1] - b[1] : a[0] - b[0]))
+      .map(([val]) => val);
+    output.push(arr);
   }
   return output;
 };
