4b397155884dbfbddf8300657de15847817a45ff
Bob Wei <bob61016@gmail.com>
2021-06-04 13:18:23 +0800

    Update divide-two-integers

--- src/leetcode/divide-two-integers/index.js
+++ src/leetcode/divide-two-integers/index.js
@@ -3,31 +3,23 @@
  * @param {number} divisor
  * @return {number}
  */
-
-const MAX = 2 ** 31;
-
 var divide = function(dividend, divisor) {
   const sign = (dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0) ? 1 : -1;
-  dividend = Math.abs(dividend);
-  divisor = Math.abs(divisor);
-  let output = 0;
-  let m = dividend;
-  let n = divisor;
-  let i = 1;
-  while (m >= n) {
-    while (n + n <= m) {
-      n = n + n;
-      i = i + i;
-      if (sign > 0 && n > MAX - 1) {
-        return MAX - 1;
-      } else if (sign < 0 && n > MAX) {
-        return MAX;
-      }
-    }
-    output += i;
-    m -= n;
-    n = divisor;
-    i = 1;
-  }
-  return sign > 0 ? output : -output;
+  const r = helper(Math.abs(dividend), Math.abs(divisor));
+  return sign > 0 ? Math.min(r, MAX - 1) : Math.max(-r, -MAX);
 };
+
+function helper(dividend, divisor) {
+  if (dividend < divisor) {
+    return 0;
+  }
+  let i = 1;
+  let n = divisor;
+  while (n + n < dividend) {
+    i += i;
+    n += n;
+  }
+  return i + helper(dividend - n, divisor);
+}
+
+const MAX = 2 ** 31;
