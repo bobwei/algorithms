8775f18911d2f1def1f882ede353f329ce1b5e48
Bob Wei <bob61016@gmail.com>
2021-06-05 00:08:07 +0800

    Update first-bad-version

--- src/leetcode/first-bad-version/index.js
+++ src/leetcode/first-bad-version/index.js
@@ -21,11 +21,11 @@ var solution = function(isBadVersion) {
     let left = 1;
     let right = n;
     while (left < right) {
-      const mid = Math.floor((left + right) / 2);
-      if (isBadVersion(mid)) {
-        right = mid;
+      const m = Math.floor((left + right) / 2);
+      if (isBadVersion(m)) {
+        right = m;
       } else {
-        left = mid + 1;
+        left = m + 1;
       }
     }
     return left;
