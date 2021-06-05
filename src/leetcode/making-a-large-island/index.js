6ab7978dc72d512f714960a8c4b39d8358e11d32
Bob Wei <bob61016@gmail.com>
2021-06-05 15:18:59 +0800

    Update making-a-large-island

--- src/leetcode/making-a-large-island/index.js
+++ src/leetcode/making-a-large-island/index.js
@@ -5,84 +5,57 @@
 var largestIsland = function(grid) {
   const m = grid.length;
   const n = grid[0].length;
-  const set = new DisjointSet();
-  // prettier-ignore
-  const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+  const map = {};
+  let index = 2;
+  let max = 0;
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j] === 1) {
-        const k1 = createKey(m, n, i, j);
-        set.find(k1);
-        for (const [di, dj] of dirs) {
-          const x = i + di;
-          const y = j + dj;
-          if (isValid(m, n, x, y) && grid[x][y] === 1) {
-            const k2 = createKey(m, n, x, y);
-            set.union(k1, k2);
-          }
-        }
+        const size = dfs(grid, m, n, i, j, index);
+        map[index] = size;
+        max = Math.max(max, size);
+        index += 1;
       }
     }
   }
-  let max = set.maxSize;
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j] === 0) {
         const visited = new Set();
-        let area = 1;
+        let size = 1;
         for (const [di, dj] of dirs) {
           const x = i + di;
           const y = j + dj;
-          if (isValid(m, n, x, y) && grid[x][y] === 1) {
-            const key = createKey(m, n, x, y);
-            const root = set.find(key);
-            if (!visited.has(root)) {
-              visited.add(root);
-              area += set.getSize(root);
+          if (isValid(m, n, x, y)) {
+            const key = grid[x][y];
+            if (!visited.has(index) && key in map) {
+              visited.add(key);
+              size += map[key];
             }
           }
         }
-        max = Math.max(max, area);
+        max = Math.max(max, size);
       }
     }
   }
   return max;
 };
 
-class DisjointSet {
-  constructor() {
-    this.roots = {};
-    this.sizes = {};
-  }
+// prettier-ignore
+const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
-  union(a, b) {
-    const r1 = this.find(a);
-    const r2 = this.find(b);
-    if (r1 !== r2) {
-      this.roots[r2] = r1;
-      this.sizes[r1] += this.sizes[r2];
-    }
+function dfs(grid, m, n, i, j, index) {
+  if (!isValid(m, n, i, j) || grid[i][j] === index || grid[i][j] === 0) {
+    return 0;
   }
-
-  find(r) {
-    if (!(r in this.roots)) {
-      this.roots[r] = r;
-      this.sizes[r] = 1;
-    }
-    let ptr = r;
-    while (this.roots[ptr] !== ptr) {
-      ptr = this.roots[ptr];
-    }
-    return ptr;
+  grid[i][j] = index;
+  let size = 1;
+  for (const [di, dj] of dirs) {
+    const x = i + di;
+    const y = j + dj;
+    size += dfs(grid, m, n, x, y, index);
   }
-
-  getSize(r) {
-    return this.sizes[r];
-  }
-
-  get maxSize() {
-    return Math.max(...Object.values(this.sizes));
-  }
+  return size;
 }
 
 function isValid(m, n, i, j) {
@@ -90,8 +63,4 @@ function isValid(m, n, i, j) {
     return false;
   }
   return true;
-}
-
-function createKey(m, n, i, j) {
-  return n * i + j;
 }
