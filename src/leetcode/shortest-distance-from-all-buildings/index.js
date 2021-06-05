5a078c92af0c78aaaf87fb2abb194d6e19e40d10
Bob Wei <bob61016@gmail.com>
2021-06-05 11:35:27 +0800

    Update shortest-distance-from-all-buildings

--- src/leetcode/shortest-distance-from-all-buildings/index.js
+++ src/leetcode/shortest-distance-from-all-buildings/index.js
@@ -3,52 +3,50 @@
  * @return {number}
  */
 var shortestDistance = function(grid) {
-  if (!grid.length || !grid[0].length) {
-    return -1;
-  }
   const m = grid.length;
   const n = grid[0].length;
-  const distance = [...new Array(m)].map(() => new Array(n).fill(0));
+  const dist = new Array(m).fill(null).map(() => new Array(n).fill(0));
   let target = 0;
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j] === 1) {
-        bfs(grid, { i, j }, m, n, distance, target);
+        bfs(grid, m, n, [i, j], dist, target);
         target -= 1;
       }
     }
   }
-  let min = Infinity;
+  let output = Infinity;
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j] === target) {
-        min = Math.min(min, distance[i][j]);
+        output = Math.min(output, dist[i][j]);
       }
     }
   }
-  return min < Infinity ? min : -1;
+  return output < Infinity ? output : -1;
 };
 
+// prettier-ignore
 const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
-function bfs(grid, position, m, n, distance, target) {
-  const queue = [[position, 0]];
+function bfs(grid, m, n, p, dist, target) {
+  const queue = [[p, 0]];
   while (queue.length) {
-    const [p, d] = queue.shift();
+    const [[i, j], d] = queue.shift();
     for (const [di, dj] of dirs) {
-      const i = p.i + di;
-      const j = p.j + dj;
-      if (isValid(i, j, m, n) && grid[i][j] === target) {
-        grid[i][j] -= 1;
-        distance[i][j] += d + 1;
-        queue.push([{ i, j }, d + 1]);
+      const x = i + di;
+      const y = j + dj;
+      if (isValid(m, n, x, y) && grid[x][y] === target) {
+        grid[x][y] -= 1;
+        dist[x][y] += d + 1;
+        queue.push([[x, y], d + 1]);
       }
     }
   }
 }
 
-function isValid(i, j, m, n) {
-  if (i < 0 || i >= m || j < 0 || j >= n) {
+function isValid(m, n, i, j) {
+  if (i < 0 || j < 0 || i >= m || j >= n) {
     return false;
   }
   return true;
